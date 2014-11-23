474323a9dc08cae9bb2911bc807b46725105d0d5
mmashwani <mmashwani@gmail.com>
2014-11-22 23:56:07 -0600

    Modify change date, update change log
    Modify change date, update change log

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -56,7 +56,7 @@ Param
 ## Variables: Script Info
 [version]$appDeployMainScriptVersion = [version]'3.5.0'
 [version]$appDeployMainScriptMinimumConfigVersion = [version]'3.5.0'
-[string]$appDeployMainScriptDate = '11/19/2014'
+[string]$appDeployMainScriptDate = '11/22/2014'
 [hashtable]$appDeployMainScriptParameters = $PSBoundParameters
 
 ## Variables: Datetime and Culture
