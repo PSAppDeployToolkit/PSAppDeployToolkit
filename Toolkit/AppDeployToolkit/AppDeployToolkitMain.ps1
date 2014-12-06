c366641d9b643ce367fbf30bd1a6cff4c312c1e9
mmashwani <mmashwani@gmail.com>
2014-12-05 20:50:37 -0600

    Minor edits to toolkit
    Minor edits to toolkit

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -8172,16 +8172,6 @@ Write-Log -Message "PowerShell Version is [$envPSVersion $psArchitecture]" -Sour
 Write-Log -Message "PowerShell CLR (.NET) version is [$envCLRVersion]" -Source $appDeployToolkitName
 Write-Log -Message $scriptSeparator -Source $appDeployToolkitName
 
-## Get the DPI scaling, property only exists if DPI scaling has been changed on the system at least once
-[int32]$dpiPixels = Get-RegistryKey -Key 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontDPI' | Select-Object -ExpandProperty LogPixels -ErrorAction 'SilentlyContinue'
-Switch ($dpiPixels) {
-	96 { [int32]$dpiScale = 100 }
-	120 { [int32]$dpiScale = 125 }
-	144 { [int32]$dpiScale = 150 }
-	192 { [int32]$dpiScale = 200 }
-	Default { [int32]$dpiScale = 100 }
-}
-
 ## Check deployment type (install/uninstall)
 Switch ($deploymentType) {
 	'Install'   { $deploymentTypeName = $configDeploymentTypeInstall }
@@ -8201,7 +8191,17 @@ Else {
 	Write-Log -Message "Script [$scriptPath] invoked directly" -Source $appDeployToolkitName
 }
 
-## Get a list of all users logged on to the system (both local and RDP users)
+## Get the DPI scaling, property only exists if DPI scaling has been changed on the system at least once
+[int32]$dpiPixels = Get-RegistryKey -Key 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontDPI' | Select-Object -ExpandProperty LogPixels -ErrorAction 'SilentlyContinue'
+Switch ($dpiPixels) {
+	96 { [int32]$dpiScale = 100 }
+	120 { [int32]$dpiScale = 125 }
+	144 { [int32]$dpiScale = 150 }
+	192 { [int32]$dpiScale = 200 }
+	Default { [int32]$dpiScale = 100 }
+}
+
+## Get a list of all users logged on to the system (both local and RDP users), and discover session details for account executing script
 [psobject[]]$LoggedOnUserSessions = Get-LoggedOnUser
 Write-Log -Message "Logged on user session details: `n$($LoggedOnUserSessions | Format-List | Out-String)" -Source $appDeployToolkitName
 [string[]]$usersLoggedOn = $LoggedOnUserSessions | ForEach-Object { $_.NTAccount }
