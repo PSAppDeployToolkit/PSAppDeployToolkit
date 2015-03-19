cc109c8894964578df815f1be0ac87cef75ee843
mmashwani <mmashwani@gmail.com>
2015-03-19 02:05:19 -0500

    Added -PassThru option to Test-Battery
    Added -PassThru option to Test-Battery which will return the following
    properties: IsLaptop, IsUsingACPower, ACPowerLineStatus,
    BatteryChargeStatus, BatteryLifePercent, BatteryLifeRemaining,
    BatteryFullLifetime

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -7178,17 +7178,26 @@ Function Send-Keys {
 Function Test-Battery {
 <#
 .SYNOPSIS
-	Tests whether the local machine is running on battery.
+	Tests whether the local machine is running on AC power or not.
 .DESCRIPTION
-	Tests whether the local machine is running on battery and returns true/false.
+	Tests whether the local machine is running on AC power and returns true/false. For detailed information, use -PassThru option.
+.PARAMETER PassThru
+	Outputs a hashtable containing the following properties:
+	IsLaptop, IsUsingACPower, ACPowerLineStatus, BatteryChargeStatus, BatteryLifePercent, BatteryLifeRemaining, BatteryFullLifetime
 .EXAMPLE
 	Test-Battery
+.EXAMPLE
+	(Test-Battery -PassThru).IsLaptop
+	Determines if the current system is a laptop or not.
 .NOTES
 .LINK
 	http://psappdeploytoolkit.codeplex.com
 #>
 	[CmdletBinding()]
 	Param (
+		[Parameter(Mandatory=$false)]
+		[ValidateNotNullOrEmpty()]
+		[switch]$PassThru = $false
 	)
 	
 	Begin {
@@ -7198,6 +7207,9 @@ Function Test-Battery {
 		
 		## PowerStatus class found in this assembly is more reliable than WMI in cases where the battery is failing.
 		Add-Type -Assembly System.Windows.Forms -ErrorAction 'SilentlyContinue'
+		
+		## Initialize a hashtable to store informaiton about system type and power status
+		[hashtable]$SystemTypePowerStatus = @{ }
 	}
 	Process {
 		Write-Log -Message 'Check if system is using AC power or if it is running on battery...' -Source ${CmdletName}
@@ -7209,9 +7221,11 @@ Function Test-Battery {
 		#	Online  : The system is using AC power.
 		#	Unknown : The power status of the system is unknown.
 		[string]$PowerLineStatus = $PowerStatus.PowerLineStatus
+		$SystemTypePowerStatus.Add('ACPowerLineStatus', $PowerStatus.PowerLineStatus)
 		
 		## Get the current battery charge status. Possible values: High, Low, Critical, Charging, NoSystemBattery, Unknown.
 		[string]$BatteryChargeStatus = $PowerStatus.BatteryChargeStatus
+		$SystemTypePowerStatus.Add('BatteryChargeStatus', $PowerStatus.BatteryChargeStatus)
 		
 		## Get the approximate amount, from 0.00 to 1.0, of full battery charge remaining.
 		#  This property can report 1.0 when the battery is damaged and Windows can't detect a battery.
@@ -7220,14 +7234,17 @@ Function Test-Battery {
 		If (($BatteryChargeStatus -eq 'NoSystemBattery') -or ($BatteryChargeStatus -eq 'Unknown')) {
 			[single]$BatteryLifePercent = 0.0
 		}
+		$SystemTypePowerStatus.Add('BatteryLifePercent', $PowerStatus.BatteryLifePercent)
 		
 		## The reported approximate number of seconds of battery life remaining. It will report –1 if the remaining life is unknown because the system is on AC power.
 		[int32]$BatteryLifeRemaining = $PowerStatus.BatteryLifeRemaining
+		$SystemTypePowerStatus.Add('BatteryLifeRemaining', $PowerStatus.BatteryLifeRemaining)
 		
 		## Get the manufacturer reported full charge lifetime of the primary battery power source in seconds.
 		#  The reported number of seconds of battery life available when the battery is fully charged, or -1 if it is unknown.
 		#  This will only be reported if the battery supports reporting this information. You will most likely get -1, indicating unknown.
 		[int32]$BatteryFullLifetime = $PowerStatus.BatteryFullLifetime
+		$SystemTypePowerStatus.Add('BatteryFullLifetime', $PowerStatus.BatteryFullLifetime)
 		
 		## Determine if the system is using AC power
 		[boolean]$OnACPower = $false
@@ -7247,8 +7264,22 @@ Function Test-Battery {
 				Write-Log -Message "System power status is [$PowerLineStatus] and battery charge status is [$BatteryChargeStatus]. Therefore, we will report system is using battery power." -Source ${CmdletName}
 			}
 		}
+		$SystemTypePowerStatus.Add('IsUsingACPower', $OnACPower)
 		
-		Write-Output $OnACPower
+		## Determine if the system is a IsLaptop
+		If (($BatteryChargeStatus -eq 'NoSystemBattery') -or ($BatteryChargeStatus -eq 'Unknown')) {
+			$SystemTypePowerStatus.Add('IsLaptop', $false)
+		}
+		Else {
+			$SystemTypePowerStatus.Add('IsLaptop', $true)
+		}
+		
+		If ($PassThru) {
+			Write-Output $SystemTypePowerStatus
+		}
+		Else {
+			Write-Output $OnACPower
+		}
 	}
 	End {
 		Write-FunctionHeaderOrFooter -CmdletName ${CmdletName} -Footer
