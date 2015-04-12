ea64f5bd4ba4780036d0ae549e5062a9214c3028
mmashwani <mmashwani@gmail.com>
2015-04-11 20:39:34 -0500

    Minor update to custom C# types
    Minor update to custom C# types

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.cs
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.cs
@@ -17,7 +17,7 @@ namespace PSADT
 		{
 			DONT_RESOLVE_DLL_REFERENCES 		= 0x00000001,
 			LOAD_IGNORE_CODE_AUTHZ_LEVEL		= 0x00000010,
-			LOAD_LIBRARY_AS_DATAFILE			= 0x00000002,
+			LOAD_LIBRARY_AS_DATAFILE 			= 0x00000002,
 			LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE	= 0x00000040,
 			LOAD_LIBRARY_AS_IMAGE_RESOURCE  	= 0x00000020,
 			LOAD_WITH_ALTERED_SEARCH_PATH 		= 0x00000008
@@ -712,10 +712,10 @@ namespace PSADT
 	public class TerminalSessionData
 	{
 		public int SessionId;
-		public QueryUserSession.WTS_CONNECTSTATE_CLASS ConnectionState;
+		public QueryUser.WTS_CONNECTSTATE_CLASS ConnectionState;
 		public string SessionName;
 		public bool IsUserSession;
-		public TerminalSessionData(int sessionId, QueryUserSession.WTS_CONNECTSTATE_CLASS connState, string sessionName, bool isUserSession)
+		public TerminalSessionData(int sessionId, QueryUser.WTS_CONNECTSTATE_CLASS connState, string sessionName, bool isUserSession)
 		{
 			SessionId = sessionId;
 			ConnectionState = connState;
@@ -732,7 +732,7 @@ namespace PSADT
 		public string DomainName;
 		public int SessionId;
 		public string SessionName;
-		public QueryUserSession.WTS_CONNECTSTATE_CLASS ConnectState;
+		public QueryUser.WTS_CONNECTSTATE_CLASS ConnectState;
 		public bool IsCurrentSession;
 		public bool IsConsoleSession;
 		public bool IsUserSession;
