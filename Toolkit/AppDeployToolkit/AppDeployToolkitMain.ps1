ab66f95b16fc9d03d1e99b7a10126df2afe02ce3
mmashwani <mmashwani@gmail.com>
2015-04-22 06:31:24 -0500

    Updated example for Show-InstallationWelcome
    Updated example for Show-InstallationWelcome

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -4813,8 +4813,8 @@ Function Show-InstallationWelcome {
 	Show-InstallationWelcome -CloseApps 'winword=Microsoft Office Word,excel=Microsoft Office Excel' -CloseAppsCountdown 600
 	Prompt the user to close Word and Excel, with customized descriptions for the applications and automatically close the applications after 10 minutes.
 .EXAMPLE
-	Show-InstallationWelcome -CloseApps 'winword.exe,msaccess.exe,excel.exe' -PersistPrompt
-	Prompt the user to close Word, MSAccess and Excel if the processes match the exact name specified (use .exe for exact matches).
+	Show-InstallationWelcome -CloseApps 'winword,msaccess,excel' -PersistPrompt
+	Prompt the user to close Word, MSAccess and Excel.
 	By using the PersistPrompt switch, the dialog will return to the center of the screen every 10 seconds so the user cannot ignore it by dragging it aside.
 .EXAMPLE
 	Show-InstallationWelcome -AllowDefer -DeferDeadline '25/08/2013'
