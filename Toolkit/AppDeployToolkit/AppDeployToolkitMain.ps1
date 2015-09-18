fc6fbc596b42cabbf4ce846581dcf44c480a57cb
mmashwani <mmashwani@gmail.com>
2015-09-18 10:38:18 -0400

    Fixed typo in config XML file
    Fixed typo in config XML file for Swedish language message for
    'ClosePrompt_CountdownMessage'

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -54,7 +54,7 @@ Param (
 
 ## Variables: Script Info
 [version]$appDeployMainScriptVersion = [version]'3.6.6'
-[version]$appDeployMainScriptMinimumConfigVersion = [version]'3.6.5'
+[version]$appDeployMainScriptMinimumConfigVersion = [version]'3.6.6'
 [string]$appDeployMainScriptDate = '09/18/2015'
 [hashtable]$appDeployMainScriptParameters = $PSBoundParameters
 
