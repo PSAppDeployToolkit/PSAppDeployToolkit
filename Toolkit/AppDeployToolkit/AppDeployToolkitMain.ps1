f499a3fbed7d71784f4d67b99ea622bcce8d7fcd
mmashwani <mmashwani@gmail.com>
2015-09-21 23:37:57 -0400

    Updated date/time stamps

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -55,7 +55,7 @@ Param (
 ## Variables: Script Info
 [version]$appDeployMainScriptVersion = [version]'3.6.6'
 [version]$appDeployMainScriptMinimumConfigVersion = [version]'3.6.6'
-[string]$appDeployMainScriptDate = '09/18/2015'
+[string]$appDeployMainScriptDate = '09/21/2015'
 [hashtable]$appDeployMainScriptParameters = $PSBoundParameters
 
 ## Variables: Datetime and Culture
