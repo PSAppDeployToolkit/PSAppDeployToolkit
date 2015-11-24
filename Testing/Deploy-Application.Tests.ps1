00a2beea86de9ac403dcef76403f0b8396052650
Luke Boening <luke@boening.us>
2015-11-24 15:19:40 -0600

    Add simple test
    Let's see what it does

---
+++ Testing/Deploy-Application.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Deploy-Application" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
