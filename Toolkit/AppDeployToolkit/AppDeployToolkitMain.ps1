3fc45de4cc9078b4212a56bea5298c0e18f2cf36
wala0003 <wala0003@Gmail.com>
2017-01-13 14:07:37 -0600

    Add Execute-MSP function

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -508,7 +508,67 @@ Function Write-FunctionHeaderOrFooter {
 	}
 }
 #endregion
+#region Function Execute-MSP
+Function Execute-MSP {
+<#
+.SYNOPSIS
+	Reads SummaryInfo targeted product codes in MSP file and determines if the MSP file applies to any installed products
+    If a valid installed product is found, triggers the Execute-MSI function to patch the installation.
+.PARAMETER Path
+.EXAMPLE
+	Execute-MSP -Path 'Adobe_Reader_11.0.3_EN.msp'
+.NOTES
+.LINK
+	http://psappdeploytoolkit.com
+#>
+    [CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,HelpMessage='Please enter the path to the MSP file')]
+		[ValidateScript({('.msp' -contains [IO.Path]::GetExtension($_))})]
+		[Alias('FilePath')]
+		[string]$Path
+    )
 
+    Begin {
+		## Get the name of this function and write header
+		[string]${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name
+		Write-FunctionHeaderOrFooter -CmdletName ${CmdletName} -CmdletBoundParameters $PSBoundParameters -Header
+	}
+	Process {
+        ## If the MSP is in the Files directory, set the full path to the MSP
+		If (Test-Path -LiteralPath (Join-Path -Path $dirFiles -ChildPath $path -ErrorAction 'SilentlyContinue') -PathType 'Leaf' -ErrorAction 'SilentlyContinue') {
+			[string]$mspFile = Join-Path -Path $dirFiles -ChildPath $path
+		}
+		ElseIf (Test-Path -LiteralPath $Path -ErrorAction 'SilentlyContinue') {
+			[string]$mspFile = (Get-Item -LiteralPath $Path).FullName
+		}
+		Else {
+			Write-Log -Message "Failed to find MSP file [$path]." -Severity 3 -Source ${CmdletName}
+			If (-not $ContinueOnError) {
+				Throw "Failed to find MSP file [$path]."
+			}
+			Continue
+		}
+        Write-Log -Message 'Checking MSP file for valid product codes' -Source ${CmdletName}
+			
+        [boolean]$IsMSPNeeded = $false
+
+        $Installer = New-Object -com WindowsInstaller.Installer
+        $Database = $Installer.GetType().InvokeMember(“OpenDatabase”, “InvokeMethod”, $Null, $Installer, $($mspFile,([int32]32)))
+        [__comobject]$SummaryInformation = Get-ObjectProperty -InputObject $Database -PropertyName 'SummaryInformation'
+        [hashtable]$SummaryInfoProperty = @{}
+        $all = (Get-ObjectProperty -InputObject $SummaryInformation -PropertyName 'Property' -ArgumentList @(7)).Split(";")
+        Foreach($FormattedProductCode in $all) { 
+            [psobject]$MSIInstalled = Get-InstalledApplication -ProductCode $FormattedProductCode
+            If ($MSIInstalled) {[boolean]$IsMSPNeeded = $true }
+        }
+        Try { $null = [Runtime.Interopservices.Marshal]::ReleaseComObject($SummaryInformation) } Catch { }
+        Try { $null = [Runtime.Interopservices.Marshal]::ReleaseComObject($DataBase) } Catch { }
+        Try { $null = [Runtime.Interopservices.Marshal]::ReleaseComObject($Installer) } Catch { }
+        If ($IsMSPNeeded) { Execute-MSI -Action Patch -Path $Path }
+    }    
+}
+#endregion
 
 #region Function Write-Log
 Function Write-Log {
