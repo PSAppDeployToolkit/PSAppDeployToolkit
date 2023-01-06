41201f268b6bb7d9d705c428e670bf2673e76028
Sean Lillis <seanlillis@users.noreply.github.com>
2023-01-06 16:57:35 +0000

    Updated copyright to 2023 throughout

--- Toolkit/AppDeployToolkit/AppDeployToolkitHelp.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitHelp.ps1
@@ -3,7 +3,7 @@
 	Displays a graphical console to browse the help for the App Deployment Toolkit functions
     # LICENSE #
     PowerShell App Deployment Toolkit - Provides a set of functions to perform common application deployment tasks on Windows.
-    Copyright (C) 2017 - Sean Lillis, Dan Cunningham, Muhammad Mashwani, Aman Motazedian.
+    Copyright (C) 2023 - Sean Lillis, Dan Cunningham, Muhammad Mashwani, Aman Motazedian.
     This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
     You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 .DESCRIPTION
