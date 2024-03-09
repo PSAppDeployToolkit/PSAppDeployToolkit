b08c9c3c504a039dbe1a173b66af2776a0e6401a
Sean Lillis <seanlillis@users.noreply.github.com>
2024-03-09 15:52:32 +0000

    Added the ability to copy the Toolkit folder to a cache folder on the local machine and execute from there. Function: Copy-ContentToCache. The cache path can be configured in the config.xml file, default is $envProgramData\SoftwareCache

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -15869,6 +15869,68 @@ This function does not return any objects.
 }
 #endregion
 
+#region Function Copy-ContentToCache
+Function Copy-ContentToCache {
+    <#  
+.SYNOPSIS
+    Copies the toolkit content to a cache folder on the local machine and sets the $dirFiles directory to the cache path
+.DESCRIPTION
+    Copies the toolkit content to a cache folder on the local machine and sets the $dirFiles directory to the cache path
+.PARAMETER Path 
+    The path to the software cache folder
+.EXAMPLE
+    Copy-ContentToCache -Path 'C:\Windows\Temp\PSAppDeployToolkit'
+.NOTES
+.LINK
+    https://psappdeploytoolkit.com
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [String]$Path = "$configToolkitCachePath\$installName"
+    )
+
+    Begin {
+        ## Get the name of this function and write header
+        [String]${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name
+        Write-FunctionHeaderOrFooter -CmdletName ${CmdletName} -CmdletBoundParameters $PSBoundParameters -Header
+    }
+    Process {
+        Try {
+            ## Create the cache folder if it does not exist
+            If (-not (Test-Path -LiteralPath $Path -PathType 'Container')) {
+                Try {
+                    Write-Log -Message "Creating cache folder [$Path]." -Source ${CmdletName}
+                    $null = New-Item -Path $Path -ItemType 'Directory' -ErrorAction 'Stop'
+                }
+                Catch {
+                    Write-Log -Message "Failed to create cache folder [$Path]. `r`n$(Resolve-Error)" -Severity 3 -Source ${CmdletName}
+                    Throw "Failed to create cache folder [$Path]: $($_.Exception.Message)"  
+                }
+            }
+            Else {
+                Write-Log -Message "Cache folder [$Path] already exists." -Source ${CmdletName}
+            }
+
+            ## Copy the toolkit content to the cache folder
+            Write-Log -Message "Copying toolkit content to cache folder [$Path]." -Source ${CmdletName}
+            Copy-File -Path $scriptParentPath -Destination $Path -Recurse
+            # Set the Files directory to the cache path
+            Set-Variable -Name 'dirFiles' -Value "$CachePath\Files" -Scope 'Script'
+        }
+        Catch {
+            Write-Log -Message "Failed to copy toolkit content to cache folder [$Path]. `r`n$(Resolve-Error)" -Severity 3 -Source ${CmdletName}
+            Throw "Failed to copy toolkit content to cache folder [$Path]: $($_.Exception.Message)"
+        }
+    }
+    End {
+        Write-FunctionHeaderOrFooter -CmdletName ${CmdletName} -Footer
+    }
+}
+#endregion
+
+
 #endregion
 ##*=============================================
 ##* END FUNCTION LISTINGS
