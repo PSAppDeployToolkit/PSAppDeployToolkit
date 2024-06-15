50b0f21d77f7f25253aa6d51f80a0b76a0db149f
Mitchell Richters <mjr4077au@gmail.com>
2024-06-16 00:43:38 +1000

    - Simplify `Get-ADTDialogFunction`'s internals.
    Just use the literal DialogStyle value from the config. If the caller's made a typo then that's on them, we shouldn't be inferring anyone's default.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Private/Get-ADTDialogFunction.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Private/Get-ADTDialogFunction.ps1
@@ -1,9 +1,5 @@
 ﻿function Get-ADTDialogFunction
 {
-    # If the is classic, use this, otherwise use fluent for anything else.
-    if ((Get-ADTConfig).UI.DialogStyle -eq 'Classic')
-    {
-        return (Get-Command -Name ((Get-Variable -Name MyInvocation -Scope 1 -ValueOnly).MyCommand -replace '^(.+-ADT)(.+$)','$1Classic$2'))
-    }
-    return (Get-Command -Name ((Get-Variable -Name MyInvocation -Scope 1 -ValueOnly).MyCommand -replace '^(.+-ADT)(.+$)','$1Fluent$2'))
+    # Return the underlying function for the configured DialogStyle.
+    Get-Command -Name ((Get-Variable -Name MyInvocation -Scope 1 -ValueOnly).MyCommand -replace '^(.+-ADT)(.+$)',"`$1$((Get-ADTConfig).UI.DialogStyle)`$2")
 }
