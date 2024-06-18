d17ac8288e29b5bce9e3e3fdf4fbf738e404757b
Mitchell Richters <mjr4077au@gmail.com>
2024-06-18 18:25:13 +1000

    - Slight optimisation to `Import-ADTModuleState`.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Import-ADTModuleState.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Import-ADTModuleState.ps1
@@ -1,18 +1,18 @@
 ﻿function Import-ADTModuleState
 {
     # Restore the previously exported session and prepare it for asynchronous operation. The serialised state may be on-disk during BlockExecution operations.
-    Set-Variable -Name ADT -Scope Script -Option ReadOnly -Force -Value $(if ([System.IO.File]::Exists(($onDiskClixml = "$((Get-ADTModuleInfo).Name).xml")))
+    if ([System.IO.File]::Exists(($onDiskClixml = "$((Get-ADTModuleInfo).Name).xml")))
     {
-        Import-Clixml -LiteralPath $onDiskClixml
+        $adtData = (Set-Variable -Name ADT -Scope Script -Option ReadOnly -Force -PassThru -Value (Import-Clixml -LiteralPath $onDiskClixml)).Value
     }
     else
     {
-        [System.Management.Automation.PSSerializer]::Deserialize([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(($regPath = $Script:Serialisation.Hive.OpenSubKey($Script:Serialisation.Key, $true)).GetValue($Script:Serialisation.Name))))
+        $adtData = (Set-Variable -Name ADT -Scope Script -Option ReadOnly -Force -PassThru -Value ([System.Management.Automation.PSSerializer]::Deserialize([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(($regPath = $Script:Serialisation.Hive.OpenSubKey($Script:Serialisation.Key, $true)).GetValue($Script:Serialisation.Name)))))).Value
         $regPath.DeleteValue($Script:Serialisation.Name, $true)
-    })
+    }
 
     # Create new object based on serialised state and configure for async operations.
-    for ($i = 0; $i -lt ($adtData = Get-ADT).Sessions.Count; $i++)
+    for ($i = 0; $i -lt $adtData.Sessions.Count; $i++)
     {
         $adtData.Sessions[$i] = [ADTSession]$adtData.Sessions[$i]
         $adtData.Sessions[$i].InstallPhase = 'Asynchronous'
