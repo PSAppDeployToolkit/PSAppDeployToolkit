a3dd63c6b3032fdeb163fda4a4b84ae6d054c419
Mitchell Richters <mjr4077au@gmail.com>
2024-06-18 21:51:23 +1000

    - Move the export of the module's environment variables from `Open-ADTSession` to `Initialize-ADTModule`.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Initialize-ADTModule.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Initialize-ADTModule.ps1
@@ -1,7 +1,17 @@
 ﻿function Initialize-ADTModule
 {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCmdlet]$Cmdlet
+    )
+
+    # Initialise the module's global state.
     Initialize-ADTEnvironment
     Import-ADTConfig
     Import-ADTLocalizedStrings
-    (Get-ADT).LastExitCode = 0	
+    (Get-ADT).LastExitCode = 0
+
+    # Export environment variables to the user's scope.
+    [System.Void]$ExecutionContext.InvokeCommand.InvokeScript($Cmdlet.SessionState, {$args[0].GetEnumerator().ForEach({New-Variable -Name $_.Key -Value $_.Value -Option ReadOnly -Force})}.Ast.GetScriptBlock(), (Get-ADT).Environment)
 }
