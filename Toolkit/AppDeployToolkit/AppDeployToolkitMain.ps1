ac32817f5fb476db8e2bb048c399c610f6efc356
jvwam <130722708+jvwam@users.noreply.github.com>
2024-06-20 10:23:58 +1200

    Fix Get-FileVersion -ProductVersion whitespace bug
    #1006

--- Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -7234,6 +7234,7 @@ https://psappdeploytoolkit.com
                 }
 
                 If ($fileVersion) {
+                    $fileVersion = $fileVersion.Trim()
                     If ($ProductVersion) {
                         Write-Log -Message "Product version is [$fileVersion]." -Source ${CmdletName}
                     }
