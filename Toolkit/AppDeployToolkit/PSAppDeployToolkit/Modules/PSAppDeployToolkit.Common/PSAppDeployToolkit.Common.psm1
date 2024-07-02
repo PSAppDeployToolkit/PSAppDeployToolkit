b3bdd4aa2ef7f649ba624857d971c6c1f47026e2
Mitchell Richters <mjr4077au@gmail.com>
2024-07-02 18:42:23 +1000

    - Move constants from `Convert-ADTRegistryPath` into module's state.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Common/PSAppDeployToolkit.Common.psm1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Common/PSAppDeployToolkit.Common.psm1
@@ -15,3 +15,26 @@ Add-Type -AssemblyName System.Windows.Forms
 # Dot-source our imports and perform exports.
 (Get-ChildItem -Path $PSScriptRoot\*\*.ps1).FullName.ForEach({. $_})
 Export-ModuleMember -Function (Get-ChildItem -LiteralPath $PSScriptRoot\Public).BaseName
+
+# Registry path transformation constants used within Convert-ADTRegistryPath.
+New-Variable -Name ADTRegistry -Option Constant -Value ([ordered]@{
+    PathMatches = [System.Array]::AsReadOnly(@(
+        ':\\'
+        ':'
+        '\\'
+    ))
+    PathReplacements = ([ordered]@{
+        '^HKLM' = 'HKEY_LOCAL_MACHINE\'
+        '^HKCR' = 'HKEY_CLASSES_ROOT\'
+        '^HKCU' = 'HKEY_CURRENT_USER\'
+        '^HKU' = 'HKEY_USERS\'
+        '^HKCC' = 'HKEY_CURRENT_CONFIG\'
+        '^HKPD' = 'HKEY_PERFORMANCE_DATA\'
+    })
+    WOW64Replacements = ([ordered]@{
+        '^(HKEY_LOCAL_MACHINE\\SOFTWARE\\Classes\\|HKEY_CURRENT_USER\\SOFTWARE\\Classes\\|HKEY_CLASSES_ROOT\\)(AppID\\|CLSID\\|DirectShow\\|Interface\\|Media Type\\|MediaFoundation\\|PROTOCOLS\\|TypeLib\\)' = '$1Wow6432Node\$2'
+        '^HKEY_LOCAL_MACHINE\\SOFTWARE\\' = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\'
+        '^HKEY_LOCAL_MACHINE\\SOFTWARE$' = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node'
+        '^HKEY_CURRENT_USER\\Software\\Microsoft\\Active Setup\\Installed Components\\' = 'HKEY_CURRENT_USER\Software\Wow6432Node\Microsoft\Active Setup\Installed Components\'
+    })
+}).AsReadOnly()
