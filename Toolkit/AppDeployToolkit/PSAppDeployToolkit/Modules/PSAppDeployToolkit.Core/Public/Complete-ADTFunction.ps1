a32ff1df743944da5e2625bdc2d71b20f4e32066
Mitchell Richters <mjr4077au@gmail.com>
2024-07-04 00:02:58 +1000

    - Incorporate workaround for https://github.com/PowerShell/PowerShell/issues/4568.
    🤢🤮

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Complete-ADTFunction.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Complete-ADTFunction.ps1
@@ -7,5 +7,12 @@
         [System.Management.Automation.PSCmdlet]$Cmdlet
     )
 
+    # Write debug log messages.
     Write-ADTLogEntry -Message 'Function End' -Source $Cmdlet.MyInvocation.MyCommand.Name -DebugMessage
+
+    # Restore original global verbosity if a value was archived off.
+    if ($null -ne ($OriginalVerbosity = $Cmdlet.SessionState.PSVariable.GetValue('OriginalVerbosity', $null)))
+    {
+        $Global:VerbosePreference = $OriginalVerbosity
+    }
 }
