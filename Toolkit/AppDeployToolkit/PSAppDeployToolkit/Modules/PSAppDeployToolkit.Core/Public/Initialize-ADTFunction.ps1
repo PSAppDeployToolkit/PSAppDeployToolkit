35c7c88ebfb40e9b1d578547adb78b04bbb9c866
Mitchell Richters <mjr4077au@gmail.com>
2024-07-03 23:39:20 +1000

    - Incorporate workaround for https://github.com/PowerShell/PowerShell/issues/1759.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Initialize-ADTFunction.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Initialize-ADTFunction.ps1
@@ -19,15 +19,17 @@
     }
 
     # Amend the caller's $ErrorActionPreference to archive off their provided value so we can always stop on a dime.
+    # For the caller-provided values, we deliberately use a string value to escape issues when 'Ignore' is passed.
+    # https://github.com/PowerShell/PowerShell/issues/1759#issuecomment-442916350
     $Cmdlet.SessionState.PSVariable.Set('OriginalErrorAction', $(if ($Cmdlet.MyInvocation.BoundParameters.ContainsKey('ErrorAction'))
     {
         # Caller's value directly against the function.
-        $Cmdlet.MyInvocation.BoundParameters.ErrorAction
+        $Cmdlet.MyInvocation.BoundParameters.ErrorAction.ToString()
     }
     elseif ($PSBoundParameters.ContainsKey('ErrorAction'))
     {
         # A function's own specified override.
-        $PSBoundParameters.ErrorAction
+        $PSBoundParameters.ErrorAction.ToString()
     }
     else
     {
