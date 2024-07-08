a3b51fffe3a3c56e195cd03fe648eefaa9f8354e
Mitchell Richters <mjr4077au@gmail.com>
2024-07-08 10:27:26 +1000

    - Properly throw if `Initialize-ADTModule` fails within the dialog functions we support without sessions.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Public/Show-ADTInstallationPrompt.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Public/Show-ADTInstallationPrompt.ps1
@@ -128,7 +128,14 @@
         # Initialise variables.
         if (!($adtSession = if (Test-ADTSessionActive) {Get-ADTSession}) -and !(Test-ADTModuleInitialised))
         {
-            Initialize-ADTModule
+            try
+            {
+                Initialize-ADTModule
+            }
+            catch
+            {
+                $PSCmdlet.ThrowTerminatingError($_)
+            }
         }
         $adtConfig = Get-ADTConfig
 
