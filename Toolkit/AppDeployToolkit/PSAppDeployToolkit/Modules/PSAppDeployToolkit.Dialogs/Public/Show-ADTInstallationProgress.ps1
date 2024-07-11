426cb9ebb144971efc561a93557a35cb09912711
Mitchell Richters <mjr4077au@gmail.com>
2024-07-12 09:04:29 +1000

    - Consolidate some of the dialog initialisation code into a private function.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Public/Show-ADTInstallationProgress.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Public/Show-ADTInstallationProgress.ps1
@@ -78,17 +78,7 @@
     dynamicparam
     {
         # Initialise the module first if needed.
-        if (!($adtSession = if (Test-ADTSessionActive) {Get-ADTSession}) -and !(Test-ADTModuleInitialised))
-        {
-            try
-            {
-                Initialize-ADTModule
-            }
-            catch
-            {
-                $PSCmdlet.ThrowTerminatingError($_)
-            }
-        }
+        $adtSession = Initialize-ADTDialogFunction -Cmdlet $PSCmdlet
         $adtStrings = Get-ADTStrings
         $fluentUi = (Get-ADTConfig).UI.DialogStyle -eq 'Fluent'
 
