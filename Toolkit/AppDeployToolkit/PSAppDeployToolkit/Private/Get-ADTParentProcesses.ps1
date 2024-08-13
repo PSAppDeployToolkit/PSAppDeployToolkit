af3b35a97d4fb9a4dac70d1a2ae5cc76aae08306
Mitchell Richters <mjr4077au@gmail.com>
2024-08-13 19:20:43 +1000

    - Add `Get-ADTParentProcesses` in prep for some upcoming work to change log paths to an SCCM or Intune location.

---
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Private/Get-ADTParentProcesses.ps1
@@ -0,0 +1,33 @@
+#---------------------------------------------------------------------------
+#
+#
+#
+#---------------------------------------------------------------------------
+
+function Get-ADTParentProcesses
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = "This function is appropriately named and we don't need PSScriptAnalyzer telling us otherwise.")]
+    param
+    (
+    )
+
+    # Open object to store all parents for returning. This also avoids an infinite loop situation.
+    $parents = [System.Collections.Generic.List[Microsoft.Management.Infrastructure.CimInstance]]::new()
+
+    # Get all processes from the system. WMI consistently gives us the parent on PowerShell 5.x and Core targets.
+    $processes = & $Script:CommandTable.'Get-CimInstance' -ClassName Win32_Process
+    $process = $processes | & {process {if ($_.ProcessId -eq $PID) {return $_}}}
+
+    # Get all parents for the currently stored process.
+    while ($process = $processes | & {process {if ($_.ProcessId -eq $process.ParentProcessId) {return $_}}})
+    {
+        if ($parents.Contains($process))
+        {
+            break
+        }
+        $parents.Add($process)
+    }
+
+    # Return all parents to the caller.
+    return $parents
+}
