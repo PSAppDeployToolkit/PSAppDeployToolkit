025eb20e86e266d0569e3a5c64214c89a6af4565
Mitchell Richters <mjr4077au@gmail.com>
2024-08-14 16:26:08 +1000

    - Re-write `Show-ADTBalloonTip` functions so that the worker scriptblock isn't unbound.
    The stacktrace was absolutely garbage when this error'd out. Also fixes issues with the XML when illegal characters are in the input values.

---
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Core/Public/Out-ADTPowerShellEncodedCommand.ps1
@@ -0,0 +1,12 @@
+﻿function Out-ADTPowerShellEncodedCommand
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.String]$Command
+	)
+
+	return [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Command))
+}
--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Submodules/PSAppDeployToolkit.Dialogs.Fluent/Public/Show-ADTBalloonTipFluent.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Dialogs/Submodules/PSAppDeployToolkit.Dialogs.Fluent/Public/Show-ADTBalloonTipFluent.ps1
@@ -63,21 +63,46 @@
         [System.Management.Automation.SwitchParameter]$NoWait
     )
 
-    # Initialise variables.
-    $adtEnv = Get-ADTEnvironment
-    $adtConfig = Get-ADTConfig
+    # Define internal worker function.
+    function New-ADTToastNotification
+    {
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ToolkitName,
 
-    # Define script block for toast notifications, pre-injecting variables and values.
-    $toastScriptBlock = [System.Management.Automation.ScriptBlock]::Create($ExecutionContext.InvokeCommand.ExpandString({
-        # Ensure script runs in strict mode since its in a new scope.
-        (Get-Variable -Name ErrorActionPreference).Value = [System.Management.Automation.ActionPreference]::Stop
-        (Get-Variable -Name ProgressPreference).Value = [System.Management.Automation.ActionPreference]::SilentlyContinue
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ModuleBase,
+
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ToastName,
+
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ToastLogo,
+
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ToastTitle,
+
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.String]$ToastText
+        )
+
+        # Ensure script runs in strict mode since this may be called in a new scope.
+        $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+        $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
         Set-StrictMode -Version 3
 
         # Add in required assemblies.
-        if ((Get-Variable -Name PSVersionTable -ValueOnly).PSEdition.Equals('Core'))
+        if ($PSVersionTable.PSEdition.Equals('Core'))
         {
-            Add-Type -AssemblyName (Get-ChildItem -Path '$Script:PSScriptRoot\lib\net6.0\*.dll').FullName
+            Add-Type -AssemblyName (Get-ChildItem -Path $ModuleBase\lib\net6.0\*.dll).FullName
         }
         else
         {
@@ -86,32 +111,47 @@
         }
 
         # Configure the notification centre.
-        Remove-Item -LiteralPath 'Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\$($adtEnv.appDeployToolkitName)' -Force -Confirm:`$false -ErrorAction Ignore
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\$($adtEnv.appDeployToolkitName)', 'ShowInActionCenter', 1, 'DWord')
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\$($adtEnv.appDeployToolkitName)', 'Enabled', 1, 'DWord')
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\$($adtEnv.appDeployToolkitName)', 'SoundFile', '', 'String')
+        $regPath = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings'
+        Remove-Item -LiteralPath "Registry::$regPath\$ToolkitName" -Force -Confirm:$false -ErrorAction Ignore
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'ShowInActionCenter', 1, [Microsoft.Win32.RegistryValueKind]::DWord)
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'Enabled', 1, [Microsoft.Win32.RegistryValueKind]::DWord)
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'SoundFile', [System.String]::Empty, [Microsoft.Win32.RegistryValueKind]::String)
 
         # Configure the toast notification.
-        Remove-Item -LiteralPath 'Registry::HKEY_CURRENT_USER\Software\Classes\AppUserModelId\$($adtEnv.appDeployToolkitName)' -Force -Confirm:`$false -ErrorAction Ignore
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Classes\AppUserModelId\$($adtEnv.appDeployToolkitName)', 'DisplayName', '$($adtConfig.UI.ToastName)', 'String')
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Classes\AppUserModelId\$($adtEnv.appDeployToolkitName)', 'ShowInSettings', 0, 'DWord')
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Classes\AppUserModelId\$($adtEnv.appDeployToolkitName)', 'IconUri', '$($adtConfig.Assets.Logo)', 'ExpandString')
-        [Microsoft.Win32.Registry]::SetValue('HKEY_CURRENT_USER\Software\Classes\AppUserModelId\$($adtEnv.appDeployToolkitName)', 'IconBackgroundColor', '', 'ExpandString')
+        $regPath = 'HKEY_CURRENT_USER\Software\Classes\AppUserModelId'
+        Remove-Item -LiteralPath "Registry::$regPath\$ToolkitName" -Force -Confirm:$false -ErrorAction Ignore
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'DisplayName', $ToastName, [Microsoft.Win32.RegistryValueKind]::String)
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'ShowInSettings', 0, [Microsoft.Win32.RegistryValueKind]::DWord)
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'IconUri', $ToastLogo, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+        [Microsoft.Win32.Registry]::SetValue("$regPath\$ToolkitName", 'IconBackgroundColor', [System.String]::Empty, [Microsoft.Win32.RegistryValueKind]::ExpandString)
 
         # Build out toast XML and display it.
-        (New-Variable -Name toastXml -Value ([Windows.Data.Xml.Dom.XmlDocument]::new()) -PassThru).Value.LoadXml('<toast launch="app-defined-string"><visual><binding template="ToastImageAndText02"><text id="1">$BalloonTipTitle</text><text id="2">$BalloonTipText</text><image id="1" src="file://$($adtConfig.Assets.Logo)" /></binding></visual></toast>')
-        [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('$($adtEnv.appDeployToolkitName)').Show((Get-Variable -Name toastXml -ValueOnly))
-    }))
+        $toastXml = [Windows.Data.Xml.Dom.XmlDocument]::new()
+        $toastXml.LoadXml("<toast launch=`"app-defined-string`"><visual><binding template=`"ToastImageAndText02`"><text id=`"1`">$([System.Security.SecurityElement]::Escape($ToastTitle))</text><text id=`"2`">$([System.Security.SecurityElement]::Escape($ToastText))</text><image id=`"1`" src=`"file://$ToastLogo`" /></binding></visual></toast>")
+        [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($ToolkitName).Show($toastXml)
+    }
+
+    # Initialise variables.
+    $adtEnv = Get-ADTEnvironment
+    $adtConfig = Get-ADTConfig
+
+    # Build out parameters for internal worker function.
+    $natnParams = [ordered]@{
+        ToolkitName = $adtEnv.appDeployToolkitName
+        ModuleBase = $Script:PSScriptRoot
+        ToastName = $adtConfig.UI.ToastName
+        ToastLogo = $adtConfig.Assets.Logo
+        ToastTitle = $BalloonTipTitle
+        ToastText = $BalloonTipText
+    }
 
     # If we're running as the active user, display directly; otherwise, run via Execute-ProcessAsUser.
-    if ($adtEnv.ProcessNTAccount -eq $adtEnv.runAsActiveUser.NTAccount)
-    {
-        Write-ADTLogEntry -Message "Displaying toast notification with message [$BalloonTipText]."
-        & $toastScriptBlock
-    }
-    else
+    if ($adtEnv.ProcessNTAccount -ne $adtEnv.runAsActiveUser.NTAccount)
     {
         Write-ADTLogEntry -Message "Displaying toast notification with message [$BalloonTipText] using Execute-ProcessAsUser."
-        Execute-ProcessAsUser -Path $adtEnv.envPSProcessPath -Parameters "-NonInteractive -NoProfile -NoLogo -WindowStyle Hidden -EncodedCommand $([System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes([System.String]::Join("`n", $toastScriptBlock.ToString().Trim().Split("`n").Trim()))))" -Wait -RunLevel LeastPrivilege
+        Execute-ProcessAsUser -Path $adtEnv.envPSProcessPath -Parameters "-NonInteractive -NoProfile -NoLogo -WindowStyle Hidden -EncodedCommand $(Out-ADTPowerShellEncodedCommand -Command "& {${Function:New-ADTToastNotification}} $(($natnParams | Resolve-ADTBoundParameters).Replace('"', '\"'))")" -Wait -RunLevel LeastPrivilege
+        return
     }
+    Write-ADTLogEntry -Message "Displaying toast notification with message [$BalloonTipText]."
+    New-ADTToastNotification @natnParams
 }
