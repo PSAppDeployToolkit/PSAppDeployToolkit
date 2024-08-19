ef65ac4701c8f227b7d788f8dc7830a48e07819a
Mitchell Richters <mjr4077au@gmail.com>
2024-08-19 19:49:39 +1000

    - Rework WIM mount/dismount code so that dismounting a WIM file mounted while an ADTSession is active dismounts everywhere the WIM file is mounted.
    If a PC is rebooted mid-way through a deployment, this setup will allow for the WIM file to be remounted at a new point and then for proper dismounting of the WIM file and all its mount points at the end.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Private/Get-ADTMountedWimFile.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Private/Get-ADTMountedWimFile.ps1
@@ -4,23 +4,24 @@
 #
 #---------------------------------------------------------------------------
 
-function Test-ADTMountedWimPath
+function Get-ADTMountedWimFile
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'ImagePath', Justification = "This parameter is used within delegates that PSScriptAnalyzer has no visibility of. See https://github.com/PowerShell/PSScriptAnalyzer/issues/1472 for more details.")]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'Path', Justification = "This parameter is used within delegates that PSScriptAnalyzer has no visibility of. See https://github.com/PowerShell/PSScriptAnalyzer/issues/1472 for more details.")]
     [CmdletBinding()]
-    [OutputType([System.Boolean])]
+    [OutputType([Microsoft.Dism.Commands.MountedImageInfoObject])]
     param
     (
         [Parameter(Mandatory = $true, ParameterSetName = 'ImagePath')]
         [ValidateNotNullOrEmpty()]
-        [System.String]$ImagePath,
+        [System.IO.FileInfo[]]$ImagePath,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'Path')]
         [ValidateNotNullOrEmpty()]
-        [System.String]$Path
+        [System.IO.DirectoryInfo[]]$Path
     )
 
     # Get the caller's provided input via the ParameterSetName so we can filter on its name and value.
     $parameter = & $Script:CommandTable.'Get-Variable' -Name $PSCmdlet.ParameterSetName
-    return !!(& $Script:CommandTable.'Get-WindowsImage' -Mounted | & { process { if ($_.($parameter.Name) -eq $parameter.Value) { return $_ } } })
+    return (& $Script:CommandTable.'Get-WindowsImage' -Mounted | & { process { if ($parameter.Value.FullName.Contains($_.($parameter.Name))) { return $_ } } })
 }
