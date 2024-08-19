9bf4b3e6e74f944f0fa33ef6252e6f23d5d253b2
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:55 -0400

    Updates to Comment-Based Help for Get-ADTRunAsActiveUser

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTRunAsActiveUser.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTRunAsActiveUser.ps1
@@ -7,12 +7,40 @@
 function Get-ADTRunAsActiveUser
 {
     <#
+    .SYNOPSIS
+        Retrieves the active user session information.
+
+    .DESCRIPTION
+        The Get-ADTRunAsActiveUser function determines the account that will be used to execute commands in the user session when the toolkit is running under the SYSTEM account.
+        The active console user will be chosen first. If no active console user is found, for multi-session operating systems, the first logged-on user will be used instead.
+
+    .INPUTS
+        None
+
+        This function does not take any pipeline input.
+
+    .OUTPUTS
+        PSADT.Types.UserSessionInfo
+
+        Returns a custom object containing the user session information.
+
+    .EXAMPLE
+        # Example 1
+        Get-ADTRunAsActiveUser
+
+        This example retrieves the active user session information.
 
     .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
 
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
     #>
-
     [CmdletBinding()]
     param
     (
