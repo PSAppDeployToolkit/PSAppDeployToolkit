7e0dc1ef5c1aa08f7f8481dedd3b58ff2db471ea
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:59 -0400

    Updates to Comment-Based Help for Test-ADTModuleInitialised

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Test-ADTModuleInitialised.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Test-ADTModuleInitialised.ps1
@@ -6,5 +6,40 @@
 
 function Test-ADTModuleInitialised
 {
+    <#
+    .SYNOPSIS
+        Checks if the ADT (PSAppDeployToolkit) module is initialized.
+
+    .DESCRIPTION
+        This function checks if the ADT (PSAppDeployToolkit) module is initialized by retrieving the module data and returning the initialization status.
+
+    .INPUTS
+        None
+
+        This function does not take any piped input.
+
+    .OUTPUTS
+        System.Boolean
+
+        Returns $true if the ADT module is initialized, otherwise $false.
+
+    .EXAMPLE
+        # Example 1
+        Test-ADTModuleInitialised
+
+        Checks if the ADT module is initialized and returns true or false.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     return (Get-ADTModuleData).Initialised
 }
