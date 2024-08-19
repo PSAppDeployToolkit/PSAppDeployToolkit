c2a35ac2c352b837199126d12bf3b798f1c5d33a
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:54 -0400

    Updates to Comment-Based Help for Get-ADTPendingReboot

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTPendingReboot.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTPendingReboot.ps1
@@ -7,63 +7,60 @@
 function Get-ADTPendingReboot
 {
     <#
-
     .SYNOPSIS
-    Get the pending reboot status on a local computer.
+        Get the pending reboot status on a local computer.
 
     .DESCRIPTION
-    Check WMI and the registry to determine if the system has a pending reboot operation from any of the following:
-    a) Component Based Servicing (Vista, Windows 2008)
-    b) Windows Update / Auto Update (XP, Windows 2003 / 2008)
-    c) SCCM 2012 Clients (DetermineIfRebootPending WMI method)
-    d) App-V Pending Tasks (global based Appv 5.0 SP2)
-    e) Pending File Rename Operations (XP, Windows 2003 / 2008)
+        Check WMI and the registry to determine if the system has a pending reboot operation from any of the following:
+        a) Component Based Servicing (Vista, Windows 2008)
+        b) Windows Update / Auto Update (XP, Windows 2003 / 2008)
+        c) SCCM 2012 Clients (DetermineIfRebootPending WMI method)
+        d) App-V Pending Tasks (global based Appv 5.0 SP2)
+        e) Pending File Rename Operations (XP, Windows 2003 / 2008)
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        This function does not take any pipeline input.
 
     .OUTPUTS
-    PSObject. Returns a custom object with the following properties:
-    - ComputerName
-    - LastBootUpTime
-    - IsSystemRebootPending
-    - IsCBServicingRebootPending
-    - IsWindowsUpdateRebootPending
-    - IsSCCMClientRebootPending
-    - IsFileRenameRebootPending
-    - PendingFileRenameOperations
-    - ErrorMsg
+        PSADT.Types.RebootInfo
+
+        Returns a custom object with the following properties:
+        - ComputerName
+        - LastBootUpTime
+        - IsSystemRebootPending
+        - IsCBServicingRebootPending
+        - IsWindowsUpdateRebootPending
+        - IsSCCMClientRebootPending
+        - IsFileRenameRebootPending
+        - PendingFileRenameOperations
+        - ErrorMsg
 
     .EXAMPLE
-    Get-ADTPendingReboot
+        # Example 1
+        Get-ADTPendingReboot
 
-    Returns custom object with following properties:
-    - ComputerName
-    - LastBootUpTime
-    - IsSystemRebootPending
-    - IsCBServicingRebootPending
-    - IsWindowsUpdateRebootPending
-    - IsSCCMClientRebootPending
-    - IsFileRenameRebootPending
-    - PendingFileRenameOperations
-    - ErrorMsg
+        This example retrieves the pending reboot status on the local computer and returns a custom object with detailed information.
 
-    .EXAMPLE
-    (Get-ADTPendingReboot).IsSystemRebootPending
+        # Example 2
+        (Get-ADTPendingReboot).IsSystemRebootPending
 
-    Returns boolean value determining whether or not there is a pending reboot operation.
+        This example returns a boolean value determining whether or not there is a pending reboot operation.
 
     .NOTES
-    ErrorMsg only contains something if an error occurred
+        ErrorMsg only contains something if an error occurred.
 
-    .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
-
     [CmdletBinding()]
     [OutputType([PSADT.Types.RebootInfo])]
     param
