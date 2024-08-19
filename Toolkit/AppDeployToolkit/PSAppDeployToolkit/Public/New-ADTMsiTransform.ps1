170e2ffa8b90841290482e33af505ab38c4208b4
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:56 -0400

    Updates to Comment-Based Help for New-ADTMsiTransform

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/New-ADTMsiTransform.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/New-ADTMsiTransform.ps1
@@ -7,51 +7,70 @@
 function New-ADTMsiTransform
 {
     <#
-
     .SYNOPSIS
-    Create a transform file for an MSI database.
+        Create a transform file for an MSI database.
 
     .DESCRIPTION
-    Create a transform file for an MSI database and create/modify properties in the Properties table.
+        Create a transform file for an MSI database and create/modify properties in the Properties table. This function allows you to specify an existing transform to apply before making changes and to define the path for the new transform file. If the new transform file already exists, it will be deleted before creating a new one.
 
     .PARAMETER MsiPath
-    Specify the path to an MSI file.
+        Specify the path to an MSI file.
+
+        Mandatory: True
 
     .PARAMETER ApplyTransformPath
-    Specify the path to a transform which should be applied to the MSI database before any new properties are created or modified.
+        Specify the path to a transform which should be applied to the MSI database before any new properties are created or modified.
+
+        Mandatory: False
 
     .PARAMETER NewTransformPath
-    Specify the path where the new transform file with the desired properties will be created. If a transform file of the same name already exists, it will be deleted before a new one is created.
+        Specify the path where the new transform file with the desired properties will be created. If a transform file of the same name already exists, it will be deleted before a new one is created.
 
-    Default is: a) If -ApplyTransformPath was specified but not -NewTransformPath, then <ApplyTransformPath>.new.mst
-                b) If only -MsiPath was specified, then <MsiPath>.mst
+        Default is:
+        a) If -ApplyTransformPath was specified but not -NewTransformPath, then <ApplyTransformPath>.new.mst
+        b) If only -MsiPath was specified, then <MsiPath>.mst
+
+        Mandatory: False
 
     .PARAMETER TransformProperties
-    Hashtable which contains calls to Set-ADTMsiProperty for configuring the desired properties which should be included in new transform file.
+        Hashtable which contains calls to Set-ADTMsiProperty for configuring the desired properties which should be included in the new transform file.
 
-    Example hashtable: [Hashtable]$TransformProperties = @{ 'ALLUSERS' = '1' }
+        Example hashtable: [Hashtable]$TransformProperties = @{ 'ALLUSERS' = '1' }
+
+        Mandatory: True
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        You cannot pipe objects to this function.
 
     .OUTPUTS
-    None. This function does not generate any output.
+        None
+
+        This function does not generate any output.
 
     .EXAMPLE
-    New-ADTMsiTransform -MsiPath 'C:\Temp\PSADTInstall.msi' -TransformProperties @{
-        'ALLUSERS' = '1'
-        'AgreeToLicense' = 'Yes'
-        'REBOOT' = 'ReallySuppress'
-        'RebootYesNo' = 'No'
-        'ROOTDRIVE' = 'C:'
-    }
+        # Example 1
+        New-ADTMsiTransform -MsiPath 'C:\Temp\PSADTInstall.msi' -TransformProperties @{
+            'ALLUSERS' = '1'
+            'AgreeToLicense' = 'Yes'
+            'REBOOT' = 'ReallySuppress'
+            'RebootYesNo' = 'No'
+            'ROOTDRIVE' = 'C:'
+        }
+
+        Creates a new transform file for the specified MSI with the given properties.
 
     .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = "This function does not change system state.")]
