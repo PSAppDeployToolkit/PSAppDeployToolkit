8350392e09cc37689a3858496a061cb0727518d7
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:53 -0400

    Updates to Comment-Based Help for Add-ADTSessionOpeningCallback

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTSessionOpeningCallback.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTSessionOpeningCallback.ps1
@@ -6,6 +6,45 @@
 
 function Add-ADTSessionOpeningCallback
 {
+    <#
+    .SYNOPSIS
+        Adds a callback to be executed when the ADT session is opening.
+
+    .DESCRIPTION
+        The Add-ADTSessionOpeningCallback function registers a callback command to be executed when the ADT session is opening. This function sends the callback to the backend function for processing.
+
+    .PARAMETER Callback
+        The callback command(s) to be executed when the ADT session is opening.
+
+        Mandatory: True
+
+    .INPUTS
+        System.Management.Automation.CommandInfo[]
+
+        The callback command(s) to be executed when the ADT session is opening.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        # Example 1
+        Add-ADTSessionOpeningCallback -Callback $myCallback
+
+        This example adds the specified callback to be executed when the ADT session is opening.
+
+    .NOTES
+        An active ADT session is required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
     [CmdletBinding()]
     param
     (
