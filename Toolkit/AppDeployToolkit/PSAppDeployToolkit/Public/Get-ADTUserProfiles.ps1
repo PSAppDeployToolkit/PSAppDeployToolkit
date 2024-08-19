f46f0a3be7284a903ad5246d06832bf564b5a0c3
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:55 -0400

    Updates to Comment-Based Help for Get-ADTUserProfiles

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTUserProfiles.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTUserProfiles.ps1
@@ -7,51 +7,74 @@
 function Get-ADTUserProfiles
 {
     <#
-
     .SYNOPSIS
-    Get the User Profile Path, User Account SID, and the User Account Name for all users that log onto the machine and also the Default User (which does not log on).
+        Get the User Profile Path, User Account SID, and the User Account Name for all users that log onto the machine and also the Default User.
 
     .DESCRIPTION
-    Get the User Profile Path, User Account SID, and the User Account Name for all users that log onto the machine and also the Default User (which does  not log on).
-
-    Please note that the NTAccount property may be empty for some user profiles but the SID and ProfilePath properties will always be populated.
+        Get the User Profile Path, User Account SID, and the User Account Name for all users that log onto the machine and also the Default User (which does not log on).
+        Please note that the NTAccount property may be empty for some user profiles but the SID and ProfilePath properties will always be populated.
 
     .PARAMETER ExcludeNTAccount
-    Specify NT account names in DOMAIN\username format to exclude from the list of user profiles.
+        Specify NT account names in DOMAIN\username format to exclude from the list of user profiles.
+
+        Mandatory: False
 
     .PARAMETER IncludeSystemProfiles
-    Include system profiles: SYSTEM, LOCAL SERVICE, NETWORK SERVICE. Default is: $false.
+        Include system profiles: SYSTEM, LOCAL SERVICE, NETWORK SERVICE. Default is: $false.
+
+        Mandatory: False
 
     .PARAMETER IncludeServiceProfiles
-    Include service profiles where NTAccount begins with NT SERVICE. Default is: $false.
+        Include service profiles where NTAccount begins with NT SERVICE. Default is: $false.
+
+        Mandatory: False
 
     .PARAMETER ExcludeDefaultUser
-    Exclude the Default User. Default is: $false.
+        Exclude the Default User. Default is: $false.
+
+        Mandatory: False
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        You cannot pipe objects to this function.
 
     .OUTPUTS
-    PSADT.Types.UserProfile. Returns a PSADT.Types.UserProfile object with the following properties: NTAccount, SID, ProfilePath
+        PSADT.Types.UserProfile
+
+        Returns a PSADT.Types.UserProfile object with the following properties:
+        - NTAccount
+        - SID
+        - ProfilePath
 
     .EXAMPLE
-    # Return the following properties for each user profile on the system: NTAccount, SID, ProfilePath
-    Get-ADTUserProfiles
+        # Example 1
+        Get-ADTUserProfiles
+
+        Return the following properties for each user profile on the system: NTAccount, SID, ProfilePath.
 
     .EXAMPLE
-    # Return the following properties for each user profile on the system, except for 'Robot' and 'ntadmin': NTAccount, SID, ProfilePath
-    Get-ADTUserProfiles -ExcludeNTAccount CONTOSO\Robot,CONTOSO\ntadmin
+        # Example 2
+        Get-ADTUserProfiles -ExcludeNTAccount CONTOSO\Robot,CONTOSO\ntadmin
+
+        Return the following properties for each user profile on the system, except for 'Robot' and 'ntadmin': NTAccount, SID, ProfilePath.
 
     .EXAMPLE
-    # Return the user profile path for each user on the system. This information can then be used to make modifications under the user profile on the filesystem.
-    [string[]]$ProfilePaths = Get-ADTUserProfiles | Select-Object -ExpandProperty ProfilePath
+        # Example 3
+        [string[]]$ProfilePaths = Get-ADTUserProfiles | Select-Object -ExpandProperty ProfilePath
+
+        Return the user profile path for each user on the system. This information can then be used to make modifications under the user profile on the filesystem.
 
     .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = "This function is appropriately named and we don't need PSScriptAnalyzer telling us otherwise.")]
