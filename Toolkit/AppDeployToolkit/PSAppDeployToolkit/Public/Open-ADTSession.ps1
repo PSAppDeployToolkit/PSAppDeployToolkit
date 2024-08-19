3e4908147cb6f23514347a6491910cf784a11381
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:56 -0400

    Updates to Comment-Based Help for Open-ADTSession

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Open-ADTSession.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Open-ADTSession.ps1
@@ -6,6 +6,177 @@
 
 function Open-ADTSession
 {
+    <#
+    .SYNOPSIS
+        Opens a new ADT session.
+
+    .DESCRIPTION
+        This function initializes and opens a new ADT session with the specified parameters. It handles the setup of the session environment and processes any callbacks defined for the session. If the session fails to open, it handles the error and closes the session if necessary.
+
+    .PARAMETER SessionState
+        Caller's SessionState.
+
+        Mandatory: True
+
+    .PARAMETER DeploymentType
+        Deploy-Application.ps1 Parameter. Specifies the type of deployment: Install, Uninstall, or Repair.
+
+        Mandatory: False
+
+    .PARAMETER DeployMode
+        Deploy-Application.ps1 Parameter. Specifies the deployment mode: Interactive, NonInteractive, or Silent.
+
+        Mandatory: False
+
+    .PARAMETER AllowRebootPassThru
+        Deploy-Application.ps1 Parameter. Allows reboot pass-through.
+
+        Mandatory: False
+
+    .PARAMETER TerminalServerMode
+        Deploy-Application.ps1 Parameter. Enables Terminal Server mode.
+
+        Mandatory: False
+
+    .PARAMETER DisableLogging
+        Deploy-Application.ps1 Parameter. Disables logging for the session.
+
+        Mandatory: False
+
+    .PARAMETER AppVendor
+        Deploy-Application.ps1 Parameter. Specifies the application vendor.
+
+        Mandatory: False
+
+    .PARAMETER AppName
+        Deploy-Application.ps1 Parameter. Specifies the application name.
+
+        Mandatory: False
+
+    .PARAMETER AppVersion
+        Deploy-Application.ps1 Parameter. Specifies the application version.
+
+        Mandatory: False
+
+    .PARAMETER AppArch
+        Deploy-Application.ps1 Parameter. Specifies the application architecture.
+
+        Mandatory: False
+
+    .PARAMETER AppLang
+        Deploy-Application.ps1 Parameter. Specifies the application language.
+
+        Mandatory: False
+
+    .PARAMETER AppRevision
+        Deploy-Application.ps1 Parameter. Specifies the application revision.
+
+        Mandatory: False
+
+    .PARAMETER AppExitCodes
+        Deploy-Application.ps1 Parameter. Specifies the application exit codes.
+
+        Mandatory: False
+
+    .PARAMETER AppRebootCodes
+        Deploy-Application.ps1 Parameter. Specifies the application reboot codes.
+
+        Mandatory: False
+
+    .PARAMETER AppScriptVersion
+        Deploy-Application.ps1 Parameter. Specifies the application script version.
+
+        Mandatory: False
+
+    .PARAMETER AppScriptDate
+        Deploy-Application.ps1 Parameter. Specifies the application script date.
+
+        Mandatory: False
+
+    .PARAMETER AppScriptAuthor
+        Deploy-Application.ps1 Parameter. Specifies the application script author.
+
+        Mandatory: False
+
+    .PARAMETER DefaultMsiFile
+        Deploy-Application.ps1 Parameter. Specifies the default MSI file.
+
+        Mandatory: False
+
+    .PARAMETER DefaultMstFile
+        Deploy-Application.ps1 Parameter. Specifies the default MST file.
+
+        Mandatory: False
+
+    .PARAMETER DefaultMspFiles
+        Deploy-Application.ps1 Parameter. Specifies the default MSP files.
+
+        Mandatory: False
+
+    .PARAMETER InstallName
+        Deploy-Application.ps1 Parameter. Specifies the install name.
+
+        Mandatory: False
+
+    .PARAMETER InstallTitle
+        Deploy-Application.ps1 Parameter. Specifies the install title.
+
+        Mandatory: False
+
+    .PARAMETER DeployAppScriptFriendlyName
+        Deploy-Application.ps1 Parameter. Specifies the friendly name of the deploy application script.
+
+        Mandatory: False
+
+    .PARAMETER DeployAppScriptVersion
+        Deploy-Application.ps1 Parameter. Specifies the version of the deploy application script.
+
+        Mandatory: False
+
+    .PARAMETER DeployAppScriptDate
+        Deploy-Application.ps1 Parameter. Specifies the date of the deploy application script.
+
+        Mandatory: False
+
+    .PARAMETER DeployAppScriptParameters
+        Deploy-Application.ps1 Parameter. Specifies the parameters for the deploy application script.
+
+        Mandatory: False
+
+    .PARAMETER PassThru
+        Deploy-Application.ps1 Parameter. Passes the session object through the pipeline.
+
+        Mandatory: False
+
+    .INPUTS
+        None
+
+        This function does not take any pipeline input.
+
+    .OUTPUTS
+        System.Object
+
+        This function returns the session object if -PassThru is specified.
+
+    .EXAMPLE
+        # Example 1
+        $sessionState = Get-SessionState
+        Open-ADTSession -SessionState $sessionState -DeploymentType "Install" -DeployMode "Interactive"
+
+        Opens a new ADT session with the specified parameters.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     [CmdletBinding()]
     param
     (
