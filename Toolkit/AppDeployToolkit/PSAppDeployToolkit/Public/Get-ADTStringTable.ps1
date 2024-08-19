8bd3b1e11bd2ea447671b3b87a8322765014879a
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:55 -0400

    Updates to Comment-Based Help for Get-ADTStringTable

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTStringTable.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTStringTable.ps1
@@ -6,6 +6,43 @@
 
 function Get-ADTStringTable
 {
+    <#
+    .SYNOPSIS
+        Retrieves the string database from the ADT module.
+
+    .DESCRIPTION
+        The Get-ADTStringTable function returns the string database if it has been initialized. If the string database is not initialized, it throws an error indicating that Initialize-ADTModule should be called before using this function.
+
+    .INPUTS
+        None
+
+        This function does not take any pipeline input.
+
+    .OUTPUTS
+        System.Collections.Hashtable
+
+        Returns a hashtable containing the string database.
+
+    .EXAMPLE
+        # Example 1
+        Get-ADTStringTable
+
+        This example retrieves the string database from the ADT module.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Requires: The module should be initialized using Initialize-ADTModule
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     [CmdletBinding()]
     param
     (
