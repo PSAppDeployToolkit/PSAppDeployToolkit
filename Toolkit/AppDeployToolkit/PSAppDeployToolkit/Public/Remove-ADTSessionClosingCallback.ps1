68672bab487f68c318350a25db26ffa62b2d8216
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:57 -0400

    Updates to Comment-Based Help for Remove-ADTSessionClosingCallback

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Remove-ADTSessionClosingCallback.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Remove-ADTSessionClosingCallback.ps1
@@ -4,6 +4,46 @@
 #
 #---------------------------------------------------------------------------
 
+<#
+    .SYNOPSIS
+        Removes a callback function from the ADT session closing event.
+
+    .DESCRIPTION
+        This function removes a specified callback function from the ADT session closing event. The callback function must be provided as a parameter. If the operation fails, it throws a terminating error.
+
+    .PARAMETER Callback
+        The callback function to remove from the ADT session closing event.
+
+        Mandatory: True
+
+    .INPUTS
+        System.Management.Automation.CommandInfo[]
+
+        An array of CommandInfo objects representing the callback functions to be removed.
+
+    .OUTPUTS
+        None
+
+        This function does not generate any output.
+
+    .EXAMPLE
+        # Example 1
+        Remove-ADTSessionClosingCallback -Callback (Get-Command -Name 'MyCallbackFunction')
+
+        Removes the specified callback function from the ADT session closing event.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+#>
+
 function Remove-ADTSessionClosingCallback
 {
     [CmdletBinding()]
