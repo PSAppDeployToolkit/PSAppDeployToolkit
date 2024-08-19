0cb26a5ad908ceb03975a2b421bcb85348c9e320
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:58 -0400

    Updates to Comment-Based Help for Show-ADTHelpConsole

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Show-ADTHelpConsole.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Show-ADTHelpConsole.ps1
@@ -7,10 +7,38 @@
 function Show-ADTHelpConsole
 {
     <#
+    .SYNOPSIS
+        Displays a help console for the ADT module.
+
+    .DESCRIPTION
+        Displays a help console for the ADT module in a new PowerShell window. The console provides a graphical interface to browse and view detailed help information for all commands exported by the ADT module. The help console includes a list box to select commands and a text box to display the full help content for the selected command.
+
+    .INPUTS
+        None
+
+        This function does not take any piped input.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        # Example 1
+        Show-ADTHelpConsole
+
+        Opens a new PowerShell window displaying the help console for the ADT module.
 
     .NOTES
-    This function can be called without an active ADT session.
+        This function can be called without an active ADT session.
 
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
     #>
 
     # Run this via a new PowerShell window so it doesn't stall the main thread.
