72e832c5c5c250561126bd68e7f3c51ceb8a3afd
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:55 -0400

    Updates to Comment-Based Help for Get-ADTShortcut

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTShortcut.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTShortcut.ps1
@@ -7,45 +7,55 @@
 function Get-ADTShortcut
 {
     <#
-
     .SYNOPSIS
-    Get information from a new .lnk or .url type shortcut
+        Get information from a .lnk or .url type shortcut.
 
     .DESCRIPTION
-    Get information from a new .lnk or .url type shortcut. Returns a hashtable.
+        Get information from a .lnk or .url type shortcut. Returns a hashtable with details about the shortcut such as TargetPath, Arguments, Description, and more.
 
     .PARAMETER Path
-    Path to the shortcut to get information from.
+        Path to the shortcut to get information from.
+
+        Mandatory: True
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        You cannot pipe objects to this function.
 
     .OUTPUTS
-    System.Collections.Hashtable. Returns a hashtable with the following keys:
-    - TargetPath
-    - Arguments
-    - Description
-    - WorkingDirectory
-    - WindowStyle
-    - Hotkey
-    - IconLocation
-    - IconIndex
-    - RunAsAdmin
+        System.Collections.Hashtable
+
+        Returns a hashtable with the following keys:
+        - TargetPath
+        - Arguments
+        - Description
+        - WorkingDirectory
+        - WindowStyle
+        - Hotkey
+        - IconLocation
+        - IconIndex
+        - RunAsAdmin
 
     .EXAMPLE
-    Get-ADTShortcut -Path "$envProgramData\Microsoft\Windows\Start Menu\My Shortcut.lnk"
+        # Example 1
+        Get-ADTShortcut -Path "$envProgramData\Microsoft\Windows\Start Menu\My Shortcut.lnk"
+
+        Retrieves information from the specified .lnk shortcut.
 
     .NOTES
-    Url shortcuts only support TargetPath, IconLocation and IconIndex.
+        Url shortcuts only support TargetPath, IconLocation, and IconIndex.
 
-    .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
-
     [CmdletBinding()]
     [OutputType([PSADT.Types.ShortcutUrl])]
     [OutputType([PSADT.Types.ShortcutLnk])]
