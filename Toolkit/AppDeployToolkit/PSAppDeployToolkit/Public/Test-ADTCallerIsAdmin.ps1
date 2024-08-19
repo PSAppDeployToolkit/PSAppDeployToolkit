4d2048b25e01d2bdef81d61ba519079f35cdf6df
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:59 -0400

    Updates to Comment-Based Help for Test-ADTCallerIsAdmin

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Test-ADTCallerIsAdmin.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Test-ADTCallerIsAdmin.ps1
@@ -7,11 +7,39 @@
 function Test-ADTCallerIsAdmin
 {
     <#
+    .SYNOPSIS
+        Checks if the current user has administrative privileges.
 
-	.NOTES
-	This function can be called without an active ADT session.
+    .DESCRIPTION
+        This function checks if the current user is a member of the Administrators group. It returns a boolean value indicating whether the user has administrative privileges.
 
-	#>
+    .INPUTS
+        None
+
+        This function does not take any piped input.
+
+    .OUTPUTS
+        System.Boolean
+
+        Returns $true if the current user is an administrator, otherwise $false.
+
+    .EXAMPLE
+        # Example 1
+        Test-ADTCallerIsAdmin
+
+        Checks if the current user has administrative privileges and returns true or false.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
 
     return [System.Security.Principal.WindowsPrincipal]::new([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltinRole]::Administrator)
 }
