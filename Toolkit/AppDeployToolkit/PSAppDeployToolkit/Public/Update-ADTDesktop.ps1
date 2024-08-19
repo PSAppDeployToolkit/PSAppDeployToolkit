e353d02aa2433f8b3189c12e4c6307c00699fa66
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:02:00 -0400

    Updates to Comment-Based Help for Update-ADTDesktop

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Update-ADTDesktop.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Update-ADTDesktop.ps1
@@ -7,28 +7,38 @@
 function Update-ADTDesktop
 {
     <#
-
     .SYNOPSIS
-    Refresh the Windows Explorer Shell, which causes the desktop icons and the environment variables to be reloaded.
+        Refresh the Windows Explorer Shell, which causes the desktop icons and the environment variables to be reloaded.
 
     .DESCRIPTION
-    Refresh the Windows Explorer Shell, which causes the desktop icons and the environment variables to be reloaded.
+        This function refreshes the Windows Explorer Shell, causing the desktop icons and environment variables to be reloaded. This can be useful after making changes that affect the desktop or environment variables, ensuring that the changes are reflected immediately.
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        This function does not take any piped input.
 
     .OUTPUTS
-    None. This function does not return objects.
+        None
+
+        This function does not return any objects.
 
     .EXAMPLE
-    Update-ADTDesktop
+        # Example 1
+        Update-ADTDesktop
+
+        Refreshes the Windows Explorer Shell, reloading the desktop icons and environment variables.
 
     .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [CmdletBinding()]
