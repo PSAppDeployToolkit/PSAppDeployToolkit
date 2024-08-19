b7cca5461ba0b458025b71b41a52fe4784541b71
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:55 -0400

    Updates to Comment-Based Help for Get-ADTSession

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTSession.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Get-ADTSession.ps1
@@ -6,6 +6,43 @@
 
 function Get-ADTSession
 {
+    <#
+    .SYNOPSIS
+        Retrieves the most recent ADT session.
+
+    .DESCRIPTION
+        The Get-ADTSession function returns the most recent session from the ADT module data. If no sessions are found, it throws an error indicating that an ADT session should be opened using Open-ADTSession before calling this function.
+
+    .INPUTS
+        None
+
+        This function does not take any pipeline input.
+
+    .OUTPUTS
+        PSADT.Types.Session
+
+        Returns the most recent session object from the ADT module data.
+
+    .EXAMPLE
+        # Example 1
+        Get-ADTSession
+
+        This example retrieves the most recent ADT session.
+
+    .NOTES
+        An active ADT session is required to use this function.
+
+        Requires: PSADT session should be initialized using Open-ADTSession
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+
+    #>
     [CmdletBinding()]
     param
     (
