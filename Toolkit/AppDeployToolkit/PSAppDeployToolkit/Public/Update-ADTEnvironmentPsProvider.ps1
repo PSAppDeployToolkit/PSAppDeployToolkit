42c9c2216a99a46f7fd5023a2a965ba51d3c6d4e
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:02:00 -0400

    Updates to Comment-Based Help for Update-ADTEnvironmentPsProvider

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Update-ADTEnvironmentPsProvider.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Update-ADTEnvironmentPsProvider.ps1
@@ -7,33 +7,44 @@
 function Update-ADTEnvironmentPsProvider
 {
     <#
-
     .SYNOPSIS
-    Updates the environment variables for the current PowerShell session with any environment variable changes that may have occurred during script execution.
+        Updates the environment variables for the current PowerShell session with any environment variable changes that may have occurred during script execution.
 
     .DESCRIPTION
-    Environment variable changes that take place during script execution are not visible to the current PowerShell session.
-
-    Use this function to refresh the current PowerShell session with all environment variable settings.
+        Environment variable changes that take place during script execution are not visible to the current PowerShell session.
+        Use this function to refresh the current PowerShell session with all environment variable settings.
 
     .PARAMETER LoadLoggedOnUserEnvironmentVariables
-    If script is running in SYSTEM context, this option allows loading environment variables from the active console user. If no console user exists but users are logged in, such as on terminal servers, then the first logged-in non-console user.
+        If script is running in SYSTEM context, this option allows loading environment variables from the active console user. If no console user exists but users are logged in, such as on terminal servers, then the first logged-in non-console user.
+
+        Mandatory: False
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        This function does not take any piped input.
 
     .OUTPUTS
-    None. This function does not return objects.
+        None
+
+        This function does not return any objects.
 
     .EXAMPLE
-    Update-ADTEnvironmentPsProvider
+        # Example 1
+        Update-ADTEnvironmentPsProvider
+
+        Refreshes the current PowerShell session with all environment variable settings.
 
     .NOTES
-    This function can be called without an active ADT session.
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [CmdletBinding()]
