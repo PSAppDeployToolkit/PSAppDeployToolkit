13a2d6067cbdc66b61e6d8a1d6210c2fb3c136fc
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:53 -0400

    Updates to Comment-Based Help for Add-ADTEdgeExtension

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTEdgeExtension.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTEdgeExtension.ps1
@@ -7,43 +7,63 @@
 function Add-ADTEdgeExtension
 {
     <#
-
     .SYNOPSIS
-    Adds an extension for Microsoft Edge using the ExtensionSettings policy
+        Adds an extension for Microsoft Edge using the ExtensionSettings policy.
 
     .DESCRIPTION
-    This function adds an extension for Microsoft Edge using the ExtensionSettings policy: https://learn.microsoft.com/en-us/deployedge/microsoft-edge-manage-extensions-ref-guide.
+        This function adds an extension for Microsoft Edge using the ExtensionSettings policy: https://learn.microsoft.com/en-us/deployedge/microsoft-edge-manage-extensions-ref-guide.
 
-    This enables Edge Extensions to be installed and managed like applications, enabling extensions to be pushed to specific devices or users alongside existing GPO/Intune extension policies.
+        This enables Edge Extensions to be installed and managed like applications, enabling extensions to be pushed to specific devices or users alongside existing GPO/Intune extension policies.
 
-    This should not be used in conjunction with Edge Management Service which leverages the same registry key to configure Edge extensions.
+        This should not be used in conjunction with Edge Management Service which leverages the same registry key to configure Edge extensions.
 
     .PARAMETER ExtensionID
-    The ID of the extension to add.
+        The ID of the extension to add.
+
+        Mandatory: True
 
     .PARAMETER UpdateUrl
-    The update URL of the extension. This is the URL where the extension will check for updates.
+        The update URL of the extension. This is the URL where the extension will check for updates.
+
+        Mandatory: True
 
     .PARAMETER InstallationMode
-    The installation mode of the extension. Allowed values: blocked, allowed, removed, force_installed, normal_installed
+        The installation mode of the extension. Allowed values: blocked, allowed, removed, force_installed, normal_installed.
+
+        Mandatory: True
 
     .PARAMETER MinimumVersionRequired
-    The minimum version of the extension required for installation.
+        The minimum version of the extension required for installation.
+
+        Mandatory: False
+
+    .INPUTS
+        System.String
+
+        The function takes the extension ID, update URL, installation mode, and optionally the minimum version required.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
 
     .EXAMPLE
-    Add-ADTEdgeExtension -Add -ExtensionID "extensionID" -InstallationMode "force_installed" -UpdateUrl "https://edge.microsoft.com/extensionwebstorebase/v1/crx"
+        # Example 1
+        Add-ADTEdgeExtension -ExtensionID "extensionID" -InstallationMode "force_installed" -UpdateUrl "https://edge.microsoft.com/extensionwebstorebase/v1/crx"
+
+        This example adds the specified extension to be force installed in Microsoft Edge.
 
     .NOTES
-    This function is provided as a template to add an extension for Microsoft Edge. This should not be used in conjunction with Edge Management Service which leverages the same registry key to configure Edge extensions.
+        An active ADT session is NOT required to use this function.
 
-    .NOTES
-    This function can be called without an active ADT session.
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
-
     [CmdletBinding()]
     param
     (
