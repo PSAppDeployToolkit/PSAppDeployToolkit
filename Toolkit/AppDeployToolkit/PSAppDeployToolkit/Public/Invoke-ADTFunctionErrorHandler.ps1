3d91cfc2034c08131d7f90bb8ba92ed610b7dcff
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:56 -0400

    Updates to Comment-Based Help for Invoke-ADTFunctionErrorHandler

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Invoke-ADTFunctionErrorHandler.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Invoke-ADTFunctionErrorHandler.ps1
@@ -6,6 +6,77 @@
 
 function Invoke-ADTFunctionErrorHandler
 {
+    <#
+    .SYNOPSIS
+        Handles errors within ADT functions by logging and optionally passing through the error.
+
+    .DESCRIPTION
+        This function handles errors within ADT functions by logging the error message and optionally passing through the error record. It recovers the true ErrorActionPreference set by the caller and sets it within the function. If a log message is provided, it appends the resolved error record to the log message. Depending on the ErrorActionPreference, it either throws a terminating error or writes a non-terminating error.
+
+    .PARAMETER Cmdlet
+        The cmdlet that is calling this function.
+
+        Mandatory: True
+
+    .PARAMETER SessionState
+        The session state of the calling cmdlet.
+
+        Mandatory: True
+
+    .PARAMETER ErrorRecord
+        The error record to handle.
+
+        Mandatory: True
+
+    .PARAMETER PassThru
+        If specified, the function will return the error record.
+
+        Mandatory: False
+
+    .PARAMETER LogMessage
+        A custom log message to be written along with the error.
+
+        Mandatory: True (when using the 'LogMessage' parameter set)
+
+    .PARAMETER DisableErrorResolving
+        If specified, the function will not append the resolved error record to the log message.
+
+        Mandatory: False (when using the 'LogMessage' parameter set)
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        System.Management.Automation.ErrorRecord
+
+        Returns the error record if PassThru is specified.
+
+    .EXAMPLE
+        # Example 1
+        Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_
+
+        Handles the error within the calling cmdlet and logs it.
+
+    .EXAMPLE
+        # Example 2
+        Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_ -LogMessage "An error occurred" -DisableErrorResolving
+
+        Handles the error within the calling cmdlet, logs a custom message without resolving the error record, and logs it.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     [CmdletBinding(DefaultParameterSetName = 'None')]
     [OutputType([System.Management.Automation.ErrorRecord])]
     param
