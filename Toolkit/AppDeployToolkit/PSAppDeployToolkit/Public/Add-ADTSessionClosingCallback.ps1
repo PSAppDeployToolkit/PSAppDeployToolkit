c469015a6ee61e8923b7bec3db1a1ca0d679265c
Dan Cunningham <dancunningham@me.com>
2024-08-18 23:01:53 -0400

    Updates to Comment-Based Help for Add-ADTSessionClosingCallback

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTSessionClosingCallback.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Public/Add-ADTSessionClosingCallback.ps1
@@ -6,6 +6,46 @@
 
 function Add-ADTSessionClosingCallback
 {
+    <#
+    .SYNOPSIS
+        Adds a callback to be executed when the ADT session is closing.
+
+    .DESCRIPTION
+        The Add-ADTSessionClosingCallback function registers a callback command to be executed when the ADT session is closing. This function sends the callback to the backend function for processing.
+
+    .PARAMETER Callback
+        The callback command(s) to be executed when the ADT session is closing.
+
+        Mandatory: True
+
+    .INPUTS
+        System.Management.Automation.CommandInfo[]
+
+        The callback command(s) to be executed when the ADT session is closing.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        # Example 1
+        Add-ADTSessionClosingCallback -Callback $myCallback
+
+        This example adds the specified callback to be executed when the ADT session is closing.
+
+    .NOTES
+        An active ADT session is required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     [CmdletBinding()]
     param
     (
