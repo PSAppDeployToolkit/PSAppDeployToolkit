35c20168b4e1a9d2048be2e67293f198d0a02e84
Mitchell Richters <mjr4077au@gmail.com>
2024-09-10 10:07:31 +1000

    - Fix missed `-InformationAction` change from "- Fix InformationAction value within Zero-Config MSI code.".

--- src/PSAppDeployToolkit/Classes/ADTSession.ps1
+++ src/PSAppDeployToolkit/Classes/ADTSession.ps1
@@ -216,7 +216,7 @@ class ADTSession
         # Mount the WIM file and reset DirFiles to the mount point.
         $this.WriteZeroConfigDivider()
         $this.WriteLogEntry("Discovered Zero-Config WIM file [$wimFile].")
-        & $Script:CommandTable.'Mount-ADTWimFile' -ImagePath $wimFile -Path ($this.DirFiles = [System.IO.Path]::Combine($this.DirFiles, [System.IO.Path]::GetRandomFileName())) -Index 1 -InformationAction Ignore
+        & $Script:CommandTable.'Mount-ADTWimFile' -ImagePath $wimFile -Path ($this.DirFiles = [System.IO.Path]::Combine($this.DirFiles, [System.IO.Path]::GetRandomFileName())) -Index 1 6>$null
         $this.WriteLogEntry("Successfully mounted WIM file to [$($this.DirFiles)].")
         $this.WriteLogEntry("Using [$($this.DirFiles)] as the base DirFiles directory.")
     }
