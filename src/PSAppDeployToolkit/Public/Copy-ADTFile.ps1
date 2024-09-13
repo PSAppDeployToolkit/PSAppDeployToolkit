aaba14975c348e83397550309350001b54791d42
Dan Gough <goughdj@gmail.com>
2024-09-13 14:31:42 +0100

    Set dynamic param $RobocopyAdditionalParams to $null if not supplied

--- src/PSAppDeployToolkit/Public/Copy-ADTFile.ps1
+++ src/PSAppDeployToolkit/Public/Copy-ADTFile.ps1
@@ -167,6 +167,9 @@ function Copy-ADTFile
                     {
                         $PSBoundParameters.RobocopyAdditionalParams
                     }
+                    else {
+                        $null
+                    }
                 }
             }
             else
