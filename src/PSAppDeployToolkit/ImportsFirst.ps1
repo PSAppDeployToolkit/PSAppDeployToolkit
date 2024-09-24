3336c13df365921899d27a5832043b951b047772
Mitchell Richters <mjr4077au@gmail.com>
2024-09-24 18:39:00 +1000

    - Fix formatting issues stemming from "- Slightly cleaner setup for initialising read-only collections within module init code.".

--- src/PSAppDeployToolkit/ImportsFirst.ps1
+++ src/PSAppDeployToolkit/ImportsFirst.ps1
@@ -15,15 +15,15 @@ $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyC
 
 # Define modules needed to build out CommandTable.
 $RequiredModules = [System.Collections.ObjectModel.ReadOnlyCollection[Microsoft.PowerShell.Commands.ModuleSpecification]]$(
-        @{ ModuleName = 'CimCmdlets'; Guid = 'fb6cc51d-c096-4b38-b78d-0fed6277096a'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'Dism'; Guid = '389c464d-8b8d-48e9-aafe-6d8a590d6798'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'Microsoft.PowerShell.Archive'; Guid = 'eb74e8da-9ae2-482a-a648-e96550fb8733'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'Microsoft.PowerShell.Management'; Guid = 'eefcb906-b326-4e99-9f54-8b4bb6ef3c6d'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'Microsoft.PowerShell.Security'; Guid = 'a94c8c7e-9810-47c0-b8af-65089c13a35a'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'Microsoft.PowerShell.Utility'; Guid = '1da87e53-152b-403e-98dc-74d7b4d63d59'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'NetAdapter'; Guid = '1042b422-63a8-4016-a6d6-293e19e8f8a6'; ModuleVersion = '1.0' }
-        @{ ModuleName = 'ScheduledTasks'; Guid = '5378ee8e-e349-49bb-83b9-f3d9c396c0a6'; ModuleVersion = '1.0' }
-    )
+    @{ ModuleName = 'CimCmdlets'; Guid = 'fb6cc51d-c096-4b38-b78d-0fed6277096a'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'Dism'; Guid = '389c464d-8b8d-48e9-aafe-6d8a590d6798'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'Microsoft.PowerShell.Archive'; Guid = 'eb74e8da-9ae2-482a-a648-e96550fb8733'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'Microsoft.PowerShell.Management'; Guid = 'eefcb906-b326-4e99-9f54-8b4bb6ef3c6d'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'Microsoft.PowerShell.Security'; Guid = 'a94c8c7e-9810-47c0-b8af-65089c13a35a'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'Microsoft.PowerShell.Utility'; Guid = '1da87e53-152b-403e-98dc-74d7b4d63d59'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'NetAdapter'; Guid = '1042b422-63a8-4016-a6d6-293e19e8f8a6'; ModuleVersion = '1.0' }
+    @{ ModuleName = 'ScheduledTasks'; Guid = '5378ee8e-e349-49bb-83b9-f3d9c396c0a6'; ModuleVersion = '1.0' }
+)
 
 # Build out lookup table for all cmdlets used within module, starting with the core cmdlets.
 $CommandTable = [ordered]@{}; $ExecutionContext.SessionState.InvokeCommand.GetCmdlets() | & { process { if ($_.PSSnapIn -and $_.PSSnapIn.Name.Equals('Microsoft.PowerShell.Core') -and $_.PSSnapIn.IsDefault) { $CommandTable.Add($_.Name, $_) } } }
--- src/PSAppDeployToolkit/ImportsLast.ps1
+++ src/PSAppDeployToolkit/ImportsLast.ps1
@@ -89,32 +89,32 @@
                 Legacy = '[{1} {2}] [{3}] [{4}] [{5}] :: {0}'
             }).AsReadOnly()
         Severities = [System.Collections.ObjectModel.ReadOnlyCollection[System.Collections.Specialized.OrderedDictionary]]$(
-                ([ordered]@{
-                    Name = 'Success'
-                    Colours = ([ordered]@{
-                            ForegroundColor = [System.ConsoleColor]::Green
-                            BackgroundColor = [System.ConsoleColor]::Black
-                        }).AsReadOnly()
-                }).AsReadOnly()
-                ([ordered]@{
-                    Name = 'Info'
-                    Colours = ([ordered]@{}).AsReadOnly()
-                }).AsReadOnly()
-                ([ordered]@{
-                    Name = 'Warning'
-                    Colours = ([ordered]@{
-                            ForegroundColor = [System.ConsoleColor]::Yellow
-                            BackgroundColor = [System.ConsoleColor]::Black
-                        }).AsReadOnly()
-                }).AsReadOnly()
-                ([ordered]@{
-                    Name = 'Error'
-                    Colours = ([ordered]@{
-                            ForegroundColor = [System.ConsoleColor]::Red
-                            BackgroundColor = [System.ConsoleColor]::Black
-                        }).AsReadOnly()
-                }).AsReadOnly()
-            )
+            ([ordered]@{
+                Name = 'Success'
+                Colours = ([ordered]@{
+                        ForegroundColor = [System.ConsoleColor]::Green
+                        BackgroundColor = [System.ConsoleColor]::Black
+                    }).AsReadOnly()
+            }).AsReadOnly()
+            ([ordered]@{
+                Name = 'Info'
+                Colours = ([ordered]@{}).AsReadOnly()
+            }).AsReadOnly()
+            ([ordered]@{
+                Name = 'Warning'
+                Colours = ([ordered]@{
+                        ForegroundColor = [System.ConsoleColor]::Yellow
+                        BackgroundColor = [System.ConsoleColor]::Black
+                    }).AsReadOnly()
+            }).AsReadOnly()
+            ([ordered]@{
+                Name = 'Error'
+                Colours = ([ordered]@{
+                        ForegroundColor = [System.ConsoleColor]::Red
+                        BackgroundColor = [System.ConsoleColor]::Black
+                    }).AsReadOnly()
+            }).AsReadOnly()
+        )
     }).AsReadOnly()
 
 # DialogBox constants used within Show-ADTDialogBox.
@@ -145,10 +145,10 @@
 # Registry path transformation constants used within Convert-ADTRegistryPath.
 & $CommandTable.'New-Variable' -Name Registry -Option Constant -Value ([ordered]@{
         PathMatches = [System.Collections.ObjectModel.ReadOnlyCollection[System.String]]$(
-                ':\\'
-                ':'
-                '\\'
-            )
+            ':\\'
+            ':'
+            '\\'
+        )
         PathReplacements = ([ordered]@{
                 '^HKLM' = 'HKEY_LOCAL_MACHINE\'
                 '^HKCR' = 'HKEY_CLASSES_ROOT\'
