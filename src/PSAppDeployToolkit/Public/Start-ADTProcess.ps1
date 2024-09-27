55ffb93e81ac726dfc4646cf0cae8dbc4670b06d
Mitchell Richters <mjr4077au@gmail.com>
2024-09-28 09:19:15 +1000

    - CBH consistency fixes for all Process-based functions.

--- src/PSAppDeployToolkit/Public/Start-ADTProcess.ps1
+++ src/PSAppDeployToolkit/Public/Start-ADTProcess.ps1
@@ -7,101 +7,114 @@
 function Start-ADTProcess
 {
     <#
-
     .SYNOPSIS
-    Execute a process with optional arguments, working directory, window style.
+        Execute a process with optional arguments, working directory, window style.
 
     .DESCRIPTION
-    Executes a process, e.g. a file included in the Files directory of the App Deploy Toolkit, or a file on the local machine. Provides various options for handling the return codes (see Parameters).
+        Executes a process, e.g. a file included in the Files directory of the App Deploy Toolkit, or a file on the local machine. Provides various options for handling the return codes (see Parameters).
 
     .PARAMETER Path
-    Path to the file to be executed. If the file is located directly in the "Files" directory of the App Deploy Toolkit, only the file name needs to be specified.
+        Path to the file to be executed. If the file is located directly in the "Files" directory of the App Deploy Toolkit, only the file name needs to be specified.
 
-    Otherwise, the full path of the file must be specified. If the files is in a subdirectory of "Files", use the "$dirFiles" variable as shown in the example.
+        Otherwise, the full path of the file must be specified. If the files is in a subdirectory of "Files", use the "$dirFiles" variable as shown in the example.
 
     .PARAMETER Parameters
-    Arguments to be passed to the executable
+        Arguments to be passed to the executable
 
     .PARAMETER SecureParameters
-    Hides all parameters passed to the executable from the Toolkit log file
+        Hides all parameters passed to the executable from the Toolkit log file
 
     .PARAMETER WindowStyle
-    Style of the window of the process executed. Options: Normal, Hidden, Maximized, Minimized. Default: Normal. Only works for native Windows GUI applications. If the WindowStyle is set to Hidden, UseShellExecute should be set to $true.
+        Style of the window of the process executed. Options: Normal, Hidden, Maximized, Minimized. Default: Normal. Only works for native Windows GUI applications. If the WindowStyle is set to Hidden, UseShellExecute should be set to $true.
 
-    Note: Not all processes honor WindowStyle. WindowStyle is a recommendation passed to the process. They can choose to ignore it.
+        Note: Not all processes honor WindowStyle. WindowStyle is a recommendation passed to the process. They can choose to ignore it.
 
     .PARAMETER CreateNoWindow
-    Specifies whether the process should be started with a new window to contain it. Only works for Console mode applications. UseShellExecute should be set to $false. Default is false.
+        Specifies whether the process should be started with a new window to contain it. Only works for Console mode applications. UseShellExecute should be set to $false. Default is false.
 
     .PARAMETER WorkingDirectory
-    The working directory used for executing the process. Defaults to the directory of the file being executed. The use of UseShellExecute affects this parameter.
+        The working directory used for executing the process. Defaults to the directory of the file being executed. The use of UseShellExecute affects this parameter.
 
     .PARAMETER NoWait
-    Immediately continue after executing the process.
+        Immediately continue after executing the process.
 
     .PARAMETER PassThru
-    If NoWait is not specified, returns an object with ExitCode, STDOut and STDErr output from the process. If NoWait is specified, returns an object with Id, Handle and ProcessName.
+        If NoWait is not specified, returns an object with ExitCode, STDOut and STDErr output from the process. If NoWait is specified, returns an object with Id, Handle and ProcessName.
 
     .PARAMETER WaitForMsiExec
-    Sometimes an EXE bootstrapper will launch an MSI install. In such cases, this variable will ensure that this function waits for the msiexec engine to become available before starting the install.
+        Sometimes an EXE bootstrapper will launch an MSI install. In such cases, this variable will ensure that this function waits for the msiexec engine to become available before starting the install.
 
     .PARAMETER MsiExecWaitTime
-    Specify the length of time in seconds to wait for the msiexec engine to become available. Default: 600 seconds (10 minutes).
+        Specify the length of time in seconds to wait for the msiexec engine to become available. Default: 600 seconds (10 minutes).
 
     .PARAMETER SuccessCodes
-    List of exit codes to be considered successful. Defaults to values set during ADTSession initialization, otherwise: 0
+        List of exit codes to be considered successful. Defaults to values set during ADTSession initialization, otherwise: 0
 
     .PARAMETER RebootCodes
-    List of exit codes to indicate a reboot is required. Defaults to values set during ADTSession initialization, otherwise: 1641, 3010
+        List of exit codes to indicate a reboot is required. Defaults to values set during ADTSession initialization, otherwise: 1641, 3010
 
     .PARAMETER IgnoreExitCodes
-    List the exit codes to ignore or * to ignore all exit codes.
+        List the exit codes to ignore or * to ignore all exit codes.
 
     .PARAMETER PriorityClass
-    Specifies priority class for the process. Options: Idle, Normal, High, AboveNormal, BelowNormal, RealTime. Default: Normal
+        Specifies priority class for the process. Options: Idle, Normal, High, AboveNormal, BelowNormal, RealTime. Default: Normal
 
     .PARAMETER NoExitOnProcessFailure
-    Specifies whether the function shouldn't call Close-ADTSession when the process returns an exit code that is considered an error/failure. Default: $false
+        Specifies whether the function shouldn't call Close-ADTSession when the process returns an exit code that is considered an error/failure. Default: $false
 
     .PARAMETER UseShellExecute
-    Specifies whether to use the operating system shell to start the process. $true if the shell should be used when starting the process; $false if the process should be created directly from the executable file.
+        Specifies whether to use the operating system shell to start the process. $true if the shell should be used when starting the process; $false if the process should be created directly from the executable file.
 
-    The word "Shell" in this context refers to a graphical shell (similar to the Windows shell) rather than command shells (for example, bash or sh) and lets users launch graphical applications or open documents. It lets you open a file or a url and the Shell will figure out the program to open it with.
+        The word "Shell" in this context refers to a graphical shell (similar to the Windows shell) rather than command shells (for example, bash or sh) and lets users launch graphical applications or open documents. It lets you open a file or a url and the Shell will figure out the program to open it with.
 
-    The WorkingDirectory property behaves differently depending on the value of the UseShellExecute property. When UseShellExecute is true, the WorkingDirectory property specifies the location of the executable. When UseShellExecute is false, the WorkingDirectory property is not used to find the executable. Instead, it is used only by the process that is started and has meaning only within the context of the new process.
+        The WorkingDirectory property behaves differently depending on the value of the UseShellExecute property. When UseShellExecute is true, the WorkingDirectory property specifies the location of the executable. When UseShellExecute is false, the WorkingDirectory property is not used to find the executable. Instead, it is used only by the process that is started and has meaning only within the context of the new process.
 
-    If you set UseShellExecute to $true, there will be no available output from the process.
-
-    .EXAMPLE
-    Start-ADTProcess -Path 'setup.exe' -Parameters '/S' -IgnoreExitCodes 1,2
+        If you set UseShellExecute to $true, there will be no available output from the process.
 
     .EXAMPLE
-    Start-ADTProcess -Path "$dirFiles\Bin\setup.exe" -Parameters '/S' -WindowStyle 'Hidden'
+        Start-ADTProcess -Path 'setup.exe' -Parameters '/S' -IgnoreExitCodes 1,2
 
     .EXAMPLE
-    Start-ADTProcess -Path 'uninstall_flash_player_64bit.exe' -Parameters '/uninstall' -WindowStyle 'Hidden'
-
-    If the file is in the "Files" directory of the App Deploy Toolkit, only the file name needs to be specified.
+        Start-ADTProcess -Path "$dirFiles\Bin\setup.exe" -Parameters '/S' -WindowStyle 'Hidden'
 
     .EXAMPLE
-    Start-ADTProcess -Path 'setup.exe' -Parameters "-s -f2`"$((Get-ADTConfig).Toolkit.LogPath)\$installName.log`""
+        Start-ADTProcess -Path 'uninstall_flash_player_64bit.exe' -Parameters '/uninstall' -WindowStyle 'Hidden'
 
-    Launch InstallShield "setup.exe" from the ".\Files" sub-directory and force log files to the logging folder.
+        If the file is in the "Files" directory of the App Deploy Toolkit, only the file name needs to be specified.
 
     .EXAMPLE
-    Start-ADTProcess -Path 'setup.exe' -Parameters "/s /v`"ALLUSERS=1 /qn /L* \`"$((Get-ADTConfig).Toolkit.LogPath)\$installName.log`"`""
+        Start-ADTProcess -Path 'setup.exe' -Parameters "-s -f2`"$((Get-ADTConfig).Toolkit.LogPath)\$installName.log`""
 
-    Launch InstallShield "setup.exe" with embedded MSI and force log files to the logging folder.
+        Launch InstallShield "setup.exe" from the ".\Files" sub-directory and force log files to the logging folder.
+
+    .EXAMPLE
+        Start-ADTProcess -Path 'setup.exe' -Parameters "/s /v`"ALLUSERS=1 /qn /L* \`"$((Get-ADTConfig).Toolkit.LogPath)\$installName.log`"`""
+
+        Launch InstallShield "setup.exe" with embedded MSI and force log files to the logging folder.
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        You cannot pipe objects to this function.
 
     .OUTPUTS
-    None. This function does not generate any output.
+        PSADT.Types.ProcessResult
+
+        Returns an object with the results of the installation if -PassThru is specified.
+        - ExitCode
+        - StdOut
+        - StdErr
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [CmdletBinding()]
