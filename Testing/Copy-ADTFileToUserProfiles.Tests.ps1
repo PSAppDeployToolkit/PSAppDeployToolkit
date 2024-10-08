44ecec4cf2650315302b9f9fb21e2cb37e136969
Dan Gough <goughdj@gmail.com>
2024-10-08 15:32:53 +0100

    Update tests to handle new BasePath param

--- Testing/Copy-ADTFileToUserProfiles.Tests.ps1
+++ Testing/Copy-ADTFileToUserProfiles.Tests.ps1
@@ -8,16 +8,78 @@ Describe 'Copy-ADTFileToUserProfiles' {
 		Mock -ModuleName PSAppDeployToolkit Copy-ADTFile {
 		}
 		Mock -ModuleName PSAppDeployToolkit Get-ADTUserProfiles {
-			return @(
-				@{ ProfilePath = "C:\Users\User1" },
-				@{ ProfilePath = "C:\Users\User2" }
-			)
+			if ($PesterBoundParameters.LoadProfilePaths) {
+				[PSADT.Types.UserProfile]::new(
+					'User1',
+					'S-1-5-21-User1',
+					'C:\Users\User1',
+					'C:\Users\User1\AppData\Roaming',
+					'C:\Users\User1\AppData\Local',
+					'C:\Users\User1\Desktop',
+					'C:\Users\User1\Documents',
+					'C:\Users\User1\AppData\Roaming\Microsoft\Windows\Start Menu',
+					'C:\Users\User1\AppData\Local\Temp',
+					'C:\Users\User1\OneDrive',
+					'C:\Users\User1\OneDrive'
+				)
+				[PSADT.Types.UserProfile]::new(
+					'User2',
+					'S-1-5-21-User2',
+					'C:\Users\User2',
+					'C:\Users\User2\AppData\Roaming',
+					'C:\Users\User2\AppData\Local',
+					'C:\Users\User2\Desktop',
+					'C:\Users\User2\Documents',
+					'C:\Users\User2\AppData\Roaming\Microsoft\Windows\Start Menu',
+					'C:\Users\User2\AppData\Local\Temp',
+					'C:\Users\User2\OneDrive',
+					'C:\Users\User2\OneDrive'
+				)
+			} else {
+				[PSADT.Types.UserProfile]::new(
+					'User1',
+					'S-1-5-21-User1',
+					'C:\Users\User1',
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null
+				)
+				[PSADT.Types.UserProfile]::new(
+					'User2',
+					'S-1-5-21-User2',
+					'C:\Users\User2',
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null,
+					$null
+				)
+			}
 		}
 	}
 
 	Context 'Functionality' {
 		It 'Calls Copy-ADTFile for each user profile' {
 			Copy-ADTFileToUserProfiles -Path "$SourcePath\test.txt" -Destination "AppData\Local\Test"
+
+			Should -Invoke -ModuleName 'PSAppDeployToolkit' -CommandName 'Copy-ADTFile' -Times 1 -Exactly -ParameterFilter {
+				$Path -eq "$SourcePath\test.txt" -and $Destination -eq "C:\Users\User1\AppData\Local\Test"
+			}
+			Should -Invoke -ModuleName 'PSAppDeployToolkit' -CommandName 'Copy-ADTFile' -Times 1 -Exactly -ParameterFilter {
+				$Path -eq "$SourcePath\test.txt" -and $Destination -eq "C:\Users\User2\AppData\Local\Test"
+			}
+		}
+
+		It 'Calls Copy-ADTFile for each user profile with a non-default BasePath' {
+			Copy-ADTFileToUserProfiles -Path "$SourcePath\test.txt" -Destination "Test" -BasePath LocalAppData
 
 			Should -Invoke -ModuleName 'PSAppDeployToolkit' -CommandName 'Copy-ADTFile' -Times 1 -Exactly -ParameterFilter {
 				$Path -eq "$SourcePath\test.txt" -and $Destination -eq "C:\Users\User1\AppData\Local\Test"
