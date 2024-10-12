06112d5b9146025db415bd1fae345f4ea985fad9
Mitchell Richters <mjr4077au@gmail.com>
2024-10-12 18:28:31 +1100

    - Don't rely on `$PSBoundParameters` within `Invoke-AppDeployToolkit.ps1` so callers are free to extend the parameters with their own variables if desirable.

--- Examples/VLC/Invoke-AppDeployToolkit.ps1
+++ Examples/VLC/Invoke-AppDeployToolkit.ps1
@@ -102,6 +102,13 @@ $adtSession = @{
     DeployAppScriptVersion = [System.Version]'3.91.0'
     DeployAppScriptDate = '05/03/2024'
     DeployAppScriptParameters = $PSBoundParameters
+
+    # Script parameters.
+    DeploymentType = $DeploymentType
+    DeployMode = $DeployMode
+    AllowRebootPassThru = $AllowRebootPassThru
+    TerminalServerMode = $TerminalServerMode
+    DisableLogging = $DisableLogging
 }
 
 function Install-ADTApplication
@@ -109,7 +116,7 @@ function Install-ADTApplication
     ##================================================
     ## MARK: Pre-Install
     ##================================================
-    $adtSession.InstallPhase = "Pre-$($DeploymentType)"
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
 
     ## Show Welcome Message, close VLC if required, allow up to 3 deferrals, and persist the prompt
     Show-ADTInstallationWelcome -ProcessObjects ([PSADT.Types.ProcessObject]::new('vlc', $adtSession.AppName)) -AllowDeferCloseApps -DeferTimes 3 -PersistPrompt -NoMinimizeWindows
@@ -123,12 +130,12 @@ function Install-ADTApplication
     ##================================================
     ## MARK: Install
     ##================================================
-    $adtSession.InstallPhase = $DeploymentType
+    $adtSession.InstallPhase = $adtSession.DeploymentType
 
     ## Handle Zero-Config MSI installations.
     if ($adtSession.UseDefaultMsi)
     {
-        $ExecuteDefaultMSISplat = @{ Action = $DeploymentType; Path = $adtSession.DefaultMsiFile }
+        $ExecuteDefaultMSISplat = @{ Action = $adtSession.DeploymentType; Path = $adtSession.DefaultMsiFile }
         if ($adtSession.DefaultMstFile)
         {
             $ExecuteDefaultMSISplat.Add('Transform', $adtSession.DefaultMstFile)
@@ -147,7 +154,7 @@ function Install-ADTApplication
     ##================================================
     ## MARK: Post-Install
     ##================================================
-    $adtSession.InstallPhase = "Post-$($DeploymentType)"
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
 
     ## <Perform Post-Installation tasks here>
     Remove-ADTFile -Path "$envCommonDesktop\VLC media player.lnk","$envCommonStartMenuPrograms\VideoLAN\Release Notes.lnk","$envCommonStartMenuPrograms\VideoLAN\Documentation.lnk","$envCommonStartMenuPrograms\VideoLAN\VideoLAN Website.lnk"
@@ -165,7 +172,7 @@ function Uninstall-ADTApplication
     ##================================================
     ## MARK: Pre-Uninstall
     ##================================================
-    $adtSession.InstallPhase = "Pre-$($DeploymentType)"
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
 
     ## Show Welcome Message, close VLC with a 60 second countdown before automatically closing
     Show-ADTInstallationWelcome -ProcessObjects ([PSADT.Types.ProcessObject]::new('vlc', $adtSession.AppName)) -CloseAppsCountdown 60
@@ -179,12 +186,12 @@ function Uninstall-ADTApplication
     ##================================================
     ## MARK: Uninstall
     ##================================================
-    $adtSession.InstallPhase = $DeploymentType
+    $adtSession.InstallPhase = $adtSession.DeploymentType
 
     ## Handle Zero-Config MSI uninstallations.
     if ($adtSession.UseDefaultMsi)
     {
-        $ExecuteDefaultMSISplat = @{ Action = $DeploymentType; Path = $adtSession.DefaultMsiFile }
+        $ExecuteDefaultMSISplat = @{ Action = $adtSession.DeploymentType; Path = $adtSession.DefaultMsiFile }
         if ($adtSession.DefaultMstFile)
         {
             $ExecuteDefaultMSISplat.Add('Transform', $adtSession.DefaultMstFile)
@@ -198,7 +205,7 @@ function Uninstall-ADTApplication
     ##================================================
     ## MARK: Post-Uninstallation
     ##================================================
-    $adtSession.InstallPhase = "Post-$($DeploymentType)"
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
 
     ## <Perform Post-Uninstallation tasks here>
 }
@@ -208,7 +215,7 @@ function Repair-ADTApplication
     ##================================================
     ## MARK: Pre-Repair
     ##================================================
-    $adtSession.InstallPhase = "Pre-$($DeploymentType)"
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
 
     ## Show Welcome Message, close VLC with a 60 second countdown before automatically closing.
     Show-ADTInstallationWelcome -ProcessObjects ([PSADT.Types.ProcessObject]::new('vlc', $adtSession.AppName)) -CloseAppsCountdown 60
@@ -222,12 +229,12 @@ function Repair-ADTApplication
     ##================================================
     ## MARK: Repair
     ##================================================
-    $adtSession.InstallPhase = $DeploymentType
+    $adtSession.InstallPhase = $adtSession.DeploymentType
 
     ## Handle Zero-Config MSI repairs.
     if ($adtSession.UseDefaultMsi)
     {
-        $ExecuteDefaultMSISplat = @{ Action = $DeploymentType; Path = $adtSession.DefaultMsiFile }
+        $ExecuteDefaultMSISplat = @{ Action = $adtSession.DeploymentType; Path = $adtSession.DefaultMsiFile }
         if ($adtSession.DefaultMstFile)
         {
             $ExecuteDefaultMSISplat.Add('Transform', $adtSession.DefaultMstFile)
@@ -243,7 +250,7 @@ function Repair-ADTApplication
     ##================================================
     ## MARK: Post-Repair
     ##================================================
-    $adtSession.InstallPhase = "Post-$($DeploymentType)"
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
 
     ## <Perform Post-Repair tasks here>
     Remove-ADTFile -Path "$envCommonDesktop\VLC media player.lnk","$envCommonStartMenuPrograms\VideoLAN\Release Notes.lnk","$envCommonStartMenuPrograms\VideoLAN\Documentation.lnk","$envCommonStartMenuPrograms\VideoLAN\VideoLAN Website.lnk"
@@ -266,7 +273,7 @@ try
     Import-Module -Name $PSScriptRoot\..\..\PSAppDeployToolkit -Force
     try
     {
-        $adtSession = Open-ADTSession -SessionState $ExecutionContext.SessionState @PSBoundParameters @adtSession -PassThru
+        $adtSession = Open-ADTSession -SessionState $ExecutionContext.SessionState @adtSession -PassThru
     }
     catch
     {
@@ -287,7 +294,7 @@ catch
 
 try
 {
-    & "$($DeploymentType)-ADTApplication"
+    & "$($adtSession.DeploymentType)-ADTApplication"
     Close-ADTSession
 }
 catch
