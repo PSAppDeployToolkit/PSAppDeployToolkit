db024ad17098602942c87f962b2d5271ec9d7010
Mitchell Richters <mjr4077au@gmail.com>
2024-10-14 10:37:13 +1100

    - Use the Shell objects from the environment table where possible to do so.
    Initially, the dialog code was meant to run on an uninitialised module, however this is not possible as we need to initialise it to read the config. As such, we can rely on the environment table here again once-more.

--- src/PSAppDeployToolkit/Public/Show-ADTDialogBox.ps1
+++ src/PSAppDeployToolkit/Public/Show-ADTDialogBox.ps1
@@ -147,7 +147,7 @@ function Show-ADTDialogBox
         {
             try
             {
-                $result = switch ([System.Activator]::CreateInstance([System.Type]::GetTypeFromProgID('WScript.Shell')).Popup($Text, $Timeout, $Title, ($Script:DialogBox.Buttons.$Buttons + $Script:DialogBox.Icons.$Icon + $Script:DialogBox.DefaultButtons.$DefaultButton + (4096 * !$NotTopMost))))
+                $result = switch ((Get-ADTEnvironment).Shell.Popup($Text, $Timeout, $Title, ($Script:DialogBox.Buttons.$Buttons + $Script:DialogBox.Icons.$Icon + $Script:DialogBox.DefaultButtons.$DefaultButton + (4096 * !$NotTopMost))))
                 {
                     1 { 'OK'; break }
                     2 { 'Cancel'; break }
