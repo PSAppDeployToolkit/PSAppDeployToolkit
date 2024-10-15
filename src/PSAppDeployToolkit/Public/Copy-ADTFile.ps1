ce7e279385ab33692e55e48d695a7c583d9459f0
Dan Gough <goughdj@gmail.com>
2024-10-15 14:51:13 +0100

    Move Robocopy params to end of command line to fix #1054

--- src/PSAppDeployToolkit/Public/Copy-ADTFile.ps1
+++ src/PSAppDeployToolkit/Public/Copy-ADTFile.ps1
@@ -286,7 +286,7 @@ function Copy-ADTFile
                     $destFolderAttributes = [System.IO.File]::GetAttributes($robocopyDestination)
 
                     # Begin copy operation.
-                    $robocopyArgs = "$RobocopyParams $RobocopyAdditionalParams `"$robocopySource`" `"$robocopyDestination`" `"$robocopyFile`""
+                    $robocopyArgs = "`"$robocopySource`" `"$robocopyDestination`" `"$robocopyFile`" $RobocopyParams $RobocopyAdditionalParams"
                     Write-ADTLogEntry -Message "Executing Robocopy command: $robocopyCommand $robocopyArgs"
                     $robocopyResult = Start-ADTProcess -Path $robocopyCommand -Parameters $robocopyArgs -CreateNoWindow -NoExitOnProcessFailure -PassThru -SuccessCodes 0, 1, 2, 3, 4, 5, 6, 7, 8 -ErrorAction Ignore
 
