673e965c98cc7492c422d944a8b4d152e05d8ec2
Dan Cunningham <dancunningham@me.com>
2024-10-15 01:19:08 -0400

    First pass of PSADT UI

---
+++ Sources/PSADT.UI/PSADT.UserInterface/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using System.Diagnostics;
+global using System.Windows;
+global using Wpf.Ui.Controls;
---
+++ Sources/PSADT.UI/PSADT.UserInterface/UserInterface/Classes/AppProcessInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace PSADT.UserInterface.Utilities
+{
+    public class AppProcessInfo
+    {
+        public string? ProcessName { get; set; }
+        public string? ProcessDescription { get; set; }
+        public string? ProductName { get; set; }
+        public string? PublisherName { get; set; }
+        public ImageSource? Icon { get; set; }
+    }
+}
