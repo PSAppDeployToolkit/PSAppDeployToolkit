4d5b39b416171aee30e3a2a7f7927b912b848024
Mitchell Richters <mjr4077au@gmail.com>
2024-10-18 22:05:33 +1100

    - Publicise the Object functions and create compatibility wrappers.

--- src/PSAppDeployToolkit/Public/Get-ADTObjectProperty.ps1
+++ src/PSAppDeployToolkit/Public/Get-ADTObjectProperty.ps1
@@ -7,40 +7,44 @@
 function Get-ADTObjectProperty
 {
     <#
-
     .SYNOPSIS
-    Get a property from any object.
+        Get a property from any object.
 
     .DESCRIPTION
-    Get a property from any object.
+        Get a property from any object.
 
     .PARAMETER InputObject
-    Specifies an object which has properties that can be retrieved.
+        Specifies an object which has properties that can be retrieved.
 
     .PARAMETER PropertyName
-    Specifies the name of a property to retrieve.
+        Specifies the name of a property to retrieve.
 
     .PARAMETER ArgumentList
-    Argument to pass to the property being retrieved.
+        Argument to pass to the property being retrieved.
 
     .INPUTS
-    None. You cannot pipe objects to this function.
+        None
+
+        You cannot pipe objects to this function.
 
     .OUTPUTS
-    System.Object. Returns the value of the property being retrieved.
+        System.Object
+
+        Returns the value of the property being retrieved.
 
     .EXAMPLE
-    Get-ADTObjectProperty -InputObject $Record -PropertyName 'StringData' -ArgumentList @(1)
+        Get-ADTObjectProperty -InputObject $Record -PropertyName 'StringData' -ArgumentList @(1)
 
     .NOTES
-    This is an internal script function and should typically not be called directly.
+        An active ADT session is NOT required to use this function.
 
-    .NOTES
-    An active ADT session is NOT required to use this function.
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (c) 2024 PSAppDeployToolkit Team, licensed under LGPLv3
+        License: https://opensource.org/license/lgpl-3-0
 
     .LINK
-    https://psappdeploytoolkit.com
-
+        https://psappdeploytoolkit.com
     #>
 
     [CmdletBinding()]
