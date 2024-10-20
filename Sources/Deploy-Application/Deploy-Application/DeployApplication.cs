db6dc2fbe553527e6b5cfd3b3257a44a0f72e2df
Mitchell Richters <mjr4077au@gmail.com>
2024-10-21 06:26:04 +1100

    - Rework the `Deploy-Application` executable to work against the module backend.

--- Sources/Deploy-Application/Deploy-Application/DeployApplication.cs
+++ Sources/Deploy-Application/Deploy-Application/DeployApplication.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Xml;
+using System.Management.Automation.Language;
 using System.Windows.Forms;
 
 namespace PSAppDeployToolkit
@@ -11,162 +12,168 @@ namespace PSAppDeployToolkit
     {
         public static void Main()
         {
+            // Set up exit code.
+            int exitCode = 60010;
+
             try
             {
-                // Set up variables
-                int processExitCode = 60010;
-                string currentAppFolder = AppDomain.CurrentDomain.BaseDirectory;
-                string appDeployScriptPath = Path.Combine(currentAppFolder, "Deploy-Application.ps1");
-                string appDeployToolkitFolder = Path.Combine(currentAppFolder, "AppDeployToolkit");
-                string appDeployToolkitXMLPath = Path.Combine(appDeployToolkitFolder, "AppDeployToolkitConfig.xml");
-                string powershellExePath = Path.Combine(Environment.GetEnvironmentVariable("WinDir"), "System32\\WindowsPowerShell\\v1.0\\PowerShell.exe");
-                string powershellArgs = "-ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden";
-                List<string> commandLineArgs = new List<string>(Environment.GetCommandLineArgs());
+                // Set up variables.
+                string currentPath = AppDomain.CurrentDomain.BaseDirectory;
+                string adtFrontendPath = Path.Combine(currentPath, "Deploy-Application.ps1");
+                string adtToolkitPath = Path.Combine(currentPath, "AppDeployToolkit\\PSAppDeployToolkit\\PSAppDeployToolkit.psd1");
+                string adtConfigPath = Path.Combine(currentPath, "AppDeployToolkit\\PSAppDeployToolkit\\Config\\config.psd1");
+                string pwshExecutablePath = Path.Combine(Environment.SystemDirectory, "WindowsPowerShell\\v1.0\\PowerShell.exe");
+                string pwshArguments = "-ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden";
+                List<string> cliArguments = new List<string>(Environment.GetCommandLineArgs());
                 bool isForceX86Mode = false;
                 bool isRequireAdmin = false;
+                bool is64BitOS = false;
 
                 // Get OS Architecture. Check does not return correct value when running in x86 process on x64 system but it works for our purpose.
                 // To get correct OS architecture when running in x86 process on x64 system, we would also have to check environment variable: PROCESSOR_ARCHITEW6432.
-                bool is64BitOS = false;
                 if (Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE").Contains("64"))
+                {
                     is64BitOS = true;
+                }
 
-                // Trim ending & starting empty space from each element in the command-line
-                commandLineArgs = commandLineArgs.ConvertAll(s => s.Trim());
-                // Remove first command-line argument as this is always the executable name
-                commandLineArgs.RemoveAt(0);
+                // Trim ending & starting empty space from each element in the command-line.
+                cliArguments = cliArguments.ConvertAll(s => s.Trim());
+                // Remove first command-line argument as this is always the executable name.
+                cliArguments.RemoveAt(0);
 
-                // Check if x86 PowerShell mode was specified on command line
-                if (commandLineArgs.Exists(x => x == "/32"))
+                // Check if x86 PowerShell mode was specified on command line.
+                if (cliArguments.Exists(x => x == "/32"))
                 {
-                    isForceX86Mode = true;
-                    WriteDebugMessage("'/32' parameter was specified on the command-line. Running in forced x86 PowerShell mode...");
                     // Remove the /32 command line argument so that it is not passed to PowerShell script
-                    commandLineArgs.RemoveAll(x => x == "/32");
+                    WriteDebugMessage("'/32' parameter was specified on the command-line. Running in forced x86 PowerShell mode...");
+                    cliArguments.RemoveAll(x => x == "/32");
+                    isForceX86Mode = true;
                 }
-                
-                // Check for the App Deploy Script file being specified
-                string commandLineAppDeployScriptFileArg = String.Empty;
-                string commandLineAppDeployScriptPath = String.Empty;
-                if (commandLineArgs.Exists(x => x.StartsWith("-File ")))
+
+                // Check for the App Deploy Script file being specified.
+                if (cliArguments.Exists(x => x.StartsWith("-Command ")))
                 {
-                    throw new Exception("'-File' parameter was specified on the command-line. Please use the '-Command' parameter instead because using the '-File' parameter can return the incorrect exit code in PowerShell 2.0.");
+                    throw new Exception("'-Command' parameter was specified on the command-line. Please use the '-File' parameter instead, which will properly handle exit codes with PowerShell 3.0 and higher.");
                 }
-                else if (commandLineArgs.Exists(x => x.StartsWith("-Command ")))
+
+                if (cliArguments.Exists(x => x.StartsWith("-File ")))
                 {
-                    commandLineAppDeployScriptFileArg = commandLineArgs.Find(x => x.StartsWith("-Command "));
-                    appDeployScriptPath = commandLineAppDeployScriptFileArg.Replace("-Command ", String.Empty).Replace("\"", String.Empty);
-                    if (!Path.IsPathRooted(appDeployScriptPath))
-                        appDeployScriptPath = Path.Combine(currentAppFolder, appDeployScriptPath);
-                    commandLineArgs.RemoveAt(commandLineArgs.FindIndex(x => x.StartsWith("-Command")));
-                    WriteDebugMessage("'-Command' parameter specified on command-line. Passing command-line untouched...");
+                    adtFrontendPath = cliArguments.Find(x => x.StartsWith("-File ")).Replace("-File ", string.Empty).Replace("\"", string.Empty);
+                    if (!Path.IsPathRooted(adtFrontendPath))
+                    {
+                        adtFrontendPath = Path.Combine(currentPath, adtFrontendPath);
+                    }
+                    cliArguments.RemoveAt(cliArguments.FindIndex(x => x.StartsWith("-File")));
+                    WriteDebugMessage("'-File' parameter specified on command-line. Passing command-line untouched...");
                 }
-                else if (commandLineArgs.Exists(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")))
+                else if (cliArguments.Exists(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")))
                 {
-                    appDeployScriptPath = commandLineArgs.Find(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")).Replace("\"", String.Empty);
-                    if (!Path.IsPathRooted(appDeployScriptPath))
-                        appDeployScriptPath = Path.Combine(currentAppFolder, appDeployScriptPath);
-                    commandLineArgs.RemoveAt(commandLineArgs.FindIndex(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")));
+                    adtFrontendPath = cliArguments.Find(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")).Replace("\"", string.Empty);
+                    if (!Path.IsPathRooted(adtFrontendPath))
+                    {
+                        adtFrontendPath = Path.Combine(currentPath, adtFrontendPath);
+                    }
+                    cliArguments.RemoveAt(cliArguments.FindIndex(x => x.EndsWith(".ps1") || x.EndsWith(".ps1\"")));
                     WriteDebugMessage(".ps1 file specified on command-line. Appending '-Command' parameter name...");
                 }
                 else
                 {
-                    WriteDebugMessage("No '-Command' parameter specified on command-line. Adding parameter '-Command \"" + appDeployScriptPath + "\"'...");
+                    WriteDebugMessage($"No '-File' parameter specified on command-line. Adding parameter '-File \"{adtFrontendPath}\"'...");
                 }
 
-                // Define the command line arguments to pass to PowerShell
-                powershellArgs = powershellArgs + " -Command & { & '" + appDeployScriptPath + "'";
-                if (commandLineArgs.Count > 0)
+                // Define the command line arguments to pass to PowerShell.
+                pwshArguments = $"-ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden -File \"{adtFrontendPath}\"";
+                if (cliArguments.Count > 0)
                 {
-                    powershellArgs = powershellArgs + " " + string.Join(" ", commandLineArgs.ToArray());
-                }
-                powershellArgs = powershellArgs + "; Exit $LastExitCode }";
-
-                // Verify if the App Deploy script file exists
-                if (!File.Exists(appDeployScriptPath))
-                {
-                    throw new Exception("A critical component of the App Deployment Toolkit is missing." + Environment.NewLine + Environment.NewLine + "Unable to find the App Deploy Script file: " + appDeployScriptPath + "." + Environment.NewLine + Environment.NewLine + "Please ensure you have all of the required files available to start the installation.");
+                    pwshArguments += " " + string.Join(" ", cliArguments.ToArray());
                 }
 
-                // Verify if the App Deploy Toolkit folder exists
-                if (!Directory.Exists(appDeployToolkitFolder))
+                // Verify if the App Deploy script file exists.
+                if (!File.Exists(adtFrontendPath))
                 {
-                    throw new Exception("A critical component of the App Deployment Toolkit is missing." + Environment.NewLine + Environment.NewLine + "Unable to find the 'AppDeployToolkit' folder." + Environment.NewLine + Environment.NewLine + "Please ensure you have all of the required files available to start the installation.");
+                    throw new Exception($"A critical component of PSAppDeployToolkit is missing.\n\nUnable to find the App Deploy Script file at '{adtFrontendPath}'.\n\nPlease ensure you have all of the required files available to start the installation.");
                 }
 
-                // Verify if the App Deploy Toolkit Config XML file exists
-                if (!File.Exists(appDeployToolkitXMLPath))
+                // Verify if the App Deploy Toolkit folder exists.
+                if (!File.Exists(adtToolkitPath))
                 {
-                    throw new Exception("A critical component of the App Deployment Toolkit is missing." + Environment.NewLine + Environment.NewLine + "Unable to find the 'AppDeployToolkitConfig.xml' file." + Environment.NewLine + Environment.NewLine + "Please ensure you have all of the required files available to start the installation.");
-                }
-                else
-                {
-                    // Read the XML and determine whether we need Admin Rights
-                    XmlDocument xml = new XmlDocument();
-                    xml.Load(appDeployToolkitXMLPath);
-                    XmlNode xmlNode = null;
-                    XmlElement xmlRoot = xml.DocumentElement;
-                    xmlNode = xmlRoot.SelectSingleNode("/AppDeployToolkit_Config/Toolkit_Options/Toolkit_RequireAdmin");
-                    isRequireAdmin = Convert.ToBoolean(xmlNode.InnerText);
-                    if (isRequireAdmin)
-                    {
-                        WriteDebugMessage("Administrator rights are required...");
-                    }
+                    throw new Exception($"A critical component of PSAppDeployToolkit is missing.\n\nUnable to find the 'PSAppDeployToolkit.psd1' module file at '{adtToolkitPath}'.\n\nPlease ensure you have all of the required files available to start the installation.");
                 }
 
-                // Switch to x86 PowerShell if requested
-                if (is64BitOS & isForceX86Mode)
+                // Verify if the App Deploy Toolkit Config XML file exists.
+                if (!File.Exists(adtConfigPath))
                 {
-                    powershellExePath = Path.Combine(Environment.GetEnvironmentVariable("WinDir"), "SysWOW64\\WindowsPowerShell\\v1.0\\PowerShell.exe");
+                    throw new Exception($"A critical component of PSAppDeployToolkit is missing.\n\nUnable to find the 'config.psd1' file at '{adtConfigPath}'.\n\nPlease ensure you have all of the required files available to start the installation.");
                 }
 
-                // Define PowerShell process
-                WriteDebugMessage("PowerShell Path: " + powershellExePath);
-                WriteDebugMessage("PowerShell Parameters: " + powershellArgs);
-                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                // Parse our config and throw if we have any errors.
+                var configAst = Parser.ParseFile(adtConfigPath, out Token[] configTokens, out ParseError[] configErrors);
+                if (configErrors.Length > 0)
                 {
-                    FileName = powershellExePath,
-                    Arguments = powershellArgs,
-                    WorkingDirectory = Path.GetDirectoryName(powershellExePath),
+                    throw new Exception($"A critical component of PSAppDeployToolkit is corrupt.\n\nUnable to parse the 'config.psd1' file at '{adtConfigPath}'.\n\nPlease review your configuration to ensure it's correct before starting the installation.");
+                }
+
+                // Determine whether we require admin rights or not.
+                Hashtable configTable = (Hashtable)configAst.EndBlock.Find(p => p.GetType() == typeof(HashtableAst), false).SafeGetValue();
+                Hashtable toolkitConfig = (Hashtable)configTable["Toolkit"];
+                if (isRequireAdmin = (bool)toolkitConfig["RequireAdmin"])
+                {
+                    WriteDebugMessage("Administrator rights are required...");
+                }
+
+                // Switch to x86 PowerShell if requested.
+                if (is64BitOS && isForceX86Mode)
+                {
+                    pwshExecutablePath = Path.Combine(Environment.GetEnvironmentVariable("WinDir"), "SysWOW64\\WindowsPowerShell\\v1.0\\PowerShell.exe");
+                }
+
+                // Define PowerShell process.
+                WriteDebugMessage("Executable Path: " + pwshExecutablePath);
+                WriteDebugMessage("Arguments: " + pwshArguments);
+                WriteDebugMessage("Working Directory: " + currentPath);
+
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = pwshExecutablePath,
+                    Arguments = pwshArguments,
+                    WorkingDirectory = currentPath,
                     WindowStyle = ProcessWindowStyle.Hidden,
                     UseShellExecute = true
                 };
-                // Set the RunAs flag if the XML specifically calls for Admin Rights and OS Vista or higher
-                if (((isRequireAdmin) & (Environment.OSVersion.Version.Major >= 6)))
+
+                // Set the RunAs flag if the PSADT configuration file specifically calls for Admin Rights and OS Vista or higher
+                if (isRequireAdmin && (Environment.OSVersion.Version.Major >= 6))
                 {
                     processStartInfo.Verb = "runas";
                 }
 
                 // Start the PowerShell process and wait for completion
-                processExitCode = 60011;
-                Process process = new Process();
+                exitCode = 60011;
+                var process = new Process();
                 try
                 {
                     process.StartInfo = processStartInfo;
                     process.Start();
                     process.WaitForExit();
-                    processExitCode = process.ExitCode;
+                    exitCode = process.ExitCode;
                 }
-                catch (Exception)
+                catch
                 {
                     throw;
                 }
                 finally
                 {
-                    if ((process != null))
-                    {
-                        process.Dispose();
-                    }
+                    process?.Dispose();
                 }
 
-                // Exit
-                WriteDebugMessage("Exit Code: " + processExitCode);
-                Environment.Exit(processExitCode);
+                // Exit with the script's code.
+                WriteDebugMessage("Exit Code: " + exitCode);
+                Environment.Exit(exitCode);
             }
             catch (Exception ex)
             {
                 WriteDebugMessage(ex.Message, true, MessageBoxIcon.Error);
-                Environment.Exit(processExitCode);
+                Environment.Exit(exitCode);
             }
         }
 
@@ -176,28 +183,26 @@ namespace PSAppDeployToolkit
             Console.WriteLine(debugMessage);
 
             // If we are to display an error message...
-            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
-            if (IsDisplayError == true)
+            if (IsDisplayError)
             {
-                MessageBox.Show(new WindowWrapper(handle), debugMessage, Application.ProductName + " " + Application.ProductVersion, MessageBoxButtons.OK, (MessageBoxIcon)MsgBoxStyle, MessageBoxDefaultButton.Button1);
+                MessageBox.Show(
+                    new WindowWrapper(Process.GetCurrentProcess().MainWindowHandle),
+                    debugMessage,
+                    Application.ProductName + " " + Application.ProductVersion,
+                    MessageBoxButtons.OK,
+                    MsgBoxStyle,
+                    MessageBoxDefaultButton.Button1);
             }
         }
 
-        public class WindowWrapper : System.Windows.Forms.IWin32Window
+        public class WindowWrapper : IWin32Window
         {
             public WindowWrapper(IntPtr handle)
             {
-                _hwnd = handle;
+                Handle = handle;
             }
 
-            public IntPtr Handle
-            {
-                get { return _hwnd; }
-            }
-
-            private IntPtr _hwnd;
+            public IntPtr Handle { get; }
         }
-
-        public static int processExitCode { get; set; }
     }
 }
