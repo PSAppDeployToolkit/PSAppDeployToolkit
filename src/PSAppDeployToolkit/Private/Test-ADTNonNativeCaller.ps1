05b205ea428863cec8631ed829268b3b5c9db82c
Mitchell Richters <mjr4077au@gmail.com>
2024-10-21 20:11:44 +1100

    - Restore `Test-ADTNonNativeCaller` as we're now doing this check in more than one place.

---
+++ src/PSAppDeployToolkit/Private/Test-ADTNonNativeCaller.ps1
@@ -0,0 +1,10 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Test-ADTNonNativeCaller
+#
+#-----------------------------------------------------------------------------
+
+function Test-ADTNonNativeCaller
+{
+    return (Get-PSCallStack).Command.Contains('AppDeployToolkitMain.ps1')
+}
