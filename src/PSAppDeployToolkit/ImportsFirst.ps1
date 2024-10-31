f4cef53ba0997fcf71dc1ea84a68346427315bb2
Mitchell Richters <mjr4077au@gmail.com>
2024-10-31 17:47:46 +1100

    - Backport some changes from `PSAppDeployToolkit.Extensions` into the main module.

--- src/PSAppDeployToolkit/ImportsFirst.ps1
+++ src/PSAppDeployToolkit/ImportsFirst.ps1
@@ -1,4 +1,26 @@
-﻿#-----------------------------------------------------------------------------
+﻿<#
+
+.SYNOPSIS
+PSAppDeployToolkit - This module script contains the PSADT core runtime and functions using by a Invoke-AppDeployToolkit.ps1 script.
+
+.DESCRIPTION
+This module can be directly imported from the command line via Import-Module, but it is usually imported by the Invoke-AppDeployToolkit.ps1 script.
+
+This module can usually be updated to the latest version without impacting your per-application Invoke-AppDeployToolkit.ps1 scripts. Please check release notes before upgrading.
+
+PSAppDeployToolkit is licensed under the GNU LGPLv3 License - (C) 2024 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham and Muhammad Mashwani).
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+.LINK
+https://psappdeploytoolkit.com
+
+#>
+
+#-----------------------------------------------------------------------------
 #
 # MARK: Module Initialization Code
 #
