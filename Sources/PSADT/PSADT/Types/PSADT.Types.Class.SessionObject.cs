6c9e85325b9a9ec2ef89001688ce2b9649c0f409
Mitchell Richters <mjr4077au@gmail.com>
2024-11-17 09:12:33 +1100

    - Create initial skeleton for `PSADT.Types.SessionObject`.
    Contains all members and an empty constructor. Code compiles.

---
+++ Sources/PSADT/PSADT/Types/PSADT.Types.Class.SessionObject.cs
@@ -0,0 +1,310 @@
+using System;
+using System.IO;
+using System.Management.Automation;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PSADT.Types
+{
+    public sealed class SessionObject
+    {
+        #region Constructors.
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionObject"/> class.
+        /// </summary>
+        /// <param name="parameters">All parameters from Open-ADTSession.</param>
+        public SessionObject(IDictionary parameters)
+        {
+        }
+
+
+        #endregion
+        #region Internal variables.
+
+
+        /// <summary>
+        /// Gets whether this session object is operating in compatibility mode.
+        /// </summary>
+        private bool CompatibilityMode { get; }
+
+        /// <summary>
+        /// Gets the caller's variables from the provided $ExecutionContext.
+        /// </summary>
+        private PSVariableIntrinsics CallerVariables { get; }
+
+        /// <summary>
+        /// Gets the mounted WIM files within this session.
+        /// </summary>
+        private readonly List<FileInfo> MountedWimFiles = [];
+
+        /// <summary>
+        /// Gets the log entries written with this session.
+        /// </summary>
+        private readonly List<LogEntry> LogBuffer = [];
+
+        /// <summary>
+        /// Gets the list of executables found within a Zero-Config MSI file.
+        /// </summary>
+        private ProcessObject[] DefaultMsiExecutablesList { get; }
+
+        /// <summary>
+        /// Gets whether this session object has finished processing the Zero-Config MSI file detection.
+        /// </summary>
+        private bool ZeroConfigInitiated { get; }
+
+        /// <summary>
+        /// Gets whether this session object was instantiated via a script or the command line.
+        /// </summary>
+        private bool RunspaceOrigin { get; }
+
+        /// <summary>
+        /// Gets whether this session object should force WIM file detection, even if AppName was defined.
+        /// </summary>
+        private bool ForceWimDetection { get; }
+
+        /// <summary>
+        /// Gets the drive letter used with subst during a Zero-Config WIM file mount operation.
+        /// </summary>
+        private string DirFilesSubstDrive { get; }
+
+        /// <summary>
+        /// Gets the registry path used for getting/setting deferral information.
+        /// </summary>
+        private string RegKeyDeferHistory { get; }
+
+        /// <summary>
+        /// Gets the deployment type name from the language string table for the given DeploymentType.
+        /// </summary>
+        private string DeploymentTypeName { get; }
+
+        /// <summary>
+        /// Gets whether this session object is in non-interactive mode.
+        /// </summary>
+        private bool DeployModeNonInteractive { get; }
+
+        /// <summary>
+        /// Gets whether this session object is in silent mode.
+        /// </summary>
+        private bool DeployModeSilent { get; }
+
+        /// <summary>
+        /// Gets whether this session object has been instantiated.
+        /// </summary>
+        private bool Instantiated { get; }
+
+        /// <summary>
+        /// Gets whether this session object has been opened.
+        /// </summary>
+        private bool Opened { get; }
+
+        /// <summary>
+        /// Gets whether this session object has been closed.
+        /// </summary>
+        private bool Closed { get; }
+
+        /// <summary>
+        /// Gets the session object's filesystem log path.
+        /// </summary>
+        private string LogPath { get; }
+
+        /// <summary>
+        /// Gets the session object's closing exit code.
+        /// </summary>
+        private int ExitCode { get; set; }
+
+
+        #endregion
+        #region Frontend parameters.
+
+
+        /// <summary>
+        /// Gets the session object's deployment type.
+        /// </summary>
+        public string DeploymentType { get; } = "Install";
+
+        /// <summary>
+        /// Gets the session object's deployment mode.
+        /// </summary>
+        public string DeployMode { get; } = "Interactive";
+
+        /// <summary>
+        /// Gets whether this session object is allowed to exit with a reboot exit code.
+        /// </summary>
+        public bool AllowRebootPassThru { get; }
+
+        /// <summary>
+        /// Gets whether this session object should enable terminal services install mode.
+        /// </summary>
+        public bool TerminalServerMode { get; }
+
+        /// <summary>
+        /// Gets whether this session object should disable logging for the operation.
+        /// </summary>
+        public bool DisableLogging { get; }
+
+
+        #endregion
+        #region Frontend variables.
+
+
+        /// <summary>
+        /// Gets the session object's application vendor.
+        /// </summary>
+        public string AppVendor { get; }
+
+        /// <summary>
+        /// Gets the session object's application name.
+        /// </summary>
+        public string AppName { get; }
+
+        /// <summary>
+        /// Gets the session object's application version.
+        /// </summary>
+        public string AppVersion { get; }
+
+        /// <summary>
+        /// Gets the session object's application architecture.
+        /// </summary>
+        public string AppArch { get; }
+
+        /// <summary>
+        /// Gets the session object's application language.
+        /// </summary>
+        public string AppLang { get; }
+
+        /// <summary>
+        /// Gets the session object's application package revision.
+        /// </summary>
+        public string AppRevision { get; }
+
+        /// <summary>
+        /// Gets the session object's exit code(s) to indicate a successful deployment.
+        /// </summary>
+        public int[] AppSuccessExitCodes { get; } = [0];
+
+        /// <summary>
+        /// Gets the session object's exit code(s) to indicate a reboot is required.
+        /// </summary>
+        public int[] AppRebootExitCodes { get; } = [1641, 3010];
+
+        /// <summary>
+        /// Gets the session object's application package version.
+        /// </summary>
+        public Version AppScriptVersion { get; }
+
+        /// <summary>
+        /// Gets the session object's application package date.
+        /// </summary>
+        public string AppScriptDate { get; }
+
+        /// <summary>
+        /// Gets the session object's application package author.
+        /// </summary>
+        public string AppScriptAuthor { get; }
+
+        /// <summary>
+        /// Gets an override to the session object's installation name.
+        /// </summary>
+        public string InstallName { get; }
+
+        /// <summary>
+        /// Gets an override to the session object's installation title.
+        /// </summary>
+        public string InstallTitle { get; }
+
+        /// <summary>
+        /// Gets the session object's frontend script name.
+        /// </summary>
+        public string DeployAppScriptFriendlyName { get; }
+
+        /// <summary>
+        /// Gets the session object's frontend script version.
+        /// </summary>
+        public Version DeployAppScriptVersion { get; }
+
+        /// <summary>
+        /// Gets the session object's frontend script parameters.
+        /// </summary>
+        public IDictionary DeployAppScriptParameters { get; }
+
+        /// <summary>
+        /// Gets/sets the session object's installation phase'.
+        /// </summary>
+        public string InstallPhase { get; set; } = "Initialization";
+
+
+        #endregion
+        #region Other public variables.
+
+
+        /// <summary>
+        /// Gets the session object's starting date and time.
+        /// </summary>
+        public readonly DateTime CurrentDateTime = DateTime.Now;
+
+        /// <summary>
+        /// Gets the session object's starting date as a string.
+        /// </summary>
+        public string CurrentDate { get; }
+
+        /// <summary>
+        /// Gets the session object's starting time as a string.
+        /// </summary>
+        public string CurrentTime { get; }
+
+        /// <summary>
+        /// Gets the session object's UTC offset from GMT 0.
+        /// </summary>
+        public readonly TimeSpan CurrentTimeZoneBias = TimeZoneInfo.Local.BaseUtcOffset;
+
+        /// <summary>
+        /// Gets the script directory of the caller.
+        /// </summary>
+        public string ScriptDirectory { get; }
+
+        /// <summary>
+        /// Gets the specified or determined path to the Files folder.
+        /// </summary>
+        public string DirFiles { get; }
+
+        /// <summary>
+        /// Gets the specified or determined path to the SupportFiles folder.
+        /// </summary>
+        public string DirSupportFiles { get; }
+
+        /// <summary>
+        /// Gets the session object's Zero-Config MSI file path.
+        /// </summary>
+        public string DefaultMsiFile { get; }
+
+        /// <summary>
+        /// Gets the session object's Zero-Config MST file path.
+        /// </summary>
+        public string DefaultMstFile { get; }
+
+        /// <summary>
+        /// Gets the session object's Zero-Config MSP file paths.
+        /// </summary>
+        public string[] DefaultMspFiles { get; }
+
+        /// <summary>
+        /// Gets whether this session object found a valid Zero-Config MSI file.
+        /// </summary>
+        private bool UseDefaultMsi { get; }
+
+        /// <summary>
+        /// Gets the session object's Zero-Config MSP file paths.
+        /// </summary>
+        public string LogTempFolder { get; }
+
+        /// <summary>
+        /// Gets the session object's log filename.
+        /// </summary>
+        public string LogName { get; }
+
+
+        #endregion
+    }
+}
