0e97f01e9ccc23b8eb648c022df935ae24b99642
Mitchell Richters <mjr4077au@gmail.com>
2024-11-20 22:15:37 +1100

    - Ensure the exe ignores 60012 as an exit code (as this is our deferral code).

--- Sources/PSADT.Invoke/PSADT.Invoke/Invoke.cs
+++ Sources/PSADT.Invoke/PSADT.Invoke/Invoke.cs
@@ -165,7 +165,7 @@ namespace PSADT
                     process.Start();
                     process.WaitForExit();
                     exitCode = process.ExitCode;
-                    if ((exitCode == 1) || (exitCode == 64) || (exitCode == 255) || (exitCode >= 60000 && exitCode <= 79999))
+                    if ((exitCode == 1) || (exitCode == 64) || (exitCode == 255) || ((exitCode >= 60000) && (exitCode <= 79999) && (exitCode != 60012)))
                     {
                         throw new ApplicationException($"An error occurred while running {Path.GetFileName(adtFrontendPath)}. Exit code: {exitCode}");
                     }
