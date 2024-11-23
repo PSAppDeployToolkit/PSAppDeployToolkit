ed30ea109964ffa7d5bc8957e04bf432501cbcbb
Mitchell Richters <mjr4077au@gmail.com>
2024-11-23 11:49:27 +1100

    - Re-write `Send-Keys` into `Send-ADTKeys`.

--- Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Common/Public/Send-Keys.ps1
+++ Toolkit/AppDeployToolkit/PSAppDeployToolkit/Modules/PSAppDeployToolkit.Common/Public/Send-Keys.ps1
@@ -1,161 +1,159 @@
-﻿Function Send-Keys {
+﻿function Send-ADTKeys
+{
     <#
-.SYNOPSIS
 
-Send a sequence of keys to one or more application windows.
+    .SYNOPSIS
+    Send a sequence of keys to one or more application windows.
 
-.DESCRIPTION
+    .DESCRIPTION
+    Send a sequence of keys to one or more application window. If window title searched for returns more than one window, then all of them will receive the sent keys.
 
-Send a sequence of keys to one or more application window. If window title searched for returns more than one window, then all of them will receive the sent keys.
+    Function does not work in SYSTEM context unless launched with "psexec.exe -s -i" to run it as an interactive process under the SYSTEM account.
 
-Function does not work in SYSTEM context unless launched with "psexec.exe -s -i" to run it as an interactive process under the SYSTEM account.
+    .PARAMETER WindowTitle
+    The title of the application window to search for using regex matching.
 
-.PARAMETER WindowTitle
+    .PARAMETER GetAllWindowTitles
+    Get titles for all open windows on the system.
 
-The title of the application window to search for using regex matching.
+    .PARAMETER WindowHandle
+    Send keys to a specific window where the Window Handle is already known.
 
-.PARAMETER GetAllWindowTitles
+    .PARAMETER Keys
+    The sequence of keys to send. Info on Key input at: http://msdn.microsoft.com/en-us/library/System.Windows.Forms.SendKeys(v=vs.100).aspx
 
-Get titles for all open windows on the system.
+    .PARAMETER WaitSeconds
+    An optional number of seconds to wait after the sending of the keys.
 
-.PARAMETER WindowHandle
+    .INPUTS
+    None. You cannot pipe objects to this function.
 
-Send keys to a specific window where the Window Handle is already known.
+    .OUTPUTS
+    None. This function does not return any objects.
 
-.PARAMETER Keys
+    .EXAMPLE
+    # Send the sequence of keys "Hello world" to the application titled "foobar - Notepad".
+    Send-ADTKeys -WindowTitle 'foobar - Notepad' -Key 'Hello world'
 
-The sequence of keys to send. Info on Key input at: http://msdn.microsoft.com/en-us/library/System.Windows.Forms.SendKeys(v=vs.100).aspx
+    .EXAMPLE
+    # Send the sequence of keys "Hello world" to the application titled "foobar - Notepad" and wait 5 seconds.
+    Send-ADTKeys -WindowTitle 'foobar - Notepad' -Key 'Hello world' -WaitSeconds 5
 
-.PARAMETER WaitSeconds
+    .EXAMPLE
+    # Send the sequence of keys "Hello World" to the application with a Window Handle of '17368294'.
+    Send-ADTKeys -WindowHandle ([IntPtr]17368294) -Key 'Hello World'
 
-An optional number of seconds to wait after the sending of the keys.
+    .LINK
+    http://msdn.microsoft.com/en-us/library/System.Windows.Forms.SendKeys(v=vs.100).aspx
+    https://psappdeploytoolkit.com
 
-.INPUTS
+    #>
 
-None
-
-You cannot pipe objects to this function.
-
-.OUTPUTS
-
-None
-
-This function does not return any objects.
-
-.EXAMPLE
-
-Send-Keys -WindowTitle 'foobar - Notepad' -Key 'Hello world'
-
-Send the sequence of keys "Hello world" to the application titled "foobar - Notepad".
-
-.EXAMPLE
-
-Send-Keys -WindowTitle 'foobar - Notepad' -Key 'Hello world' -WaitSeconds 5
-
-Send the sequence of keys "Hello world" to the application titled "foobar - Notepad" and wait 5 seconds.
-
-.EXAMPLE
-
-Send-Keys -WindowHandle ([IntPtr]17368294) -Key 'Hello world'
-
-Send the sequence of keys "Hello world" to the application with a Window Handle of '17368294'.
-
-.NOTES
-
-.LINK
-
-http://msdn.microsoft.com/en-us/library/System.Windows.Forms.SendKeys(v=vs.100).aspx
-
-https://psappdeploytoolkit.com
-#>
     [CmdletBinding()]
-    Param (
+    param (
         [Parameter(Mandatory = $false, Position = 0)]
         [AllowEmptyString()]
         [ValidateNotNull()]
-        [String]$WindowTitle,
+        [System.String]$WindowTitle,
+
         [Parameter(Mandatory = $false, Position = 1)]
-        [ValidateNotNullorEmpty()]
-        [Switch]$GetAllWindowTitles = $false,
+        [System.Management.Automation.SwitchParameter]$GetAllWindowTitles,
+
         [Parameter(Mandatory = $false, Position = 2)]
-        [ValidateNotNullorEmpty()]
-        [IntPtr]$WindowHandle,
+        [ValidateNotNullOrEmpty()]
+        [System.IntPtr]$WindowHandle,
+
         [Parameter(Mandatory = $false, Position = 3)]
-        [ValidateNotNullorEmpty()]
-        [String]$Keys,
+        [ValidateNotNullOrEmpty()]
+        [System.String]$Keys,
+
         [Parameter(Mandatory = $false, Position = 4)]
-        [ValidateNotNullorEmpty()]
-        [Int32]$WaitSeconds
+        [ValidateNotNullOrEmpty()]
+        [System.Int32]$WaitSeconds
     )
 
-    Begin {
-        Write-ADTDebugHeader
-
-        [ScriptBlock]$SendKeys = {
-            Param (
-                [Parameter(Mandatory = $true)]
-                [ValidateNotNullorEmpty()]
-                [IntPtr]$WindowHandle
+    begin {
+        function Send-ADTKeysToWindow
+        {
+            [CmdletBinding()]
+            param (
+                [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+                [ValidateNotNullOrEmpty()]
+                [System.IntPtr]$WindowHandle
             )
-            Try {
-                ## Bring the window to the foreground
-                [Boolean]$IsBringWindowToFrontSuccess = [PSADT.UiAutomation]::BringWindowToFront($WindowHandle)
-                If (-not $IsBringWindowToFrontSuccess) {
-                    Throw 'Failed to bring window to foreground.'
+
+            try
+            {
+                # Bring the window to the foreground and make sure it's enabled.
+                if (![PSADT.UiAutomation]::BringWindowToFront($WindowHandle))
+                {
+                    $naerParams = @{
+                        Exception = [System.ApplicationException]::new('Failed to bring window to foreground.')
+                        Category = [System.Management.Automation.ErrorCategory]::InvalidResult
+                        ErrorId = 'WindowHandleForegroundError'
+                        TargetObject = $WindowHandle
+                        RecommendedAction = "Please check the status of this window and try again."
+                    }
+                    throw (New-ADTErrorRecord @naerParams)
+                }
+                if (![PSADT.UiAutomation]::IsWindowEnabled($WindowHandle))
+                {
+                    $naerParams = @{
+                        Exception = [System.ApplicationException]::new('Unable to send keys to window because it may be disabled due to a modal dialog being shown.')
+                        Category = [System.Management.Automation.ErrorCategory]::InvalidResult
+                        ErrorId = 'WindowHandleDisabledError'
+                        TargetObject = $WindowHandle
+                        RecommendedAction = "Please check the status of this window and try again."
+                    }
+                    throw (New-ADTErrorRecord @naerParams)
                 }
 
-                ## Send the Key sequence
-                If ($Keys) {
-                    If (-not [PSADT.UiAutomation]::IsWindowEnabled($WindowHandle)) {
-                        Throw 'Unable to send keys to window because it may be disabled due to a modal dialog being shown.'
-                    }
-                    Write-ADTLogEntry -Message "Sending key(s) [$Keys] to window title [$($Window.WindowTitle)] with window handle [$WindowHandle]."
-                    [Windows.Forms.SendKeys]::SendWait($Keys)
-                    If ($WaitSeconds) {
-                        Write-ADTLogEntry -Message "Sleeping for [$WaitSeconds] seconds."
-                        Start-Sleep -Seconds $WaitSeconds
-                    }
+                # Send the Key sequence.
+                Write-ADTLogEntry -Message "Sending key(s) [$Keys] to window title [$($Window.WindowTitle)] with window handle [$WindowHandle]."
+                [Windows.Forms.SendKeys]::SendWait($Keys)
+                if ($WaitSeconds)
+                {
+                    Write-ADTLogEntry -Message "Sleeping for [$WaitSeconds] seconds."
+                    Start-Sleep -Seconds $WaitSeconds
                 }
             }
-            Catch {
+            catch
+            {
                 Write-ADTLogEntry -Message "Failed to send keys to window title [$($Window.WindowTitle)] with window handle [$WindowHandle].`n$(Resolve-ADTError)" -Severity 3
             }
         }
+        Write-ADTDebugHeader
     }
-    Process {
-        Try {
-            If ($WindowHandle) {
-                [PSADT.Types.WindowInfo]$Window = Get-ADTWindowTitle -GetAllWindowTitles | Where-Object { $_.WindowHandle -eq $WindowHandle }
-                If (-not $Window) {
-                    Write-ADTLogEntry -Message "No windows with Window Handle [$WindowHandle] were discovered." -Severity 2
-                    Return
-                }
-                & $SendKeys -WindowHandle $Window.WindowHandle
-            }
-            Else {
-                [Hashtable]$GetWindowTitleSplat = @{}
-                If ($GetAllWindowTitles) {
-                    $GetWindowTitleSplat.Add( 'GetAllWindowTitles', $GetAllWindowTitles)
-                }
-                Else {
-                    $GetWindowTitleSplat.Add( 'WindowTitle', $WindowTitle)
-                }
-                [PSADT.Types.WindowInfo[]]$AllWindows = Get-ADTWindowTitle @GetWindowTitleSplat
-                If (-not $AllWindows) {
-                    Write-ADTLogEntry -Message 'No windows with the specified details were discovered.' -Severity 2
-                    Return
-                }
 
-                ForEach ($Window in $AllWindows) {
-                    & $SendKeys -WindowHandle $Window.WindowHandle
+    process {
+        try
+        {
+            if ($WindowHandle)
+            {
+                if (!($Window = Get-ADTWindowTitle -GetAllWindowTitles | Where-Object {$_.WindowHandle -eq $WindowHandle}))
+                {
+                    Write-ADTLogEntry -Message "No windows with Window Handle [$WindowHandle] were discovered." -Severity 2
+                    return
                 }
+                Send-ADTKeysToWindow -WindowHandle $Window.WindowHandle
+            }
+            else
+            {
+                if (!($AllWindows = if ($GetAllWindowTitles) {Get-ADTWindowTitle -GetAllWindowTitles $GetAllWindowTitles} else {Get-ADTWindowTitle -WindowTitle $WindowTitle}))
+                {
+                    Write-ADTLogEntry -Message 'No windows with the specified details were discovered.' -Severity 2
+                    return
+                }
+                $AllWindows | Send-ADTKeysToWindow
             }
         }
-        Catch {
+        catch
+        {
             Write-ADTLogEntry -Message "Failed to send keys to specified window.`n$(Resolve-ADTError)" -Severity 3
         }
     }
-    End {
+
+    end {
         Write-ADTDebugFooter
     }
 }
