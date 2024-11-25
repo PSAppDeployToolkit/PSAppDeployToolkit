12b23e05bdc62acef7011d106d7c40d4e82d274c
Mitchell Richters <mjr4077au@gmail.com>
2024-11-25 17:26:15 +1100

    - Rework `Import-ADTModuleDataFile` to handle registry overrides for values.
    Function and its internal worker have been rewritten to work in reverse. That is, rather than import the caller's config and add missing values from the defaults, use the defaults as a basis and add the caller's values in as overrides. Furthering this, add in the registry values as the final override.

---
+++ src/PSAppDeployToolkit/Private/Convert-RegistryKeyToHashtable.ps1
@@ -0,0 +1,65 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Convert-RegistryKeyToHashtable
+#
+#-----------------------------------------------------------------------------
+
+function Convert-RegistryKeyToHashtable
+{
+    begin
+    {
+        # Open collector to store all converted keys.
+        $data = @{}
+    }
+
+    process
+    {
+        # Process potential subkeys first.
+        $subdata = $_ | Get-ChildItem | & $MyInvocation.MyCommand
+
+        # Open a new subdata hashtable if we had no subkeys.
+        if ($null -eq $subdata)
+        {
+            $subdata = @{}
+        }
+
+        # Process this item and store its values.
+        $_ | Get-ItemProperty | & {
+            process
+            {
+                $_.PSObject.Properties | & {
+                    process
+                    {
+                        if (($_.Name -notmatch '^PS((Parent)?Path|ChildName|Provider)$') -and ![System.String]::IsNullOrWhiteSpace((Out-String -InputObject $_.Value)))
+                        {
+                            # Handle bools as string values.
+                            if ($_.Value -match '^(True|False)$')
+                            {
+                                $subdata.Add($_.Name, [System.Boolean]::Parse($_.Value))
+                            }
+                            else
+                            {
+                                $subdata.Add($_.Name, $_.Value)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        # Add the subdata to the sections if it's got a count.
+        if ($subdata.Count)
+        {
+            $data.Add($_.PSPath -replace '^.+\\', $subdata)
+        }
+    }
+
+    end
+    {
+        # If there's something in the collector, return it.
+        if ($data.Count)
+        {
+            return $data
+        }
+    }
+}
