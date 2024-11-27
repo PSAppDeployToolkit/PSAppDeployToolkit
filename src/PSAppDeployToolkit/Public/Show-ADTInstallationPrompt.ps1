dc6a021bf119597ec17aa50a9b04ea6c322f8755
Mitchell Richters <mjr4077au@gmail.com>
2024-11-27 20:33:30 +1100

    - Minimise the amount of `Resolve-ADTBoundParameters` usage within `Show-ADTInstallationPrompt`.

--- src/PSAppDeployToolkit/Public/Show-ADTInstallationPrompt.ps1
+++ src/PSAppDeployToolkit/Public/Show-ADTInstallationPrompt.ps1
@@ -205,11 +205,14 @@ function Show-ADTInstallationPrompt
                     return
                 }
 
+                # Resolve the bound parameters to a string.
+                $paramsString = $PSBoundParameters | Resolve-ADTBoundParameters
+
                 # If the NoWait parameter is specified, launch a new PowerShell session to show the prompt asynchronously.
                 if ($NoWait)
                 {
-                    Write-ADTLogEntry -Message "Displaying custom installation prompt asynchronously with the parameters: [$($PSBoundParameters | Resolve-ADTBoundParameters)]."
-                    Start-Process -FilePath (Get-ADTPowerShellProcessPath) -ArgumentList "$(if (!(Test-ADTModuleIsReleaseBuild)) { "-ExecutionPolicy Bypass " })-NonInteractive -NoProfile -NoLogo -WindowStyle Hidden -Command & (Import-Module -FullyQualifiedName @{ ModuleName = '$("$($Script:PSScriptRoot)\$($MyInvocation.MyCommand.Module.Name).psd1".Replace("'", "''"))'; Guid = '$($MyInvocation.MyCommand.Module.Guid)'; ModuleVersion = '$($MyInvocation.MyCommand.Module.Version)' } -PassThru) { & `$CommandTable.'Initialize-ADTModule' -ScriptDirectory '$($Script:ADT.Directories.Script.Replace("'", "''"))'; `$null = & `$CommandTable.'$($MyInvocation.MyCommand.Name)$($adtConfig.UI.DialogStyle)' $(($PSBoundParameters | Resolve-ADTBoundParameters -Exclude NoWait).Replace('"', '\"')) }" -WindowStyle Hidden -ErrorAction Ignore
+                    Write-ADTLogEntry -Message "Displaying custom installation prompt asynchronously with the parameters: [$paramsString]."
+                    Start-Process -FilePath (Get-ADTPowerShellProcessPath) -ArgumentList "$(if (!(Test-ADTModuleIsReleaseBuild)) { "-ExecutionPolicy Bypass " })-NonInteractive -NoProfile -NoLogo -WindowStyle Hidden -Command & (Import-Module -FullyQualifiedName @{ ModuleName = '$("$($Script:PSScriptRoot)\$($MyInvocation.MyCommand.Module.Name).psd1".Replace("'", "''"))'; Guid = '$($MyInvocation.MyCommand.Module.Guid)'; ModuleVersion = '$($MyInvocation.MyCommand.Module.Version)' } -PassThru) { & `$CommandTable.'Initialize-ADTModule' -ScriptDirectory '$($Script:ADT.Directories.Script.Replace("'", "''"))'; `$null = & `$CommandTable.'$($MyInvocation.MyCommand.Name)$($adtConfig.UI.DialogStyle)' $($paramsString.Replace('"', '\"')) }" -WindowStyle Hidden -ErrorAction Ignore
                     return
                 }
 
@@ -220,7 +223,7 @@ function Show-ADTInstallationPrompt
                 }
 
                 # Call the underlying function to open the message prompt.
-                Write-ADTLogEntry -Message "Displaying custom installation prompt with the parameters: [$($PSBoundParameters | Resolve-ADTBoundParameters)]."
+                Write-ADTLogEntry -Message "Displaying custom installation prompt with the parameters: [$paramsString]."
                 return & $Script:CommandTable."$($MyInvocation.MyCommand.Name)$($adtConfig.UI.DialogStyle)" @PSBoundParameters
             }
             catch
