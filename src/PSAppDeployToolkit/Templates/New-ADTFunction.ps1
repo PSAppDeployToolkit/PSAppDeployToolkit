e665b30728155052f8adc89723e9f74dac7f4cce
Mitchell Richters <mjr4077au@gmail.com>
2024-11-29 11:02:26 +1100

    - Add comment-based help to `New-ADTFunction` template.

--- src/PSAppDeployToolkit/Templates/New-ADTFunction.ps1
+++ src/PSAppDeployToolkit/Templates/New-ADTFunction.ps1
@@ -6,6 +6,40 @@
 
 function New-ADTFunction
 {
+    <#
+    .SYNOPSIS
+        Basis for a new PSAppDeployToolkit function.
+
+    .DESCRIPTION
+        This function serves as the basis for a new PSAppDeployToolkit function.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        New-ADTFunction
+
+        Invokes the New-ADTFunction function and returns any output.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt
+        Website: https://psappdeploytoolkit.com
+        Copyright: (C) 2024 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham, Muhammad Mashwani, Mitch Richters, Dan Gough).
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com
+    #>
+
     [CmdletBinding()]
     param
     (
