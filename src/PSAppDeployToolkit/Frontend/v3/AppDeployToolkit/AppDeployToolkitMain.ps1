0e361041be5062df1e9ef73b5e7ac3d781749bca
Mitchell Richters <mjr4077au@gmail.com>
2024-12-06 23:30:57 +1100

    - No need to write our own error out if `Import-Module` fails, it's error is good enough.

--- src/PSAppDeployToolkit/Frontend/v3/AppDeployToolkit/AppDeployToolkitMain.ps1
+++ src/PSAppDeployToolkit/Frontend/v3/AppDeployToolkit/AppDeployToolkitMain.ps1
@@ -5015,19 +5015,7 @@ else
     'PSAppDeployToolkit'
 }
 Remove-Module -Name PSAppDeployToolkit* -Force
-try
-{
-    $adtModule = Import-Module -FullyQualifiedName @{ ModuleName = $moduleName; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.2' } -Force -PassThru
-}
-catch
-{
-    $PSCmdlet.ThrowTerminatingError([System.Management.Automation.ErrorRecord]::new(
-            [System.IO.FileNotFoundException]::new("PSAppDeployToolkit module cannot be found."),
-            'ModuleNotFoundError',
-            [System.Management.Automation.ErrorCategory]::InvalidOperation,
-            $null
-        ))
-}
+$adtModule = Import-Module -FullyQualifiedName @{ ModuleName = $moduleName; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.2' } -Force -PassThru -ErrorAction Stop
 
 # Get all parameters from Open-ADTSession that are considered frontend params/variables.
 $sessionVars = $adtModule.ExportedCommands.'Open-ADTSession'.Parameters.Values | & {
