b0ddfd113602e61c790fb9860805fb820479ade0
Mitchell Richters <mjr4077au@gmail.com>
2024-12-12 15:00:44 +1100

    - Add `New-ADTExampleFunction` as an example function within `PSAppDeployToolkit.Extensions`.

--- src/PSAppDeployToolkit/Frontend/v4/PSAppDeployToolkit.Extensions/PSAppDeployToolkit.Extensions.psm1
+++ src/PSAppDeployToolkit/Frontend/v4/PSAppDeployToolkit.Extensions/PSAppDeployToolkit.Extensions.psm1
@@ -34,6 +34,69 @@ Set-StrictMode -Version 1
 ##* MARK: FUNCTION LISTINGS
 ##*===============================================
 
+function New-ADTExampleFunction
+{
+    <#
+    .SYNOPSIS
+        Basis for a new PSAppDeployToolkit extension function.
+
+    .DESCRIPTION
+        This function serves as the basis for a new PSAppDeployToolkit extension function.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        New-ADTExampleFunction
+
+        Invokes the New-ADTExampleFunction function and returns any output.
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+    )
+
+    begin
+    {
+        # Initialize function.
+        Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+
+    process
+    {
+        try
+        {
+            try
+            {
+            }
+            catch
+            {
+                # Re-writing the ErrorRecord with Write-Error ensures the correct PositionMessage is used.
+                Write-Error -ErrorRecord $_
+            }
+        }
+        catch
+        {
+            # Process the caught error, log it and throw depending on the specified ErrorAction.
+            Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_
+        }
+    }
+
+    end
+    {
+        # Finalize function.
+        Complete-ADTFunction -Cmdlet $PSCmdlet
+    }
+}
+
 
 ##*===============================================
 ##* MARK: SCRIPT BODY
