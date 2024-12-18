40d45a6492191b9e534a1dd3e9d70f3bd0d3f0fb
Mitchell Richters <mjr4077au@gmail.com>
2024-12-19 10:14:22 +1100

    - Change scoping of `$restrictedFunctions` in previous commit so its disposed after its used.

--- src/PSAppDeployToolkit/ImportsLast.ps1
+++ src/PSAppDeployToolkit/ImportsLast.ps1
@@ -10,10 +10,22 @@ Get-Item -LiteralPath $FunctionPaths | & { process { $CommandTable.Add($_.Name, 
 New-Variable -Name CommandTable -Value ([System.Collections.ObjectModel.ReadOnlyDictionary[System.String, System.Management.Automation.CommandInfo]]::new($CommandTable)) -Option Constant -Force -Confirm:$false
 
 # Remove all functions that invoke the UI from the export list if executed by Patch My PC ScriptRunner
-if (Get-ADTParentProcesses | & { process { if ($_.Name -eq 'PatchMyPC-ScriptRunner.exe' -and ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($_.ExecutablePath)).LegalCopyright -match 'Patch My PC') { return $true } } })
+if (Get-ADTParentProcesses | & { process { if (($_.Name -eq 'PatchMyPC-ScriptRunner.exe') -and ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($_.ExecutablePath).LegalCopyright -match 'Patch My PC')) { return $_ } } })
 {
-    $restrictedFunctions = @('Block-ADTAppExecution', 'Show-ADTBalloonTip', 'Show-ADTDialogBox', 'Show-ADTHelpConsole', 'Show-ADTInstallationProgress', 'Show-ADTInstallationPrompt', 'Show-ADTInstallationRestartPrompt', 'Show-ADTInstallationWelcome')
-    $Module.Manifest.FunctionsToExport = $Module.Manifest.FunctionsToExport | & { process { if (!$restrictedFunctions.Contains($_)) { return $_ } } }
+    $Module.Manifest.FunctionsToExport = $Module.Manifest.FunctionsToExport | & {
+        begin
+        {
+            $restrictedFunctions = @('Block-ADTAppExecution', 'Show-ADTBalloonTip', 'Show-ADTDialogBox', 'Show-ADTHelpConsole', 'Show-ADTInstallationProgress', 'Show-ADTInstallationPrompt', 'Show-ADTInstallationRestartPrompt', 'Show-ADTInstallationWelcome')
+        }
+
+        process
+        {
+            if (!$restrictedFunctions.Contains($_))
+            {
+                return $_
+            }
+        }
+    }
 }
 Export-ModuleMember -Function $Module.Manifest.FunctionsToExport
 
