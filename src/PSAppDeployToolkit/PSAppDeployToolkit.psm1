48895b36aec78a2196e7a320a25de71c352f2069
Mitchell Richters <mjr4077au@gmail.com>
2024-12-18 14:03:21 +1100

    - Extend previous commit to the unbuilt PSAppDeployToolkit.psm1 file as well.

--- src/PSAppDeployToolkit/PSAppDeployToolkit.psm1
+++ src/PSAppDeployToolkit/PSAppDeployToolkit.psm1
@@ -10,10 +10,10 @@
 # Dot-source our imports.
 if (!$Module.Compiled)
 {
-    & $CommandTable.'New-Variable' -Name ModuleFiles -Option Constant -Value ([System.IO.FileInfo[]]$([System.IO.Directory]::GetFiles("$PSScriptRoot\Private"); [System.IO.Directory]::GetFiles("$PSScriptRoot\Public")))
-    & $CommandTable.'New-Variable' -Name FunctionNames -Option Constant -Value ($ModuleFiles | & { process { return $_.BaseName } })
-    & $CommandTable.'New-Variable' -Name FunctionPaths -Option Constant -Value ($FunctionNames -replace '^', 'Microsoft.PowerShell.Core\Function::')
-    & $CommandTable.'Remove-Item' -LiteralPath $FunctionPaths -Force -ErrorAction Ignore
+    New-Variable -Name ModuleFiles -Option Constant -Value ([System.IO.FileInfo[]]$([System.IO.Directory]::GetFiles("$PSScriptRoot\Private"); [System.IO.Directory]::GetFiles("$PSScriptRoot\Public")))
+    New-Variable -Name FunctionNames -Option Constant -Value ($ModuleFiles | & { process { return $_.BaseName } })
+    New-Variable -Name FunctionPaths -Option Constant -Value ($FunctionNames -replace '^', 'Microsoft.PowerShell.Core\Function::')
+    Remove-Item -LiteralPath $FunctionPaths -Force -ErrorAction Ignore
     $ModuleFiles.FullName | . { process { . $_ } }
 }
 
