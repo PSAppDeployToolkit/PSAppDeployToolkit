bce925dedbbe53a9805e47ad48f84decf61f53ed
Mitchell Richters <mjr4077au@gmail.com>
2024-12-18 21:14:33 +1100

    - Relax the duplicate assembly checking stringency by allowing existing assemblies if the hashes match the currently importing module's assemblies.
    Suggested by @DanGough on the back of https://www.reddit.com/r/PSADT/comments/1hakvcl/comment/m2k7wox/.
    
    Also changes the `Write-Error` calls back to `throw` as it actually produces better output when `Import-Module` catches the ErrorRecord and re-throws it.

--- src/PSAppDeployToolkit/ImportsFirst.ps1
+++ src/PSAppDeployToolkit/ImportsFirst.ps1
@@ -83,53 +83,56 @@ New-Variable -Name Module -Option Constant -Force -Value ([ordered]@{
         Signed = (Get-AuthenticodeSignature -LiteralPath $MyInvocation.MyCommand.Path).Status.Equals([System.Management.Automation.SignatureStatus]::Valid)
     }).AsReadOnly()
 
-# Attempt to find the RuntimeAssembly object for PSADT.dll.
-New-Variable -Name RuntimeAssembly -Option Constant -Force -Value ([System.AppDomain]::CurrentDomain.GetAssemblies() | & { process { if ([System.IO.Path]::GetFileName($_.Location).Equals('PSADT.dll')) { return $_ } } } | Select-Object -First 1)
-
 # Import our assemblies, factoring in whether they're on a network share or not.
-if (!$RuntimeAssembly)
-{
-    # Process each assembly.
-    $Module.Assemblies | & {
-        begin
+$Module.Assemblies | & {
+    begin
+    {
+        # Cache loaded assemblies to test whether they're already loaded.
+        $domainAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+        # Determine whether we're on a network location.
+        $isNetworkLocation = [System.Uri]::new($PSScriptRoot).IsUnc -or ($PSScriptRoot -match '^([A-Za-z]:)\\' -and ((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$($Matches[1])'").ProviderName -match '^\\\\'))
+    }
+
+    process
+    {
+        # Test whether the assembly is already loaded.
+        if (($existingAssembly = $domainAssemblies | & { process { if ([System.IO.Path]::GetFileName($_.Location).Equals([System.IO.Path]::GetFileName($args[0]))) { return $_ } } } $_ | Select-Object -First 1))
         {
-            # Determine whether we're on a network location.
-            $isNetworkLocation = [System.Uri]::new($PSScriptRoot).IsUnc -or ($PSScriptRoot -match '^([A-Za-z]:)\\' -and ((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$($Matches[1])'").ProviderName -match '^\\\\'))
+            # Test the loaded assembly for SHA256 hash equality, returning early if the assembly is OK.
+            if (!(Get-FileHash -LiteralPath $existingAssembly.Location).Hash.Equals((Get-FileHash -LiteralPath $_).Hash))
+            {
+                throw [System.Management.Automation.ErrorRecord]::new(
+                    [System.InvalidOperationException]::new("A PSAppDeployToolkit assembly of a different file hash is already loaded. Please restart PowerShell and try again."),
+                    'ConflictingModuleLoaded',
+                    [System.Management.Automation.ErrorCategory]::InvalidOperation,
+                    $existingAssembly
+                )
+            }
+            return
         }
 
-        process
+        # If we're on a compiled build, confirm the DLLs are signed before proceeding.
+        if ($Module.Signed -and !($badFile = Get-AuthenticodeSignature -LiteralPath $_).Status.Equals([System.Management.Automation.SignatureStatus]::Valid))
         {
-            # If we're on a compiled build, confirm the DLLs are signed before proceeding.
-            if ($Module.Signed -and !($badFile = Get-AuthenticodeSignature -LiteralPath $_).Status.Equals([System.Management.Automation.SignatureStatus]::Valid))
-            {
-                Write-Error -ErrorRecord ([System.Management.Automation.ErrorRecord]::new(
-                        [System.InvalidOperationException]::new("The assembly [$_] has an invalid digital signature and cannot be loaded."),
-                        'ADTAssemblyFileSignatureError',
-                        [System.Management.Automation.ErrorCategory]::SecurityError,
-                        $badFile
-                    ))
-            }
+            throw [System.Management.Automation.ErrorRecord]::new(
+                [System.InvalidOperationException]::new("The assembly [$_] has an invalid digital signature and cannot be loaded."),
+                'ADTAssemblyFileSignatureError',
+                [System.Management.Automation.ErrorCategory]::SecurityError,
+                $badFile
+            )
+        }
 
-            # If loading from an SMB path, load unsafely. This is OK because in signed (release) modules, we're validating the signature above.
-            if ($isNetworkLocation)
-            {
-                [System.Reflection.Assembly]::UnsafeLoadFrom($_)
-            }
-            else
-            {
-                Add-Type -LiteralPath $_
-            }
+        # If loading from an SMB path, load unsafely. This is OK because in signed (release) modules, we're validating the signature above.
+        if ($isNetworkLocation)
+        {
+            [System.Reflection.Assembly]::UnsafeLoadFrom($_)
+        }
+        else
+        {
+            Add-Type -LiteralPath $_
         }
     }
-}
-elseif (!$RuntimeAssembly.Location.Equals(($Module.Assemblies | & { process { if ($_.EndsWith('\PSADT.dll')) { return $_ } } } | Select-Object -First 1)))
-{
-    Write-Error -ErrorRecord ([System.Management.Automation.ErrorRecord]::new(
-            [System.InvalidOperationException]::new("A duplicate PSAppDeployToolkit module is already loaded. Please restart PowerShell and try again."),
-            'ConflictingModuleLoaded',
-            [System.Management.Automation.ErrorCategory]::InvalidOperation,
-            $RuntimeAssembly
-        ))
 }
 
 # Set the process as HiDPI so long as we're in a real console.
