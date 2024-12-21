b8a4a9a4ed748e2b98a1c48ce495e17107c61348
Mitchell Richters <mjr4077au@gmail.com>
2024-12-21 23:44:54 +1100

    - Allow process filtration to be done on the process's command line, or the arguments provided on the command line.
    This does increase the expense of the operation. It would be faster without doing it via WMI, but it requires a lot more code and from my reading, WMI is safer in lower permission contexts.

--- src/PSAppDeployToolkit/Private/Get-ADTRunningProcesses.ps1
+++ src/PSAppDeployToolkit/Private/Get-ADTRunningProcesses.ps1
@@ -60,6 +60,9 @@ function Get-ADTRunningProcesses
         {
             # Cache all running processes.
             $allProcesses = Get-Process -Name $processNames -ErrorAction Ignore
+
+            # Cache process info from WMI as it gives us our command line and associated arguments.
+            $wmiProcesses = Get-CimInstance -ClassName Win32_Process
         }
 
         process
@@ -67,11 +70,14 @@ function Get-ADTRunningProcesses
             # Get all processes that matches the object and add in extra properties.
             $processes = foreach ($process in $allProcesses)
             {
-                # Continue if this isn't our process.
-                if ($process.Name -ne $_.Name)
+                # Continue if this isn't our process or it's ended since we cached it.
+                if (($process.Name -ne $_.Name) -or (!$process.Refresh() -and $process.HasExited))
                 {
                     continue
                 }
+
+                # Find the correspoding WMI process object.
+                $wmiProcess = $wmiProcesses | & { process { if ($_.ProcessId -eq $process.Id) { return $_ } } } | Select-Object -First 1
 
                 # Cache the process object's properties.
                 $procProps = $process.PSObject.Properties
@@ -92,6 +98,10 @@ function Get-ADTRunningProcesses
                     # Fall back on the process name if no description is provided by the process or as a parameter to the function.
                     $procProps.Add([System.Management.Automation.PSNoteProperty]::new('ProcessDescription', $process.ProcessName))
                 }
+
+                # Add in Arguments and the full CommandLine property from WMI.
+                $procProps.Add([System.Management.Automation.PSNoteProperty]::new('Arguments', $(if (![System.String]::IsNullOrWhiteSpace(($procArgs = $wmiProcess.CommandLine.Replace($wmiProcess.ExecutablePath, $null).TrimStart('"').Trim()))) { $procArgs } )))
+                $procProps.Add([System.Management.Automation.PSNoteProperty]::new('CommandLine', $wmiProcess.CommandLine.Trim()))
 
                 # Output the process object for collection.
                 $process
