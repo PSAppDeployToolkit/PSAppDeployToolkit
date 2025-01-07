9c42a70b120c812ec5d0c5f56b2c4fd288248506
Mitchell Richters <mjr4077au@gmail.com>
2025-01-07 18:53:47 +1100

    - Move static log utility methods out of `DeploymentSession` and into their own class.

--- src/PSAppDeployToolkit/Public/Write-ADTLogEntry.ps1
+++ src/PSAppDeployToolkit/Public/Write-ADTLogEntry.ps1
@@ -158,7 +158,7 @@ function Write-ADTLogEntry
         {
             if ([System.String]::IsNullOrWhiteSpace($Source))
             {
-                $Source = [PSADT.Module.DeploymentSession]::GetLogEntryCaller([System.Management.Automation.CallStackFrame[]](Get-PSCallStack)).Command
+                $Source = [PSADT.Module.DeploymentSession]::GetLogEntryCaller((Get-PSCallStack)).Command
             }
             $messages -replace '^', "[$([System.DateTime]::Now.ToString('O'))] [$Source] :: " | Write-Verbose
         }
