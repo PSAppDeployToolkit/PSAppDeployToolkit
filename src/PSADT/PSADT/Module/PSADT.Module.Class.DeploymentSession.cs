427e054c116ee9c39100f4f71e5fe65af6dd0489
Mitchell Richters <mjr4077au@gmail.com>
2025-01-07 16:44:37 +1100

    - Move session buffer adding/removing back to the Open/Close-ADTSession.
    Moving these into the C# code wasn't correct as a session could be added to the session buffer even if a derived class's constructor failed.

--- src/PSADT/PSADT/Module/PSADT.Module.Class.DeploymentSession.cs
+++ src/PSADT/PSADT/Module/PSADT.Module.Class.DeploymentSession.cs
@@ -805,9 +805,6 @@ namespace PSADT.Module
                     CallerSessionState = callerSessionState;
                 }
 
-                // We made it! Add this session to the module's session list for tracking.
-                InternalDatabase.GetSessionList().Add(this);
-
 
                 #endregion
             }
@@ -944,10 +941,6 @@ namespace PSADT.Module
             catch
             {
                 throw;
-            }
-            finally
-            {
-                InternalDatabase.GetSessionList().Remove(this);
             }
         }
 
