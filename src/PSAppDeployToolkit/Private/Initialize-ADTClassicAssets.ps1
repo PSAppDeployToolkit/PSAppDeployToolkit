052adda94913fc73956847b45a0111117656a6a5
Mitchell Richters <mjr4077au@gmail.com>
2025-01-08 23:06:29 +1100

    - Fix build system issues from previous commit.

--- src/PSAppDeployToolkit/Private/Initialize-ADTClassicAssets.ps1
+++ src/PSAppDeployToolkit/Private/Initialize-ADTClassicAssets.ps1
@@ -6,6 +6,7 @@
 
 function Initialize-ADTClassicAssets
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = "This function is appropriately named and we don't need PSScriptAnalyzer telling us otherwise.")]
     [CmdletBinding()]
     param
     (
