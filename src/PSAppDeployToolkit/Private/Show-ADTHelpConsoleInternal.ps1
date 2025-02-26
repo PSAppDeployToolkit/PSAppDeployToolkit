d9125984dcb02f24ef6862dc9ffeb49fc2bbacf7
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-02-26 19:45:49 +1100

    - Make the `Show-ADTHelpConsole` resizable.

--- src/PSAppDeployToolkit/Private/Show-ADTHelpConsoleInternal.ps1
+++ src/PSAppDeployToolkit/Private/Show-ADTHelpConsoleInternal.ps1
@@ -35,6 +35,8 @@ function Show-ADTHelpConsoleInternal
     $helpListBox.ClientSize = [System.Drawing.Size]::new(261, 675)
     $helpListBox.Font = [System.Drawing.SystemFonts]::MessageBoxFont
     $helpListBox.Location = [System.Drawing.Point]::new(3, 0)
+    $helpListBox.Anchor = "Top, Left, Bottom"
+    $helpListBox.IntegralHeight = $false
     $helpListBox.add_SelectedIndexChanged({ $helpTextBox.Text = [System.String]::Join("`n", ((Get-Help -Name $helpListBox.SelectedItem -Full | Out-String -Stream -Width ([System.Int32]::MaxValue)) -replace '^\s+$').TrimEnd()).Trim().Replace('<br />', $null) })
     $null = $helpListBox.Items.AddRange(($module.ExportedCommands.Keys | Sort-Object))
 
@@ -43,6 +45,7 @@ function Show-ADTHelpConsoleInternal
     $helpTextBox.ClientSize = [System.Drawing.Size]::new(1250, 675)
     $helpTextBox.Font = [System.Drawing.Font]::new('Consolas', 9)
     $helpTextBox.Location = [System.Drawing.Point]::new(271, 0)
+    $helpTextBox.Anchor = "Top, Left, Right, Bottom"
     $helpTextBox.ReadOnly = $true
     $helpTextBox.WordWrap = $false
 
@@ -53,9 +56,9 @@ function Show-ADTHelpConsoleInternal
     $helpForm.Font = [System.Drawing.SystemFonts]::MessageBoxFont
     $helpForm.AutoScaleDimensions = [System.Drawing.SizeF]::new(7, 15)
     $helpForm.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
-    $helpForm.AutoSize = $true
-    $helpForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Fixed3D
-    $helpForm.MaximizeBox = $false
+    $helpForm.ClientSize = [System.Drawing.Size]::new(1524, 679)
+    $helpForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+    $helpForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
     $helpForm.Controls.Add($helpListBox)
     $helpForm.Controls.Add($helpTextBox)
     $helpForm.ResumeLayout()
