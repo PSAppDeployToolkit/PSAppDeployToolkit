a92260af712adf658ba73731ba76198d7653708d
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-02-27 16:33:39 +1100

    - Place `Show-ADTHelpConsole` controls into panels so we can better control the borders.
    This removes the RichTextBox's default Fixed3D border, which is otherwise uncontrollable. https://stackoverflow.com/questions/700479/how-do-i-give-the-richtextbox-a-flat-look

--- src/PSAppDeployToolkit/Private/Show-ADTHelpConsoleInternal.ps1
+++ src/PSAppDeployToolkit/Private/Show-ADTHelpConsoleInternal.ps1
@@ -30,22 +30,30 @@ function Show-ADTHelpConsoleInternal
     # Import the module and store its passthru data so we can access it later.
     $module = Import-Module -FullyQualifiedName ([Microsoft.PowerShell.Commands.ModuleSpecification]::new($PSBoundParameters)) -PassThru
 
-    # Build out the form's listbox.
-    $helpListBox = [System.Windows.Forms.ListBox]::new()
-    $helpListBox.ClientSize = [System.Drawing.Size]::new(281, 556)
+    # Build out a panel to hold the list box (flattens border)
+    $helpListPanel = [System.Windows.Forms.Panel]::new()
+    $helpListPanel.ClientSize = [System.Drawing.Size]::new(281, 559)
+    $helpListPanel.Location = [System.Drawing.Point]::new(3, 0)
+    $helpListPanel.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
+    $helpListPanel.Anchor = "Top, Left, Bottom"
+    $helpListPanel.Controls.Add(($helpListBox = [System.Windows.Forms.ListBox]::new()))
+    $helpListBox.BorderStyle = [System.Windows.Forms.BorderStyle]::None
     $helpListBox.Font = [System.Drawing.SystemFonts]::MessageBoxFont
-    $helpListBox.Location = [System.Drawing.Point]::new(3, 0)
-    $helpListBox.Anchor = "Top, Left, Bottom"
+    $helpListBox.Dock = [System.Windows.Forms.DockStyle]::Fill
     $helpListBox.IntegralHeight = $false
     $helpListBox.add_SelectedIndexChanged({ $helpTextBox.Text = [System.String]::Join("`n", ((Get-Help -Name $helpListBox.SelectedItem -Full | Out-String -Stream -Width ([System.Int32]::MaxValue)) -replace '^\s+$').TrimEnd()).Trim().Replace('<br />', $null) })
     $null = $helpListBox.Items.AddRange(($module.ExportedCommands.Keys | Sort-Object))
 
-    # Build out the form's textbox.
-    $helpTextBox = [System.Windows.Forms.RichTextBox]::new()
-    $helpTextBox.ClientSize = [System.Drawing.Size]::new(1030, 556)
+    # Build out a panel to hold the rich text box (flattens border)
+    $helpTextPanel = [System.Windows.Forms.Panel]::new()
+    $helpTextPanel.ClientSize = [System.Drawing.Size]::new(1034, 559)
+    $helpTextPanel.Location = [System.Drawing.Point]::new(287, 0)
+    $helpTextPanel.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
+    $helpTextPanel.Anchor = "Top, Left, Right, Bottom"
+    $helpTextPanel.Controls.Add(($helpTextBox = [System.Windows.Forms.RichTextBox]::new()))
+    $helpTextBox.BorderStyle = [System.Windows.Forms.BorderStyle]::None
     $helpTextBox.Font = [System.Drawing.Font]::new('Consolas', 9)
-    $helpTextBox.Location = [System.Drawing.Point]::new(291, 0)
-    $helpTextBox.Anchor = "Top, Left, Right, Bottom"
+    $helpTextBox.Dock = [System.Windows.Forms.DockStyle]::Fill
     $helpTextBox.ReadOnly = $true
     $helpTextBox.WordWrap = $false
 
@@ -56,11 +64,11 @@ function Show-ADTHelpConsoleInternal
     $helpForm.Font = [System.Drawing.SystemFonts]::MessageBoxFont
     $helpForm.AutoScaleDimensions = [System.Drawing.SizeF]::new(7, 15)
     $helpForm.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
-    $helpForm.ClientSize = [System.Drawing.Size]::new(1327, 562)
+    $helpForm.ClientSize = [System.Drawing.Size]::new(1324, 562)
     $helpForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
     $helpForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
-    $helpForm.Controls.Add($helpListBox)
-    $helpForm.Controls.Add($helpTextBox)
+    $helpForm.Controls.Add($helpListPanel)
+    $helpForm.Controls.Add($helpTextPanel)
     $helpForm.ResumeLayout()
 
     # Show the form. Using Application.Run automatically manages disposal for us.
