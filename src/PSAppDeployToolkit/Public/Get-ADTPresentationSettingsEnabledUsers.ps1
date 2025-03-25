5a9fe5020e7f6ed11329bbced9b83e2ae12873f2
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-03-25 17:00:46 +1100

    - Repair setup within "- Don't bomb out in `Get-ADTPresentationSettingsEnabledUsers` if there's no user profiles to cycle through.".
    This function needs to return null when there's nothing.

--- src/PSAppDeployToolkit/Public/Get-ADTPresentationSettingsEnabledUsers.ps1
+++ src/PSAppDeployToolkit/Public/Get-ADTPresentationSettingsEnabledUsers.ps1
@@ -65,15 +65,8 @@ function Get-ADTPresentationSettingsEnabledUsers
                     UserProfiles = Get-ADTUserProfiles -ExcludeDefaultUser -InformationAction SilentlyContinue
                 }
 
-                # Return early if there's no user profiles to cycle through (brand new machine, etc).
-                if (!$iaauraParams.UserProfiles)
-                {
-                    Write-ADTLogEntry -Message "There are no user accounts found on this device."
-                    return $false
-                }
-
                 # Return UserProfile objects for each user with "I am currently giving a presentation" enabled.
-                if (($usersInPresentationMode = Invoke-ADTAllUsersRegistryAction @iaauraParams -SkipUnloadedProfiles -InformationAction SilentlyContinue))
+                if ($iaauraParams.UserProfiles -and ($usersInPresentationMode = Invoke-ADTAllUsersRegistryAction @iaauraParams -SkipUnloadedProfiles -InformationAction SilentlyContinue))
                 {
                     Write-ADTLogEntry -Message "The following users are currently in presentation mode: ['$([System.String]::Join("', '", $usersInPresentationMode.NTAccount))']."
                     return $usersInPresentationMode
