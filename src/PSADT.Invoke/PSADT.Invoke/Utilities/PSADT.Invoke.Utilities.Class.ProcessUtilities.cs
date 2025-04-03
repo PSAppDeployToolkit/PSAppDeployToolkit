a38e80f9d13f127b0999d8efa0db5fc29329653b
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-04 10:30:14 +1100

    - Remove unused overloads from PSADT.Invoke.Utilities.ProcessUtilities class.

--- src/PSADT.Invoke/PSADT.Invoke/Utilities/PSADT.Invoke.Utilities.Class.ProcessUtilities.cs
+++ src/PSADT.Invoke/PSADT.Invoke/Utilities/PSADT.Invoke.Utilities.Class.ProcessUtilities.cs
@@ -1,40 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using PSADT.Invoke.LibraryInterfaces;
 
 namespace PSADT.Invoke.Utilities
 {
+    /// <summary>
+    /// Utility class for working with Process objects.
+    /// </summary>
     internal static class ProcessUtilities
     {
-        /// <summary>
-        /// Gets the parent process of the current process.
-        /// </summary>
-        /// <returns>An instance of the Process class.</returns>
-        internal static Process GetParentProcess()
-        {
-            return GetParentProcess(Process.GetCurrentProcess().Handle);
-        }
-
-        /// <summary>
-        /// Gets the parent process of specified process.
-        /// </summary>
-        /// <param name="id">The process id.</param>
-        /// <returns>An instance of the Process class.</returns>
-        internal static Process GetParentProcess(int id)
-        {
-            return GetParentProcess(Process.GetProcessById(id).Handle);
-        }
-
         /// <summary>
         /// Gets the parent process of a specified process.
         /// </summary>
         /// <param name="handle">The process handle.</param>
         /// <returns>An instance of the Process class.</returns>
-        internal static Process GetParentProcess(IntPtr handle)
+        internal static Process GetParentProcess(Process proc)
         {
-            NtDll.NtQueryInformationProcess(handle, PROCESSINFOCLASS.ProcessBasicInformation, out var pbi);
+            NtDll.NtQueryInformationProcess(proc.Handle, PROCESSINFOCLASS.ProcessBasicInformation, out var pbi);
             return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
         }
 
@@ -44,13 +27,13 @@ namespace PSADT.Invoke.Utilities
         /// <returns>An list of instances of the Process class.</returns>
         internal static List<Process> GetParentProcesses()
         {
-            List<Process> procs = [];
             var proc = Process.GetCurrentProcess();
+            List<Process> procs = [];
             while (true)
             {
                 try
                 {
-                    if (procs.Contains((proc = GetParentProcess(proc.Handle))))
+                    if (procs.Contains(proc = GetParentProcess(proc)))
                     {
                         break;
                     }
