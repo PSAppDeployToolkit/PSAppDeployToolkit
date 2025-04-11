6b03f6dd64f4f444a2a2676e9bfd8283dfb01749
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-11 16:23:55 +1000

    - Remove some of the copyright fluff the extension files.
    The file is there for people to extend with their own code, they're free to license their own code with any license compatible with the LGPL that they wish, or just not license it at all.

--- src/PSAppDeployToolkit/Frontend/v4/PSAppDeployToolkit.Extensions/PSAppDeployToolkit.Extensions.psm1
+++ src/PSAppDeployToolkit/Frontend/v4/PSAppDeployToolkit.Extensions/PSAppDeployToolkit.Extensions.psm1
@@ -8,16 +8,6 @@ This module is a template that allows you to extend the toolkit with your own cu
 
 This module is imported by the Invoke-AppDeployToolkit.ps1 script which is used when installing or uninstalling an application.
 
-PSAppDeployToolkit is licensed under the GNU LGPLv3 License - © 2025 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham, Muhammad Mashwani, Mitch Richters, Dan Gough).
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-.LINK
-https://psappdeploytoolkit.com
-
 #>
 
 ##*===============================================
