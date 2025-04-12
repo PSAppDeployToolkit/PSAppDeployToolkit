1a6e593e46541cee254e8c0ee41a0022dfc9cfe8
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-12 17:09:57 +1000

    - Ensure FileHandleManager.GetOpenHandles() always frees its memory even if it fails.

--- src/PSADT/PSADT/FileSystem/PSADT.FileSystem.Class.FileHandleManager.cs
+++ src/PSADT/PSADT/FileSystem/PSADT.FileSystem.Class.FileHandleManager.cs
@@ -46,87 +46,89 @@ namespace PSADT.FileSystem
                 throw ExceptionUtilities.GetExceptionForLastWin32Error((WIN32_ERROR)PInvoke.RtlNtStatusToDosError(status));
             }
 
-            // Read the number of handles from the buffer.
-            var handleCount = Marshal.PtrToStructure<NtDll.SYSTEM_HANDLE_INFORMATION_EX>(handleBufferPtr).NumberOfHandles.ToUInt64();
-            var handleEntryPtr = handleBufferPtr + handleInfoExSize;
-
-            // Process all found handles.
-            var openHandles = new List<FileHandleInfo>();
-            for (ulong i = 0; i < handleCount; i++)
+            // Process all handles and return a read-only list of the ones matching our directory filter.
+            try
             {
-                // Read the handle information into a structure.
-                var sysHandle = Marshal.PtrToStructure<NtDll.SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX>(handleEntryPtr);
-                handleEntryPtr += handleEntryExSize;
+                var handleCount = Marshal.PtrToStructure<NtDll.SYSTEM_HANDLE_INFORMATION_EX>(handleBufferPtr).NumberOfHandles.ToUInt64();
+                var handleEntry = handleBufferPtr + handleInfoExSize;
+                var openHandles = new List<FileHandleInfo>();
+                for (ulong i = 0; i < handleCount; i++)
+                {
+                    // Read the handle information into a structure.
+                    var sysHandle = Marshal.PtrToStructure<NtDll.SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX>(handleEntry);
+                    handleEntry += handleEntryExSize;
 
-                // Skip this handle if it's not a file or directory handle.
-                if (!ObjectTypeLookupTable.TryGetValue(sysHandle.ObjectTypeIndex, out string? objectType) || (objectType != "File" && objectType != "Directory"))
-                {
-                    continue;
-                }
-
-                // Filter out handles that are known to cause NtQueryObject to hang.
-                if ((sysHandle.GrantedAccess == 0x00120189 && (sysHandle.HandleAttributes == 0 || sysHandle.HandleAttributes == 2)) ||
-                    (sysHandle.GrantedAccess == 0x0012019F && (sysHandle.HandleAttributes == 0 || sysHandle.HandleAttributes == 2)) ||
-                    (sysHandle.GrantedAccess == 0x001A019F && (sysHandle.HandleAttributes == 2)))
-                {
-                    continue;
-                }
-
-                // Open the owning process with rights to duplicate handles.
-                HANDLE processHandle;
-                try
-                {
-                    processHandle = Kernel32.OpenProcess(PROCESS_ACCESS_RIGHTS.PROCESS_DUP_HANDLE, false, sysHandle.UniqueProcessId.ToUInt32());
-                }
-                catch (UnauthorizedAccessException ex) when (ex.HResult == HRESULT.E_ACCESSDENIED)
-                {
-                    continue;
-                }
-
-                // Duplicate the remote handle into our process.
-                HANDLE localHandle;
-                try
-                {
-                    Kernel32.DuplicateHandle(processHandle, (HANDLE)sysHandle.HandleValue, PInvoke.GetCurrentProcess(), out localHandle, 0, true, DUPLICATE_HANDLE_OPTIONS.DUPLICATE_SAME_ACCESS);
-                }
-                catch (Win32Exception ex) when ((ex.NativeErrorCode == (int)WIN32_ERROR.ERROR_NOT_SUPPORTED) || (ex.NativeErrorCode == (int)WIN32_ERROR.ERROR_INVALID_HANDLE))
-                {
-                    continue;
-                }
-                catch (UnauthorizedAccessException ex) when (ex.HResult == HRESULT.E_ACCESSDENIED)
-                {
-                    continue;
-                }
-                finally
-                {
-                    Kernel32.CloseHandle(ref processHandle);
-                }
-
-                // Get the handle's name to check if it's a hard drive path.
-                string? objectName;
-                try
-                {
-                    objectName = GetObjectInformation(localHandle, OBJECT_INFORMATION_CLASS.ObjectNameInformation);
-                    if (string.IsNullOrWhiteSpace(objectName) || !objectName!.StartsWith("\\Device\\HarddiskVolume"))
+                    // Skip this handle if it's not a file or directory handle.
+                    if (!ObjectTypeLookupTable.TryGetValue(sysHandle.ObjectTypeIndex, out string? objectType) || (objectType != "File" && objectType != "Directory"))
                     {
                         continue;
                     }
-                }
-                finally
-                {
-                    Kernel32.CloseHandle(ref localHandle);
-                }
 
-                // Add the handle information to the list if it matches the specified directory path.
-                if (FileSystemUtilities.ConvertNtPathToDosPath(objectName) is string dosPath && (null == directoryPath || dosPath.StartsWith(directoryPath, StringComparison.OrdinalIgnoreCase)))
-                {
-                    openHandles.Add(new FileHandleInfo(sysHandle, dosPath, objectName, objectType));
-                }
+                    // Filter out handles that are known to cause NtQueryObject to hang.
+                    if ((sysHandle.GrantedAccess == 0x00120189 && (sysHandle.HandleAttributes == 0 || sysHandle.HandleAttributes == 2)) ||
+                        (sysHandle.GrantedAccess == 0x0012019F && (sysHandle.HandleAttributes == 0 || sysHandle.HandleAttributes == 2)) ||
+                        (sysHandle.GrantedAccess == 0x001A019F && (sysHandle.HandleAttributes == 2)))
+                    {
+                        continue;
+                    }
+
+                    // Open the owning process with rights to duplicate handles.
+                    HANDLE processHandle;
+                    try
+                    {
+                        processHandle = Kernel32.OpenProcess(PROCESS_ACCESS_RIGHTS.PROCESS_DUP_HANDLE, false, sysHandle.UniqueProcessId.ToUInt32());
+                    }
+                    catch (UnauthorizedAccessException ex) when (ex.HResult == HRESULT.E_ACCESSDENIED)
+                    {
+                        continue;
+                    }
+
+                    // Duplicate the remote handle into our process.
+                    HANDLE localHandle;
+                    try
+                    {
+                        Kernel32.DuplicateHandle(processHandle, (HANDLE)sysHandle.HandleValue, PInvoke.GetCurrentProcess(), out localHandle, 0, true, DUPLICATE_HANDLE_OPTIONS.DUPLICATE_SAME_ACCESS);
+                    }
+                    catch (Win32Exception ex) when ((ex.NativeErrorCode == (int)WIN32_ERROR.ERROR_NOT_SUPPORTED) || (ex.NativeErrorCode == (int)WIN32_ERROR.ERROR_INVALID_HANDLE))
+                    {
+                        continue;
+                    }
+                    catch (UnauthorizedAccessException ex) when (ex.HResult == HRESULT.E_ACCESSDENIED)
+                    {
+                        continue;
+                    }
+                    finally
+                    {
+                        Kernel32.CloseHandle(ref processHandle);
+                    }
+
+                    // Get the handle's name to check if it's a hard drive path.
+                    string? objectName;
+                    try
+                    {
+                        objectName = GetObjectInformation(localHandle, OBJECT_INFORMATION_CLASS.ObjectNameInformation);
+                        if (string.IsNullOrWhiteSpace(objectName) || !objectName!.StartsWith("\\Device\\HarddiskVolume"))
+                        {
+                            continue;
+                        }
+                    }
+                    finally
+                    {
+                        Kernel32.CloseHandle(ref localHandle);
+                    }
+
+                    // Add the handle information to the list if it matches the specified directory path.
+                    if (FileSystemUtilities.ConvertNtPathToDosPath(objectName) is string dosPath && (null == directoryPath || dosPath.StartsWith(directoryPath, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        openHandles.Add(new FileHandleInfo(sysHandle, dosPath, objectName, objectType));
+                    }
+                }                
+                return openHandles.AsReadOnly();
             }
-
-            // Free the allocated memory and return the results.
-            Marshal.FreeHGlobal(handleBufferPtr);
-            return openHandles.AsReadOnly();
+            finally
+            {
+                Marshal.FreeHGlobal(handleBufferPtr);
+            }
         }
 
         /// <summary>
