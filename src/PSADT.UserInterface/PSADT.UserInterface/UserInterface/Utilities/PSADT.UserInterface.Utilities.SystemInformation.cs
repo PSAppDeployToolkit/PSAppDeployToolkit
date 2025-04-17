4047fee13302df738e44fb37a7ea09a19cdb184e
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-17 10:41:50 +1000

    - Move `SystemInformation` class into its own file, where it belongs.

---
+++ src/PSADT.UserInterface/PSADT.UserInterface/UserInterface/Utilities/PSADT.UserInterface.Utilities.SystemInformation.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace PSADT.UserInterface.Utilities
+{
+    /// <summary>
+    /// Provides information about the system.
+    /// </summary>
+    internal static class SystemInformation
+    {
+        /// <summary>
+        /// Gets the bounds of the virtual screen in device pixels.
+        /// </summary>
+        internal static Rect VirtualScreen
+        {
+            get
+            {
+                int width = NativeMethods.GetSystemMetrics(NativeMethods.SM_CXVIRTUALSCREEN);
+                int height = NativeMethods.GetSystemMetrics(NativeMethods.SM_CYVIRTUALSCREEN);
+                int left = NativeMethods.GetSystemMetrics(NativeMethods.SM_XVIRTUALSCREEN);
+                int top = NativeMethods.GetSystemMetrics(NativeMethods.SM_YVIRTUALSCREEN);
+                return new Rect(left, top, width, height);
+            }
+        }
+
+        /// <summary>
+        /// Gets the working area of the primary display in device pixels.
+        /// </summary>
+        internal static Rect WorkingArea
+        {
+            get
+            {
+                var rc = new NativeMethods.RECT();
+                NativeMethods.SystemParametersInfo(NativeMethods.SPI_GETWORKAREA, 0, ref rc, 0);
+                return new Rect(rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top);
+            }
+        }
+    }
+}
--- src/PSADT.UserInterface/PSADT.UserInterface/UserInterface/Utilities/PSADT.UserInterface.Utilities.WPFScreen.cs
+++ src/PSADT.UserInterface/PSADT.UserInterface/UserInterface/Utilities/PSADT.UserInterface.Utilities.WPFScreen.cs
@@ -341,38 +341,4 @@ namespace PSADT.UserInterface.Utilities
         /// </summary>
         private const int PRIMARY_MONITOR = unchecked((int)0xBAADF00D);
     }
-
-    /// <summary>
-    /// Provides information about the system.
-    /// </summary>
-    public static class SystemInformation
-    {
-        /// <summary>
-        /// Gets the bounds of the virtual screen in device pixels.
-        /// </summary>
-        public static Rect VirtualScreen
-        {
-            get
-            {
-                int width = NativeMethods.GetSystemMetrics(NativeMethods.SM_CXVIRTUALSCREEN);
-                int height = NativeMethods.GetSystemMetrics(NativeMethods.SM_CYVIRTUALSCREEN);
-                int left = NativeMethods.GetSystemMetrics(NativeMethods.SM_XVIRTUALSCREEN);
-                int top = NativeMethods.GetSystemMetrics(NativeMethods.SM_YVIRTUALSCREEN);
-                return new Rect(left, top, width, height);
-            }
-        }
-
-        /// <summary>
-        /// Gets the working area of the primary display in device pixels.
-        /// </summary>
-        public static Rect WorkingArea
-        {
-            get
-            {
-                var rc = new NativeMethods.RECT();
-                NativeMethods.SystemParametersInfo(NativeMethods.SPI_GETWORKAREA, 0, ref rc, 0);
-                return new Rect(rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top);
-            }
-        }
-    }
 }
