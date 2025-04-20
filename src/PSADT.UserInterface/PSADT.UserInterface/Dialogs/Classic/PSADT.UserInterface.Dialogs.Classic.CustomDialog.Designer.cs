21c86977f74ed9bb9b56ebc60471ff007eb30dee
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-20 21:01:03 +1000

    - Scaffold out the body for the custom dialog (`Show-ADTInstallationPrompt`).

---
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.CustomDialog.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace PSADT.UserInterface.Dialogs.Classic
+{
+    partial class CustomDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CustomDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.ClientSize = new System.Drawing.Size(450, 63);
+            this.Name = "CustomDialog";
+            this.Text = "CustomDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
