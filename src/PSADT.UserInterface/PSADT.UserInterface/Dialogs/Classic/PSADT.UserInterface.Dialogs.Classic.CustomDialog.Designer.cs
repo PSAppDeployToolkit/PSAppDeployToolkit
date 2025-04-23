6247a52e6e915df69b8b44e380dae236580776c1
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-23 21:28:25 +1000

    - Don't fight the .resx creation nonsense, just let it happen.

--- src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.CustomDialog.Designer.cs
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.CustomDialog.Designer.cs
@@ -82,7 +82,7 @@ namespace PSADT.UserInterface.Dialogs.Classic
             this.labelMessage.Name = "labelMessage";
             this.labelMessage.Size = new System.Drawing.Size(317, 360);
             this.labelMessage.TabIndex = 1;
-            this.labelMessage.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            this.labelMessage.Text = resources.GetString("labelMessage.Text");
             // 
             // buttonTableLayoutPanel
             // 
