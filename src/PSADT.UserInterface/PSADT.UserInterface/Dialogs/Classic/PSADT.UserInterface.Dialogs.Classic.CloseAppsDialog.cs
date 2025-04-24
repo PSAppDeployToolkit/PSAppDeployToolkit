faa67cc385dcb0d5c68c9ab7d40b448bd5df95eb
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-24 13:33:45 +1000

    - Scaffold out the remaining classic dialog windows.
    That's right, we're going to make this WinForms to match the rest 🤘

---
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.CloseAppsDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSADT.UserInterface.Dialogs.Classic
+{
+    public partial class CloseAppsDialog : AbortableDialog
+    {
+        public CloseAppsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.ProgressDialog.Designer.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Forms;
+
+namespace PSADT.UserInterface.Dialogs.Classic
+{
+    partial class ProgressDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(450, 0);
+            this.Name = "ProgressDialog";
+            this.Text = "ProgressDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+    }
+}
