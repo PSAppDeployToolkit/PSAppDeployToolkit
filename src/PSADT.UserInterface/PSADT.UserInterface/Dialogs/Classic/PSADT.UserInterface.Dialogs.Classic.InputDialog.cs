e4a2bae06298d62e22188dcf26f546e325c29d52
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-25 11:05:27 +1000

    - Add in a stub for a classic InputDialog so we can throw properly.

---
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.InputDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PSADT.UserInterface.Dialogs.Classic
+{
+    /// <summary>
+    /// Abortable classic dialog form.
+    /// </summary>
+    public partial class InputDialog : ClassicDialog
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputDialog"/> class.
+        /// </summary>
+        public InputDialog() : this(default!)
+        {
+            if (LicenseManager.UsageMode == LicenseUsageMode.Runtime)
+            {
+                throw new InvalidOperationException("This constructor cannot be used in runtime mode.");
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputDialog"/> class with the specified options.
+        /// </summary>
+        /// <param name="options"></param>
+        public InputDialog(DialogOptions options) : base(options)
+        {
+            if (LicenseManager.UsageMode == LicenseUsageMode.Runtime)
+            {
+                throw new NotImplementedException("The InputDialog is only implemented for the Fluent dialog type.");
+            }
+            InitializeComponent();
+        }
+    }
+}
