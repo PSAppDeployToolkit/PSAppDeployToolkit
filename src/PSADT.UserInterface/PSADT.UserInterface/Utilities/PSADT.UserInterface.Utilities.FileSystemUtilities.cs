ae4e3952cb06ab281d12d418daac697e6597ec58
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-04-29 00:23:16 +1000

    - Clean up unrequired using statements within FileSystemUtilities file.

--- src/PSADT.UserInterface/PSADT.UserInterface/Utilities/PSADT.UserInterface.Utilities.FileSystemUtilities.cs
+++ src/PSADT.UserInterface/PSADT.UserInterface/Utilities/PSADT.UserInterface.Utilities.FileSystemUtilities.cs
@@ -1,10 +1,7 @@
-﻿using PSADT.UserInterface.LibraryInterfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PSADT.UserInterface.LibraryInterfaces;
 
 namespace PSADT.UserInterface.Utilities
 {
