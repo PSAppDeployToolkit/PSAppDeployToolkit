042117146e54758bcc6fa11fbe0a58f70319bc28
Dan Gough <goughdj@gmail.com>
2025-05-23 20:58:15 +0100

    Add Get/Set/Remove-ADTIniSection

---
+++ src/PSAppDeployToolkit/Public/Get-ADTIniSection.ps1
@@ -0,0 +1,105 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Get-ADTIniSection
+#
+#-----------------------------------------------------------------------------
+
+function Get-ADTIniSection
+{
+    <#
+    .SYNOPSIS
+        Parses an INI file and returns the specified section as an ordered hashtable of key value pairs.
+
+    .DESCRIPTION
+        Parses an INI file and returns the specified section as an ordered hashtable of key value pairs.
+
+    .PARAMETER FilePath
+        Path to the INI file.
+
+    .PARAMETER Section
+        Section within the INI file.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        Collections.Specialized.OrderedDictionary
+
+        Returns the value of the specified section and key.
+
+    .EXAMPLE
+        Get-ADTIniSection -FilePath "$env:ProgramFilesX86\IBM\Notes\notes.ini" -Section 'Notes'
+
+        This example retrieves the section of the 'Notes' of the specified INI file.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt<br />
+        Website: https://psappdeploytoolkit.com<br />
+        Copyright: (C) 2025 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham, Muhammad Mashwani, Mitch Richters, Dan Gough).<br />
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com/docs/reference/functions/Get-ADTIniValue
+    #>
+
+    [CmdletBinding()]
+    [OutputType([Collections.Specialized.OrderedDictionary])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if (![System.IO.File]::Exists($_))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName FilePath -ProvidedValue $_ -ExceptionMessage 'The specified file does not exist.'))
+                }
+                return ![System.String]::IsNullOrWhiteSpace($_)
+            })]
+        [System.String]$FilePath,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if ([string]::IsNullOrWhiteSpace($_))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName Path -ProvidedValue $_ -ExceptionMessage 'The specified section cannot be null, empty, or whitespace.'))
+                }
+                return ![System.String]::IsNullOrWhiteSpace($_)
+            })]
+        [System.String]$Section
+    )
+
+    begin
+    {
+        Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+
+    process
+    {
+        Write-ADTLogEntry -Message "Reading INI section: [FilePath = $FilePath] [Section = $Section]."
+        try
+        {
+            try
+            {
+                # Get the section from the INI file
+                $iniSection = [PSADT.Utilities.IniUtilities]::GetSection($Section, $FilePath)
+                Write-ADTLogEntry -Message "INI section content: $($iniSection.GetEnumerator() | & { process { "`n$($_.Key)=$($_.Value)" } })"
+                return $iniSection
+            }
+            catch
+            {
+                Write-Error -ErrorRecord $_
+            }
+        }
+        catch
+        {
+            Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_ -LogMessage "Failed to read INI section."
+        }
+    }
+
+    end
+    {
+        Complete-ADTFunction -Cmdlet $PSCmdlet
+    }
+}
---
+++ src/PSAppDeployToolkit/Public/Remove-ADTIniSection.ps1
@@ -0,0 +1,102 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Remove-ADTIniSection
+#
+#-----------------------------------------------------------------------------
+
+function Remove-ADTIniSection
+{
+    <#
+    .SYNOPSIS
+        Opens an INI file and removes the specified section.
+
+    .DESCRIPTION
+        Opens an INI file and removes the specified section.
+
+    .PARAMETER FilePath
+        Path to the INI file.
+
+    .PARAMETER Section
+        Section within the INI file.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
+    .EXAMPLE
+        Remove-ADTIniSection -FilePath "$env:ProgramFilesX86\IBM\Notes\notes.ini" -Section 'Notes'
+
+        Removes the 'Notes' section of the 'notes.ini' file.
+
+    .NOTES
+        An active ADT session is NOT required to use this function.
+
+        Tags: psadt<br />
+        Website: https://psappdeploytoolkit.com<br />
+        Copyright: (C) 2025 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham, Muhammad Mashwani, Mitch Richters, Dan Gough).<br />
+        License: https://opensource.org/license/lgpl-3-0
+
+    .LINK
+        https://psappdeploytoolkit.com/docs/reference/functions/Remove-ADTIniSection
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if (![System.IO.File]::Exists($_))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName FilePath -ProvidedValue $_ -ExceptionMessage 'The specified file does not exist.'))
+                }
+                return ![System.String]::IsNullOrWhiteSpace($_)
+            })]
+        [System.String]$FilePath,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if ([string]::IsNullOrWhiteSpace($_))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName Path -ProvidedValue $_ -ExceptionMessage 'The specified section cannot be null, empty, or whitespace.'))
+                }
+                return ![System.String]::IsNullOrWhiteSpace($_)
+            })]
+        [System.String]$Section
+    )
+
+    begin
+    {
+        Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+
+    process
+    {
+        try
+        {
+            try
+            {
+                Write-ADTLogEntry -Message "Removing INI section: [$Section]."
+                [PSADT.Utilities.IniUtilities]::WriteSectionKeyValue($Section, [NullString]::Value, [NullString]::Value, $FilePath)
+            }
+            catch
+            {
+                Write-Error -ErrorRecord $_
+            }
+        }
+        catch
+        {
+            Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_ -LogMessage "Failed to remove INI section."
+        }
+    }
+
+    end
+    {
+        Complete-ADTFunction -Cmdlet $PSCmdlet
+    }
+}
