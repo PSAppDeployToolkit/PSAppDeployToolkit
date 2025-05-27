86a0e11aba30edb4baf4981c4fb98d23e453f474
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-05-27 18:10:23 +1000

    - Add required permissions setting within `Show-ADTModalDialog` for new async display.

--- src/PSAppDeployToolkit/Private/Show-ADTModalDialog.ps1
+++ src/PSAppDeployToolkit/Private/Show-ADTModalDialog.ps1
@@ -29,6 +29,16 @@ function Private:Show-ADTModalDialog
         [System.Management.Automation.SwitchParameter]$NoWait
     )
 
+    # Get the current config, we'll need this for processing the asset permissions.
+    $adtConfig = Get-ADTConfig
+
+    # Set required permissions on this module's library files and assets first.
+    $builtinUsersSid = [System.Security.Principal.SecurityIdentifier]::new([System.Security.Principal.WellKnownSidType]::BuiltinUsersSid, $null)
+    $saipParams = @{ User = "*$($builtinUsersSid.Value)"; Permission = 'ReadAndExecute'; PermissionType = 'Allow'; Method = 'AddAccessRule'; InformationAction = 'SilentlyContinue' }
+    Set-ADTItemPermission @saipParams -Path $Script:PSScriptRoot\lib -Inheritance ObjectInherit -Propagation InheritOnly
+    Set-ADTItemPermission @saipParams -Path $adtConfig.Assets.Logo
+    Set-ADTItemPermission @saipParams -Path $adtConfig.Assets.Banner
+
     # Serialise the incoming options.
     $optionsString = switch ($Type)
     {
