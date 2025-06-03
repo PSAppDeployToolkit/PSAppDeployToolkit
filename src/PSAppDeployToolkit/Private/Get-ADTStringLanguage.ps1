ef7bd836e566dbe488b7cd12eefb499c80b06fb2
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-03 13:00:53 +1000

    - Always use the user's culture for the language where possible.

--- src/PSAppDeployToolkit/Private/Get-ADTStringLanguage.ps1
+++ src/PSAppDeployToolkit/Private/Get-ADTStringLanguage.ps1
@@ -11,9 +11,14 @@ function Private:Get-ADTStringLanguage
         # The caller has specified a specific language.
         return $languageOverride
     }
+    elseif ($Script:ADT.Environment.RunAsActiveUser)
+    {
+        # Get the actual user's $PSUICulture value. If we're running as SYSTEM, the user's locale could be different.
+        return $((Start-ADTProcess -Username $Script:ADT.Environment.RunAsActiveUser.NTAccount -FilePath powershell.exe -ArgumentList '-NonInteractive -NoProfile -NoLogo -WindowStyle Hidden -Command $PSUICulture' -MsiExecWaitTime ([System.TimeSpan]::FromSeconds($Script:ADT.Config.MSI.MutexWaitTime)) -CreateNoWindow -PassThru -InformationAction SilentlyContinue).StdOut)
+    }
     else
     {
-        # Fall back to PowerShell's.
-        return [System.Threading.Thread]::CurrentThread.CurrentUICulture
+        # Fall back to PowerShell's for this active session.
+        return $PSUICulture
     }
 }
