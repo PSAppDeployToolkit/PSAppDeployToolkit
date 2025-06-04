62628c5c059ed060cb31803e6b139e37b1acb59a
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-04 13:15:24 +1000

    - Knee-cap `ProcessDefinition`'s `Filter` setup for now.
    This requires significant re-thinking when performing this as part of the display server.

--- src/PSADT/PSADT/ProcessManagement/PSADT.ProcessManagement.Class.ProcessDefinition.cs
+++ src/PSADT/PSADT/ProcessManagement/PSADT.ProcessManagement.Class.ProcessDefinition.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Management.Automation;
 
 namespace PSADT.ProcessManagement
 {
@@ -20,26 +19,6 @@ namespace PSADT.ProcessManagement
                 throw new ArgumentNullException("Name value is null or invalid.", (Exception?)null);
             }
             Name = name;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ProcessDefinition"/> struct.
-        /// </summary>
-        /// <param name="name">The name of the process.</param>
-        /// <param name="filter">A filter for the specified process.</param>
-        public ProcessDefinition(string name, Func<RunningProcess, bool> filter)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                throw new ArgumentNullException("Name value is null or invalid.", (Exception?)null);
-            }
-            Name = name;
-
-            if (null == filter)
-            {
-                throw new ArgumentNullException("Filter value is null or invalid.", (Exception?)null);
-            }
-            Filter = filter;
         }
 
         /// <summary>
@@ -65,33 +44,6 @@ namespace PSADT.ProcessManagement
         /// <summary>
         /// Initializes a new instance of the <see cref="ProcessObject"/> struct.
         /// </summary>
-        /// <param name="name">The name of the process.</param>
-        /// <param name="description">The description of the process.</param>
-        /// <param name="filter">A filter for the specified process.</param>
-        public ProcessDefinition(string name, string description, Func<RunningProcess, bool> filter)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                throw new ArgumentNullException("Name value is null or invalid.", (Exception?)null);
-            }
-            Name = name;
-
-            if (string.IsNullOrWhiteSpace(description))
-            {
-                throw new ArgumentNullException("Description value is null or invalid.", (Exception?)null);
-            }
-            Description = description;
-
-            if (null == filter)
-            {
-                throw new ArgumentNullException("Filter value is null or invalid.", (Exception?)null);
-            }
-            Filter = filter;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ProcessObject"/> struct.
-        /// </summary>
         /// <param name="properties">The hashtable with a process's name, and optionally a description.</param>
         public ProcessDefinition(Hashtable properties)
         {
@@ -109,22 +61,6 @@ namespace PSADT.ProcessManagement
                     throw new ArgumentOutOfRangeException("Description value is not valid.", (Exception?)null);
                 }
                 Description = description;
-            }
-            if (properties.ContainsKey("Filter"))
-            {
-                var filter = properties["Filter"];
-                if (filter is ScriptBlock)
-                {
-                    Filter = (Func<RunningProcess, bool>)((PSObject)ScriptBlock.Create("return [System.Func`2[PSADT.ProcessManagement.RunningProcess,System.Boolean]]$args[0]").InvokeReturnAsIs(filter)).BaseObject;
-                }
-                else if (filter is Func<RunningProcess, bool>)
-                {
-                    Filter = (Func<RunningProcess, bool>)filter;
-                }
-                else
-                {
-                    throw new ArgumentOutOfRangeException("filter value is not valid.", (Exception?)null);
-                }
             }
 
             // The hashtable was correctly defined, assign the remaining values.
