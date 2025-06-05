1905a0e9362b79b410b94c339eb13d39fd13bc24
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-06 09:54:06 +1000

    - Add some HiDPI fixes for the classic HelpDialog (about to be hooked up).

--- src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.HelpDialog.Designer.cs
+++ src/PSADT.UserInterface/PSADT.UserInterface/Dialogs/Classic/PSADT.UserInterface.Dialogs.Classic.HelpDialog.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.comboBox = new System.Windows.Forms.ComboBox();
-            this.listBox = new System.Windows.Forms.ListBox();
             this.richTextBoxPanel = new System.Windows.Forms.Panel();
             this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.panelListBox = new System.Windows.Forms.Panel();
+            this.listBox = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel.SuspendLayout();
             this.richTextBoxPanel.SuspendLayout();
+            this.panelListBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel
@@ -45,8 +47,8 @@
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 308F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel.Controls.Add(this.comboBox, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.listBox, 0, 1);
             this.tableLayoutPanel.Controls.Add(this.richTextBoxPanel, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.panelListBox, 0, 1);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
@@ -64,23 +66,9 @@
             this.comboBox.Location = new System.Drawing.Point(4, 3);
             this.comboBox.Margin = new System.Windows.Forms.Padding(4, 3, 0, 4);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(304, 22);
+            this.comboBox.Size = new System.Drawing.Size(304, 23);
             this.comboBox.Sorted = true;
             this.comboBox.TabIndex = 0;
-            // 
-            // listBox
-            // 
-            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox.FormattingEnabled = true;
-            this.listBox.IntegralHeight = false;
-            this.listBox.ItemHeight = 14;
-            this.listBox.Location = new System.Drawing.Point(4, 29);
-            this.listBox.Margin = new System.Windows.Forms.Padding(4, 0, 0, 4);
-            this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(304, 546);
-            this.listBox.Sorted = true;
-            this.listBox.TabIndex = 1;
             // 
             // richTextBoxPanel
             // 
@@ -100,6 +88,7 @@
             // 
             this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox.Location = new System.Drawing.Point(0, 0);
             this.richTextBox.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox.Name = "richTextBox";
@@ -109,20 +98,47 @@
             this.richTextBox.Text = "";
             this.richTextBox.WordWrap = false;
             // 
+            // panelListBox
+            // 
+            this.panelListBox.AutoSize = true;
+            this.panelListBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelListBox.Controls.Add(this.listBox);
+            this.panelListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelListBox.Location = new System.Drawing.Point(4, 30);
+            this.panelListBox.Margin = new System.Windows.Forms.Padding(4, 0, 0, 4);
+            this.panelListBox.Name = "panelListBox";
+            this.panelListBox.Size = new System.Drawing.Size(304, 545);
+            this.panelListBox.TabIndex = 3;
+            // 
+            // listBox
+            // 
+            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.IntegralHeight = false;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(0, 0);
+            this.listBox.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(304, 545);
+            this.listBox.Sorted = true;
+            this.listBox.TabIndex = 1;
+            // 
             // HelpDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1322, 579);
             this.Controls.Add(this.tableLayoutPanel);
-            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "HelpDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PSAppDeployToolkit Help Console";
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
             this.richTextBoxPanel.ResumeLayout(false);
+            this.panelListBox.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,8 +148,9 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.ComboBox comboBox;
-        private System.Windows.Forms.ListBox listBox;
         private System.Windows.Forms.Panel richTextBoxPanel;
         private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Panel panelListBox;
+        private System.Windows.Forms.ListBox listBox;
     }
 }
