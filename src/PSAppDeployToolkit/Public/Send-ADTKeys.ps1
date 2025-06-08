52a8a66393c5f58f8ca1b672c0b7a6c2df122611
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-08 18:34:31 +1000

    - Fix bad comment in `Send-ADTKeys`, this isn't a dialog.

--- src/PSAppDeployToolkit/Public/Send-ADTKeys.ps1
+++ src/PSAppDeployToolkit/Public/Send-ADTKeys.ps1
@@ -124,7 +124,7 @@ function Send-ADTKeys
 
     process
     {
-        # Bypass if no one's logged on to answer the dialog.
+        # Bypass if no one's logged onto the device.
         if (!($runAsActiveUser = (Get-ADTEnvironmentTable).RunAsActiveUser))
         {
             Write-ADTLogEntry -Message "Bypassing $($MyInvocation.MyCommand.Name) as there is no active user logged onto the system."
