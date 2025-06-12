ab8a7d2c3b5553fa2ea5c1c6f83ad26706c8db0e
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-12 22:27:37 +1000

    - Neutralise the bool output from `Invoke-ADTClientServerOperation` minimise/restore operations.

--- src/PSAppDeployToolkit/Public/Show-ADTInstallationWelcome.ps1
+++ src/PSAppDeployToolkit/Public/Show-ADTInstallationWelcome.ps1
@@ -749,7 +749,7 @@ function Show-ADTInstallationWelcome
             # Minimize all other windows.
             if ($MinimizeWindows)
             {
-                Invoke-ADTClientServerOperation -MinimizeAllWindows -User $runAsActiveUser
+                $null = Invoke-ADTClientServerOperation -MinimizeAllWindows -User $runAsActiveUser
             }
 
             # Show the dialog and return the result.
@@ -1131,7 +1131,7 @@ function Show-ADTInstallationWelcome
                             # Restore minimized windows.
                             if ($MinimizeWindows)
                             {
-                                Invoke-ADTClientServerOperation -RestoreAllWindows -User $runAsActiveUser
+                                $null = Invoke-ADTClientServerOperation -RestoreAllWindows -User $runAsActiveUser
                             }
 
                             # If there's an active session, update deferral values and close it out.
@@ -1150,7 +1150,7 @@ function Show-ADTInstallationWelcome
                             # Restore minimized windows.
                             if ($MinimizeWindows)
                             {
-                                Invoke-ADTClientServerOperation -RestoreAllWindows -User $runAsActiveUser
+                                $null = Invoke-ADTClientServerOperation -RestoreAllWindows -User $runAsActiveUser
                             }
 
                             # If there's an active session, update deferral values and close it out.
