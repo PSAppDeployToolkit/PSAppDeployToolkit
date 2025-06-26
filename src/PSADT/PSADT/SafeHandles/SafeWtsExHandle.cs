2d80b40f9ab914db2793a2ea89e33204155df21e
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-06-26 11:53:47 +1000

    - Add `SafeWtsExHandle` class ahead of some upcoming work.

---
+++ src/PSADT/PSADT/SafeHandles/SafeWtsExHandle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PSADT.LibraryInterfaces;
+using Windows.Win32.System.RemoteDesktop;
+
+namespace PSADT.SafeHandles
+{
+    /// <summary>
+    /// Represents a wrapper for an environment block handle that ensures the handle is properly released.
+    /// </summary>
+    internal sealed class SafeWtsExHandle(IntPtr handle, WTS_TYPE_CLASS type, int length, bool ownsHandle) : SafeMemoryHandle(handle, length, ownsHandle)
+    {
+        /// <summary>
+        /// Reallocates the memory block to the specified size.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        internal override void ReAlloc(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Releases the handle.
+        /// </summary>
+        /// <returns></returns>
+        protected override bool ReleaseHandle()
+        {
+            return WtsApi32.WTSFreeMemoryEx(type, ref handle, (uint)Length);
+        }
+    }
+}
