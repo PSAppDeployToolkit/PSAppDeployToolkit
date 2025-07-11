d05fc65e08337708c68f0ae1163f3e1ecdacd530
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-07-11 16:05:35 +1000

    - Amend "- Rework RunAsActiveUser setup and `Get-ADTClientServerUser` to handle running the toolkit as another user from an existing interactive session." to only run the client/server process as the interactive, non-SYSTEM calling user if that user doesn't have the necessary permissions to create a process as a user.

--- src/PSAppDeployToolkit/Private/Get-ADTClientServerUser.ps1
+++ src/PSAppDeployToolkit/Private/Get-ADTClientServerUser.ps1
@@ -6,9 +6,21 @@
 
 function Private:Get-ADTClientServerUser
 {
+    # Determine whether the caller is missing any privileges to create a process as another user.
+    $missingPermissions = [PSADT.Security.SE_PRIVILEGE]::SeDebugPrivilege, [PSADT.Security.SE_PRIVILEGE]::SeIncreaseQuotaPrivilege, [PSADT.Security.SE_PRIVILEGE]::SeAssignPrimaryTokenPrivilege | & {
+        process
+        {
+            if (![PSADT.AccountManagement.AccountUtilities]::CallerPrivileges.Contains($_))
+            {
+                return $_
+            }
+        }
+    }
+
     # Get the active user from the environment if available.
-    $runAsActiveUser = if ([System.Environment]::UserInteractive -and ![PSADT.AccountManagement.AccountUtilities]::CallerSid.IsWellKnown([System.Security.Principal.WellKnownSidType]::LocalSystemSid))
+    $runAsActiveUser = if ([System.Environment]::UserInteractive -and ![PSADT.AccountManagement.AccountUtilities]::CallerSid.IsWellKnown([System.Security.Principal.WellKnownSidType]::LocalSystemSid) -and ($null -ne $missingPermissions))
     {
+        Write-ADTLogEntry -Message "The calling account [$([PSADT.AccountManagement.AccountUtilities]::CallerUsername)] is running interactively, but not as the logged on user and missing the permission(s) ['$($missingPermissions -join "', '")'] necessary to run a process as another user. The client/server process will run as the calling account, however PSAppDeployToolkit's client/server operations are designed to run directly as a logged on user, and as such, it is recommended to log on directly with the account you're testing via, assign this account the missing permissions, or test via the SYSTEM account just as ConfigMgr or Intune uses for its operations." -Severity Warning
         [PSADT.Module.RunAsActiveUser]::new([PSADT.AccountManagement.AccountUtilities]::CallerUsername, [PSADT.AccountManagement.AccountUtilities]::CallerSid)
     }
     elseif ((Test-ADTSessionActive) -or (Test-ADTModuleInitialized))
