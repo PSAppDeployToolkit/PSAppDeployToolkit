7a9a3958e9311784651ff0dfda2fe095e02a90d8
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-07-30 18:23:22 +1000

    - Ensure the cached RunAsActiveUser is still logged on and active before using it.

--- src/PSAppDeployToolkit/Private/Get-ADTClientServerUser.ps1
+++ src/PSAppDeployToolkit/Private/Get-ADTClientServerUser.ps1
@@ -16,12 +16,6 @@ function Private:Get-ADTClientServerUser
         Get-ADTRunAsActiveUser 4>$null
     }
 
-    # If there's no RunAsActiveUser but the current process is interactive, just run it as the current user.
-    if (!$runAsActiveUser -and [System.Environment]::UserInteractive)
-    {
-        return [PSADT.Module.RunAsActiveUser]::new([PSADT.AccountManagement.AccountUtilities]::CallerUsername, [PSADT.AccountManagement.AccountUtilities]::CallerSid)
-    }
-
     # Return the calculated RunAsActiveUser if we have one.
     if ($runAsActiveUser)
     {
@@ -31,6 +25,16 @@ function Private:Get-ADTClientServerUser
             Write-ADTLogEntry -Message "The calling account [$([PSADT.AccountManagement.AccountUtilities]::CallerUsername)] is running interactively, but not as the logged on user and is missing the permission(s) ['$([System.String]::Join("', '", $missingPermissions))'] necessary to create a process as another user. The client/server process will be created as the calling account, however PSAppDeployToolkit's client/server process is designed to operate directly as a logged on user. As such, it is recommended to either log on directly to Windows using this account you're testing with, assign this account the missing permissions, or test via the SYSTEM account just as ConfigMgr or Intune uses for its operations." -Severity Warning
             return [PSADT.Module.RunAsActiveUser]::new([PSADT.AccountManagement.AccountUtilities]::CallerUsername, [PSADT.AccountManagement.AccountUtilities]::CallerSid)
         }
-        return $runAsActiveUser
+
+        # Only return the calculated RunAsActiveUser if the user is still logged on and active as of right now.
+        if (($runAsUserSession = Get-ADTLoggedOnUser -InformationAction SilentlyContinue | & { process { if ($runAsActiveUser.NTAccount.Equals($_.NTAccount)) { return $_ } } } | Select-Object -First 1) -and $runAsUserSession.IsActiveUserSession)
+        {
+            return $runAsActiveUser
+        }
+    }
+    elseif ([System.Environment]::UserInteractive)
+    {
+        # If there's no RunAsActiveUser but the current process is interactive, just run it as the current user.
+        return [PSADT.Module.RunAsActiveUser]::new([PSADT.AccountManagement.AccountUtilities]::CallerUsername, [PSADT.AccountManagement.AccountUtilities]::CallerSid)
     }
 }
