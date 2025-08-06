416073cac8b7e0249199428821491c3f7714a3df
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-08-07 09:48:17 +1000

    - Add a basic multi-session installation sample.

---
+++ examples/Multi-session Installation/Invoke-AppDeployToolkit.ps1
@@ -0,0 +1,236 @@
+﻿<#
+
+.SYNOPSIS
+PSAppDeployToolkit - This script performs the installation or uninstallation of an application(s).
+
+.DESCRIPTION
+- The script is provided as a template to perform an install, uninstall, or repair of an application(s).
+- The script either performs an "Install", "Uninstall", or "Repair" deployment type.
+- The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+
+The script imports the PSAppDeployToolkit module which contains the logic and functions required to install or uninstall an application.
+
+.PARAMETER DeploymentType
+The type of deployment to perform.
+
+.PARAMETER DeployMode
+Specifies whether the installation should be run in Interactive (shows dialogs), Silent (no dialogs), NonInteractive (dialogs without prompts) mode, or Auto (shows dialogs if a user is logged on, device is not in the OOBE, and there's no running apps to close).
+
+Silent mode is automatically set if it is detected that the process is not user interactive, no users are logged on, the device is in Autopilot mode, or there's specified processes to close that are currently running.
+
+.PARAMETER SuppressRebootPassThru
+Suppresses the 3010 return code (requires restart) from being passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+
+.PARAMETER TerminalServerMode
+Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Desktop Session Hosts/Citrix servers.
+
+.PARAMETER DisableLogging
+Disables logging to file for the script.
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1 -DeployMode Silent
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1 -DeploymentType Uninstall
+
+.EXAMPLE
+Invoke-AppDeployToolkit.exe -DeploymentType Install -DeployMode Silent
+
+.INPUTS
+None. You cannot pipe objects to this script.
+
+.OUTPUTS
+None. This script does not generate any output.
+
+.NOTES
+Toolkit Exit Code Ranges:
+- 60000 - 68999: Reserved for built-in exit codes in Invoke-AppDeployToolkit.ps1, and Invoke-AppDeployToolkit.exe
+- 69000 - 69999: Recommended for user customized exit codes in Invoke-AppDeployToolkit.ps1
+- 70000 - 79999: Recommended for user customized exit codes in PSAppDeployToolkit.Extensions module.
+
+.LINK
+https://psappdeploytoolkit.com
+
+#>
+
+[CmdletBinding()]
+param
+(
+    # Default is 'Install'.
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Install')]
+    [System.String]$DeploymentType,
+
+    # Default is 'Auto'. Don't hard-code this unless required.
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Auto', 'Interactive', 'NonInteractive', 'Silent')]
+    [System.String]$DeployMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$SuppressRebootPassThru,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$TerminalServerMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$DisableLogging
+)
+
+
+##================================================
+## MARK: Session parameters
+##================================================
+
+# The actual app we'll install.
+$adtSession = @{
+    # App variables.
+    AppVendor = 'Microsoft'
+    AppName = 'Offce 365'
+    AppVersion = 'LTSC 2024'
+    AppArch = 'x64'
+    AppLang = 'EN'
+}
+
+# A pre-req that needs installing.
+$dependency = @{
+    # App variables.
+    AppVendor = 'Microsoft'
+    AppName = 'Visual C++ Runtime'
+    AppVersion = '2015-2022'
+    AppArch = 'x64'
+    AppLang = 'EN'
+}
+
+
+##================================================
+## MARK: Functions
+##================================================
+
+function Install-DependencyApplication
+{
+    [CmdletBinding()]
+    param
+    (
+    )
+
+    # Open a new deployment session in this scope.
+    $dependency = Remove-ADTHashtableNullOrEmptyValues -Hashtable $dependency
+    $adtSession = Open-ADTSession @dependency @iadtParams -PassThru
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+
+    # Commence install.
+    Write-ADTLogEntry -Message 'Sleeping for 10 seconds to simulate a dependency install.'
+    Start-Sleep -Seconds 10
+
+    # Close out this session.
+    Close-ADTSession
+}
+
+function Install-ADTDeployment
+{
+    [CmdletBinding()]
+    param
+    (
+    )
+
+    # Show UI elements to user.
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+    $saiwParams = @{
+        AllowDefer = $true
+        DeferTimes = 3
+        CheckDiskSpace = $true
+        PersistPrompt = $true
+    }
+    if ($adtSession.AppProcessesToClose.Count -gt 0)
+    {
+        $saiwParams.Add('CloseProcesses', $adtSession.AppProcessesToClose)
+    }
+    Show-ADTInstallationWelcome @saiwParams
+    Show-ADTInstallationProgress
+
+    # Install pre-req if it doesn't exist.
+    if (!(Get-ADTApplication -Name (Get-Random) -InformationAction SilentlyContinue))
+    {
+        Write-ADTLogEntry -Message "Required dependencies missing, installing as required..."
+        Install-DependencyApplication
+    }
+
+    # Commence install.
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+    Write-ADTLogEntry -Message 'Sleeping for 10 seconds to simulate an application install.'
+    Start-Sleep -Seconds 10
+
+    # Perform any defined post-install actions.
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+}
+
+
+##================================================
+## MARK: Initialization
+##================================================
+
+# Set strict error handling across entire operation.
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+Set-StrictMode -Version 1
+
+# Import the module and instantiate a new session.
+try
+{
+    # Import the module locally if available, otherwise try to find it from PSModulePath.
+    if (Test-Path -LiteralPath "$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1" -PathType Leaf)
+    {
+        Get-ChildItem -LiteralPath "$PSScriptRoot\PSAppDeployToolkit" -Recurse -File | Unblock-File -ErrorAction Ignore
+        Import-Module -FullyQualifiedName @{ ModuleName = "$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1"; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.1.0' } -Force
+    }
+    else
+    {
+        Import-Module -FullyQualifiedName @{ ModuleName = 'PSAppDeployToolkit'; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.1.0' } -Force
+    }
+
+    # Open a new deployment session, replacing $adtSession with a DeploymentSession.
+    $iadtParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation
+    $adtSession = Remove-ADTHashtableNullOrEmptyValues -Hashtable $adtSession
+    $adtSession = Open-ADTSession @adtSession @iadtParams -PassThru
+}
+catch
+{
+    $Host.UI.WriteErrorLine((Out-String -InputObject $_ -Width ([System.Int32]::MaxValue)))
+    exit 60008
+}
+
+
+##================================================
+## MARK: Invocation
+##================================================
+
+# Commence the actual deployment operation.
+try
+{
+    # Import any found extensions before proceeding with the deployment.
+    Get-ChildItem -LiteralPath $PSScriptRoot -Directory | & {
+        process
+        {
+            if ($_.Name -match 'PSAppDeployToolkit\..+$')
+            {
+                Get-ChildItem -LiteralPath $_.FullName -Recurse -File | Unblock-File -ErrorAction Ignore
+                Import-Module -Name $_.FullName -Force
+            }
+        }
+    }
+
+    # Invoke the deployment and close out the session.
+    Install-ADTDeployment
+    Close-ADTSession
+}
+catch
+{
+    # An unhandled error has been caught.
+    $mainErrorMessage = "An unhandled error within [$($MyInvocation.MyCommand.Name)] has occurred.`n$(Resolve-ADTErrorRecord -ErrorRecord $_)"
+    Write-ADTLogEntry -Message $mainErrorMessage -Severity 3
+    Show-ADTDialogBox -Text $mainErrorMessage -Icon Stop -NoWait
+    Close-ADTSession -ExitCode 60001
+}
