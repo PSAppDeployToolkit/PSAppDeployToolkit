0d614d13b4890e0384e07467e154bec9dabe5b57
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-08-13 11:38:34 +1000

    - Add `FileSystemUtilities.TestFileAccess()` for testing whether a user has the relevant permissions.

--- src/PSADT/PSADT/FileSystem/FileSystemUtilities.cs
+++ src/PSADT/PSADT/FileSystem/FileSystemUtilities.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.AccessControl;
 using PSADT.LibraryInterfaces;
+using Windows.Win32.Storage.FileSystem;
 
 namespace PSADT.FileSystem
 {
     /// <summary>
     /// A class containing utility methods for file system operations.
     /// </summary>
-    internal static class FileSystemUtilities
+    public static class FileSystemUtilities
     {
         /// <summary>
         /// Returns a lookup table for NT paths to drive letters.
@@ -40,6 +43,42 @@ namespace PSADT.FileSystem
                 targetPath.Clear();
             }
             return new(lookupTable);
+        }
+
+        /// <summary>
+        /// Tests whether the specified file can be accessed with the desired access rights.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="desiredAccess"></param>
+        /// <returns></returns>
+        public static bool TestFileAccess(FileInfo path, FileSystemRights desiredAccess = FileSystemRights.ReadAndExecute)
+        {
+            var dwShareMode = FILE_SHARE_MODE.FILE_SHARE_NONE;
+            if ((desiredAccess & FileSystemRights.Read) == FileSystemRights.Read)
+            {
+                dwShareMode |= FILE_SHARE_MODE.FILE_SHARE_READ;
+            }
+            if ((desiredAccess & FileSystemRights.Write) == FileSystemRights.Write)
+            {
+                dwShareMode |= FILE_SHARE_MODE.FILE_SHARE_WRITE;
+            }
+            if ((desiredAccess & FileSystemRights.Delete) == FileSystemRights.Delete)
+            {
+                dwShareMode |= FILE_SHARE_MODE.FILE_SHARE_DELETE;
+            }
+            try
+            {
+                using var hFile = Kernel32.CreateFile(path.FullName, desiredAccess, dwShareMode, null, FILE_CREATION_DISPOSITION.OPEN_EXISTING, FILE_FLAGS_AND_ATTRIBUTES.FILE_ATTRIBUTE_NORMAL);
+                if (hFile.IsInvalid)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
