35c3775cf0f67bf4c610a2d62b3b2df08abe0138
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-10-04 11:59:48 +1000

    - Correct the partial implementation of `AccountUtilities.WellKnownSidLookupTable`.

---
+++ src/PSADT/PSADT/LibraryInterfaces/NtSecApi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace PSADT.LibraryInterfaces
+{
+    /// <summary>
+    /// Defines the access rights for interacting with the Local Security Authority (LSA) policy.
+    /// </summary>
+    /// <remarks>https://learn.microsoft.com/en-us/windows/win32/secmgmt/policy-object-access-rights</remarks>
+    [Flags]
+    internal enum LSA_POLICY_ACCESS : uint
+    {
+        /// <summary>
+        /// This access type is needed to read the target system's miscellaneous security policy information. This includes the default quota, auditing, server state and role information, and trust information. This access type is also needed to enumerate trusted domains, accounts, and privileges.
+        /// </summary>
+        POLICY_VIEW_LOCAL_INFORMATION = Windows.Win32.PInvoke.POLICY_VIEW_LOCAL_INFORMATION,
+
+        /// <summary>
+        /// This access type is needed to view sensitive information, such as the names of accounts established for trusted domain relationships.
+        /// </summary>
+        POLICY_GET_PRIVATE_INFORMATION = Windows.Win32.PInvoke.POLICY_GET_PRIVATE_INFORMATION,
+
+        /// <summary>
+        /// This access type is needed to change the account domain or primary domain information.
+        /// </summary>
+        POLICY_TRUST_ADMIN = Windows.Win32.PInvoke.POLICY_TRUST_ADMIN,
+
+        /// <summary>
+        /// Set the default system quotas that are applied to user accounts.
+        /// </summary>
+        POLICY_SET_DEFAULT_QUOTA_LIMITS = Windows.Win32.PInvoke.POLICY_SET_DEFAULT_QUOTA_LIMITS,
+
+        /// <summary>
+        /// This access type is needed to create a new Private Data object.
+        /// </summary>
+        POLICY_CREATE_SECRET = Windows.Win32.PInvoke.POLICY_CREATE_SECRET,
+
+        /// <summary>
+        /// This access type is needed to create a new Account object.
+        /// </summary>
+        POLICY_CREATE_ACCOUNT = Windows.Win32.PInvoke.POLICY_CREATE_ACCOUNT,
+
+        /// <summary>
+        /// This access type is needed to update the auditing requirements of the system.
+        /// </summary>
+        POLICY_SET_AUDIT_REQUIREMENTS = Windows.Win32.PInvoke.POLICY_SET_AUDIT_REQUIREMENTS,
+
+        /// <summary>
+        /// This access type is needed to change the characteristics of the audit trail such as its maximum size or the retention period for audit records, or to clear the log.
+        /// </summary>
+        POLICY_AUDIT_LOG_ADMIN = Windows.Win32.PInvoke.POLICY_AUDIT_LOG_ADMIN,
+
+        /// <summary>
+        /// This access type is needed to view audit trail or audit requirements information.
+        /// </summary>
+        POLICY_VIEW_AUDIT_INFORMATION = Windows.Win32.PInvoke.POLICY_VIEW_AUDIT_INFORMATION,
+
+        /// <summary>
+        /// This access type is needed to modify the server state or role (master/replica) information.It is also needed to change the replica source and account name information.
+        /// </summary>
+        POLICY_SERVER_ADMIN = Windows.Win32.PInvoke.POLICY_SERVER_ADMIN,
+
+        /// <summary>
+        /// This access type is needed to translate between names and SIDs.
+        /// </summary>
+        POLICY_LOOKUP_NAMES = Windows.Win32.PInvoke.POLICY_LOOKUP_NAMES,
+
+        /// <summary>
+        /// Not yet supported.
+        /// </summary>
+        POLICY_CREATE_PRIVILEGE = Windows.Win32.PInvoke.POLICY_CREATE_PRIVILEGE,
+
+        /// <summary>
+        /// Represents a generic read access right that combines standard read permissions with the ability to view audit information and retrieve private policy information.
+        /// </summary>
+        GENERIC_READ = Windows.Win32.Storage.FileSystem.FILE_ACCESS_RIGHTS.STANDARD_RIGHTS_READ | POLICY_VIEW_AUDIT_INFORMATION | POLICY_GET_PRIVATE_INFORMATION,
+
+        /// <summary>
+        /// Represents a combination of access rights used to specify generic write permissions.
+        /// </summary>
+        GENERIC_WRITE = Windows.Win32.Storage.FileSystem.FILE_ACCESS_RIGHTS.STANDARD_RIGHTS_WRITE | POLICY_TRUST_ADMIN | POLICY_CREATE_ACCOUNT | POLICY_CREATE_SECRET | POLICY_CREATE_PRIVILEGE | POLICY_SET_DEFAULT_QUOTA_LIMITS | POLICY_SET_AUDIT_REQUIREMENTS | POLICY_AUDIT_LOG_ADMIN | POLICY_SERVER_ADMIN,
+
+        /// <summary>
+        /// Represents a combination of access rights that allow standard execute permissions, viewing local policy information, and looking up policy names.
+        /// </summary>
+        GENERIC_EXECUTE = Windows.Win32.Storage.FileSystem.FILE_ACCESS_RIGHTS.STANDARD_RIGHTS_EXECUTE | POLICY_VIEW_LOCAL_INFORMATION | POLICY_LOOKUP_NAMES,
+
+        /// <summary>
+        /// Represents the access rights required to perform all policy-related operations.
+        /// </summary>
+        POLICY_ALL_ACCESS = Windows.Win32.Storage.FileSystem.FILE_ACCESS_RIGHTS.STANDARD_RIGHTS_REQUIRED | POLICY_VIEW_LOCAL_INFORMATION | POLICY_VIEW_AUDIT_INFORMATION | POLICY_GET_PRIVATE_INFORMATION | POLICY_TRUST_ADMIN | POLICY_CREATE_ACCOUNT | POLICY_CREATE_SECRET | POLICY_CREATE_PRIVILEGE | POLICY_SET_DEFAULT_QUOTA_LIMITS | POLICY_SET_AUDIT_REQUIREMENTS | POLICY_AUDIT_LOG_ADMIN | POLICY_SERVER_ADMIN | POLICY_LOOKUP_NAMES,
+    }
+}
---
+++ src/PSADT/PSADT/SafeHandles/SafeLsaFreeMemoryHandle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace PSADT.SafeHandles
+{
+    /// <summary>
+    /// Represents a safe handle for memory allocated by the Local Security Authority (LSA) that requires deallocation
+    /// using the <see cref="PInvoke.LsaFreeMemory"/> function.
+    /// </summary>
+    /// <remarks>This class ensures that memory allocated by the LSA is properly released when the handle is
+    /// disposed or finalized. It is a specialized implementation of <see cref="SafeMemoryHandle"/> designed to work
+    /// with LSA memory management functions.</remarks>
+    /// <param name="handle"></param>
+    /// <param name="ownsHandle"></param>
+    internal sealed class SafeLsaFreeMemoryHandle(IntPtr handle, bool ownsHandle) : SafeMemoryHandle(handle, 0, ownsHandle)
+    {
+        /// <summary>
+        /// Releases the handle.
+        /// </summary>
+        /// <returns></returns>
+        protected override unsafe bool ReleaseHandle()
+        {
+            if (handle == default || IntPtr.Zero == handle)
+            {
+                return true;
+            }
+            var res = PInvoke.LsaFreeMemory(handle.ToPointer());
+            if (res != NTSTATUS.STATUS_SUCCESS)
+            {
+                throw new Win32Exception((int)PInvoke.LsaNtStatusToWinError(res));
+            }
+            handle = default;
+            return true;
+        }
+    }
+}
