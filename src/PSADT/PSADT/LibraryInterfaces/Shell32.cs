dfac990b9ed14a72a466f75a79bc0e71456ae72d
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-11-24 23:56:30 +1100

    Uplift and refinement of all P/Invokes within Shell32 class.

--- src/PSADT/PSADT/LibraryInterfaces/Shell32.cs
+++ src/PSADT/PSADT/LibraryInterfaces/Shell32.cs
@@ -10,8 +10,13 @@ using Windows.Win32.UI.WindowsAndMessaging;
 namespace PSADT.LibraryInterfaces
 {
     /// <summary>
-    /// CsWin32 P/Invoke wrappers for the shell32.dll library.
+    /// Provides managed wrappers and supporting structures for selected Windows Shell (shell32.dll) API functions,
+    /// enabling access to shell features such as file information, stock icons, and system image lists.
     /// </summary>
+    /// <remarks>This class is intended for internal use to facilitate interoperation with Windows Shell APIs.
+    /// It exposes methods and structures that allow managed code to interact with native shell functionality, such as
+    /// retrieving file icons, setting application user model IDs, and notifying the shell of changes. All members are
+    /// static and are not intended for use outside of the containing assembly.</remarks>
     internal static class Shell32
     {
         /// <summary>
@@ -82,46 +87,67 @@ namespace PSADT.LibraryInterfaces
         }
 
         /// <summary>
-        /// Sets the AUMID for the current process (used for naming balloon tips).
+        /// Sets the explicit Application User Model ID (AppUserModelID) for the current process.
         /// </summary>
-        /// <param name="AppID"></param>
-        /// <returns></returns>
-        internal static int SetCurrentProcessExplicitAppUserModelID(string AppID)
-        {
-            return PInvoke.SetCurrentProcessExplicitAppUserModelID(AppID).ThrowOnFailure();
-        }
+        /// <remarks>Setting an explicit AppUserModelID allows the application's windows to be grouped and
+        /// managed together in the Windows taskbar and Start menu. This method should be called before creating any
+        /// windows to ensure consistent behavior.</remarks>
+        /// <param name="AppID">The AppUserModelID to assign to the current process. This value is used by Windows to group windows and
+        /// taskbar buttons. Cannot be null.</param>
+        /// <returns>An HRESULT indicating the success or failure of the operation. A value of 0 indicates success; any other
+        /// value indicates an error.</returns>
+        internal static int SetCurrentProcessExplicitAppUserModelID(string AppID) => PInvoke.SetCurrentProcessExplicitAppUserModelID(AppID).ThrowOnFailure();
 
         /// <summary>
-        /// Queries the user notification state.
+        /// Retrieves the current user notification state, indicating the user's availability for receiving
+        /// notifications.
         /// </summary>
-        /// <param name="pquns"></param>
-        /// <returns></returns>
-        internal static HRESULT SHQueryUserNotificationState(out Windows.Win32.UI.Shell.QUERY_USER_NOTIFICATION_STATE pquns)
-        {
-            return PInvoke.SHQueryUserNotificationState(out pquns).ThrowOnFailure();
-        }
+        /// <remarks>This method wraps the native SHQueryUserNotificationState function and throws an
+        /// exception if the operation fails. The notification state can be used to determine whether it is appropriate
+        /// to display notifications to the user.</remarks>
+        /// <param name="pquns">When this method returns, contains a value from the QUERY_USER_NOTIFICATION_STATE enumeration that specifies
+        /// the user's current notification state.</param>
+        /// <returns>An HRESULT value indicating the success or failure of the operation.</returns>
+        internal static HRESULT SHQueryUserNotificationState(out Windows.Win32.UI.Shell.QUERY_USER_NOTIFICATION_STATE pquns) => PInvoke.SHQueryUserNotificationState(out pquns).ThrowOnFailure();
 
         /// <summary>
-        /// Notifies the system of an event that an application has performed.
+        /// Notifies the system of a change to the shell namespace, such as the creation, deletion, or modification of
+        /// files or folders.
         /// </summary>
-        /// <param name="wEventId"></param>
-        /// <param name="uFlags"></param>
-        /// <param name="dwItem1"></param>
-        /// <param name="dwItem2"></param>
-        internal unsafe static void SHChangeNotify([MarshalAs(UnmanagedType.I4)] SHCNE_ID wEventId, SHCNF_FLAGS uFlags, [Optional] IntPtr dwItem1, [Optional] IntPtr dwItem2)
-        {
-            PInvoke.SHChangeNotify(wEventId, uFlags, dwItem1.ToPointer(), dwItem2.ToPointer());
-        }
+        /// <remarks>This method is typically used to inform the Windows shell of changes made to the file
+        /// system or shell items by applications, ensuring that the shell and other components remain in sync with the
+        /// current state. The caller is responsible for ensuring that the parameters are valid and that the
+        /// notification is appropriate for the event.</remarks>
+        /// <param name="wEventId">A value that specifies the type of event that has occurred. This determines the kind of notification to
+        /// send.</param>
+        /// <param name="uFlags">Flags that indicate the meaning of the dwItem1 and dwItem2 parameters and how the notification is to be
+        /// handled.</param>
+        /// <param name="dwItem1">A pointer to an item or structure relevant to the event, as defined by the event type and flags. The
+        /// interpretation depends on the values of wEventId and uFlags. This parameter is optional and may be
+        /// IntPtr.Zero if not required.</param>
+        /// <param name="dwItem2">A pointer to a second item or structure relevant to the event, as defined by the event type and flags. The
+        /// interpretation depends on the values of wEventId and uFlags. This parameter is optional and may be
+        /// IntPtr.Zero if not required.</param>
+        internal unsafe static void SHChangeNotify([MarshalAs(UnmanagedType.I4)] SHCNE_ID wEventId, SHCNF_FLAGS uFlags, [Optional] IntPtr dwItem1, [Optional] IntPtr dwItem2) => PInvoke.SHChangeNotify(wEventId, uFlags, dwItem1.ToPointer(), dwItem2.ToPointer());
 
         /// <summary>
-        /// Retrieves information about a file, including its icon and display name.
+        /// Retrieves information about a file or folder, such as its icon, display name, and type, using the Windows
+        /// Shell API.
         /// </summary>
-        /// <param name="pszPath"></param>
-        /// <param name="psfi"></param>
-        /// <param name="uFlags"></param>
-        /// <param name="dwFileAttributes"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <remarks>This method is a managed wrapper for the native SHGetFileInfo function in
+        /// shell32.dll. The caller is responsible for managing any resources associated with the returned handle, such
+        /// as destroying icon handles if applicable. The method may return different types of handles depending on the
+        /// flags specified in uFlags.</remarks>
+        /// <param name="pszPath">The path to the file or folder for which to retrieve information. Can be null or an empty string if used
+        /// with certain flags that do not require a path.</param>
+        /// <param name="psfi">When this method returns, contains a structure that receives the file information retrieved by the function.</param>
+        /// <param name="uFlags">A combination of flags that specify which file information to retrieve. These flags determine the attributes
+        /// and details returned.</param>
+        /// <param name="dwFileAttributes">File attribute flags to use when retrieving information. Used only if the uFlags parameter includes a flag
+        /// indicating that file attributes are provided. The default is 0.</param>
+        /// <returns>A handle to the system image list or icon, depending on the flags specified. The handle is valid only while
+        /// the image list exists. Returns a non-zero value on success.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the file information could not be retrieved.</exception>
         internal static IntPtr SHGetFileInfo(string pszPath, out SHFILEINFO psfi, SHGFI_FLAGS uFlags, FILE_FLAGS_AND_ATTRIBUTES dwFileAttributes = 0)
         {
             [DllImport("shell32.dll", CharSet = CharSet.Auto)]
@@ -135,12 +161,16 @@ namespace PSADT.LibraryInterfaces
         }
 
         /// <summary>
-        /// Retrieves information about a stock icon.
+        /// Retrieves information about a specified stock icon, such as its handle, path, or index, based on the
+        /// provided flags.
         /// </summary>
-        /// <param name="siid"></param>
-        /// <param name="uFlags"></param>
-        /// <param name="psii"></param>
-        /// <returns></returns>
+        /// <remarks>The caller should check the returned HRESULT to determine whether the operation
+        /// succeeded. The contents of the returned SHSTOCKICONINFO structure depend on the flags specified in the
+        /// uFlags parameter.</remarks>
+        /// <param name="siid">The identifier of the stock icon to retrieve information for.</param>
+        /// <param name="uFlags">A combination of flags that specify which information about the stock icon to retrieve.</param>
+        /// <param name="psii">When this method returns, contains a structure that receives the requested stock icon information.</param>
+        /// <returns>An HRESULT value indicating the success or failure of the operation.</returns>
         internal static HRESULT SHGetStockIconInfo(SHSTOCKICONID siid, SHGSI_FLAGS uFlags, out SHSTOCKICONINFO psii)
         {
             [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
@@ -150,11 +180,15 @@ namespace PSADT.LibraryInterfaces
         }
 
         /// <summary>
-        /// Retrieves a handle to an image list that contains the system icons.
+        /// Retrieves a handle to the system image list of the specified size.
         /// </summary>
-        /// <param name="iImageList"></param>
-        /// <param name="ppvObj"></param>
-        /// <returns></returns>
+        /// <remarks>This method is typically used to obtain a system image list for use with
+        /// shell-related controls, such as list views or tree views. The caller is responsible for releasing the
+        /// returned image list interface when it is no longer needed.</remarks>
+        /// <param name="iImageList">The size of the image list to retrieve. This value specifies which system image list is returned.</param>
+        /// <param name="ppvObj">When this method returns, contains the interface pointer to the retrieved image list. This parameter is
+        /// passed uninitialized.</param>
+        /// <returns>An HRESULT value indicating the success or failure of the operation.</returns>
         internal static HRESULT SHGetImageList(SHIL_SIZE iImageList, out IImageList ppvObj)
         {
             var res = PInvoke.SHGetImageList((int)iImageList, new("46EB5926-582E-4017-9FDF-E8998DAA0950"), out var ppvObjLocal).ThrowOnFailure();
