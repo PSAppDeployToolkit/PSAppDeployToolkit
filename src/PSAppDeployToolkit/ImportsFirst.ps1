0eb2b34c9518bd77376440b593f4ffa0b2590884
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-11-30 13:49:41 +1100

    Fix typos in LGPL boilerplate text within module code.

--- src/PSAppDeployToolkit/ImportsFirst.ps1
+++ src/PSAppDeployToolkit/ImportsFirst.ps1
@@ -12,7 +12,7 @@ PSAppDeployToolkit is licensed under the GNU LGPLv3 License - © 2025 PSAppDeplo
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
 Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
 for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 .LINK
