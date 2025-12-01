0ef577448b85098bee0b7f5221116297012fc8b4
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-01 21:45:24 +1100

    Optimise the setup within `ValueTypeConverter` class for any incoming integer.
    This is only really used in PowerShell which provides no means for downcasting values.

--- src/PSADT/PSADT/Utilities/ValueTypeConverter.cs
+++ src/PSADT/PSADT/Utilities/ValueTypeConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Numerics;
 
 namespace PSADT.Utilities
 {
@@ -12,98 +12,98 @@ namespace PSADT.Utilities
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static sbyte ToSByte(long val) => unchecked((sbyte)val);
+        public static sbyte ToSByte(BigInteger val) => unchecked((sbyte)val);
 
         /// <summary>
         /// Converts the given value to an unsigned byte.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static byte ToByte(long val) => unchecked((byte)val);
+        public static byte ToByte(BigInteger val) => unchecked((byte)val);
 
         /// <summary>
         /// Converts the given value to a signed short.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static short ToShort(long val) => unchecked((short)val);
+        public static short ToShort(BigInteger val) => unchecked((short)val);
 
         /// <summary>
         /// Converts the given value to an unsigned short.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static ushort ToUShort(long val) => unchecked((ushort)val);
+        public static ushort ToUShort(BigInteger val) => unchecked((ushort)val);
 
         /// <summary>
         /// Converts the given value to a signed integer.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static int ToInt(long val) => unchecked((int)val);
+        public static int ToInt(BigInteger val) => unchecked((int)val);
 
         /// <summary>
         /// Converts the given value to an unsigned integer.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static uint ToUInt(long val) => unchecked((uint)val);
+        public static uint ToUInt(BigInteger val) => unchecked((uint)val);
 
         /// <summary>
         /// Converts the given value to a signed long.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static long ToLong(long val) => unchecked((long)val);
+        public static long ToLong(BigInteger val) => unchecked((long)val);
 
         /// <summary>
         /// Converts the given value to an unsigned long.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static ulong ToULong(long val) => unchecked((ulong)val);
+        public static ulong ToULong(BigInteger val) => unchecked((ulong)val);
 
         /// <summary>
         /// Converts the given value to a signed short.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static short ToInt16(long val) => ToShort(val);
+        public static short ToInt16(BigInteger val) => ToShort(val);
 
         /// <summary>
         /// Converts the given value to an unsigned short.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static ushort ToUInt16(long val) => ToUShort(val);
+        public static ushort ToUInt16(BigInteger val) => ToUShort(val);
 
         /// <summary>
         /// Converts the given value to a signed integer.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static int ToInt32(long val) => ToInt(val);
+        public static int ToInt32(BigInteger val) => ToInt(val);
 
         /// <summary>
         /// Converts the given value to an unsigned integer.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static uint ToUInt32(long val) => ToUInt(val);
+        public static uint ToUInt32(BigInteger val) => ToUInt(val);
 
         /// <summary>
         /// Converts the given value to a signed long.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static long ToInt64(long val) => ToLong(val);
+        public static long ToInt64(BigInteger val) => ToLong(val);
 
         /// <summary>
         /// Converts the given value to an unsigned long.
         /// </summary>
         /// <param name="val"></param>
         /// <returns></returns>
-        public static ulong ToUInt64(long val) => ToULong(val);
+        public static ulong ToUInt64(BigInteger val) => ToULong(val);
 
         /// <summary>
         /// Valid value types for ValueTypeConverter.
@@ -179,62 +179,6 @@ namespace PSADT.Utilities
             /// An unsigned 64-bit integer.
             /// </summary>
             UInt64,
-        }
-    }
-
-    /// <summary>
-    /// Utility class to convert values to another type by casting (PowerShell can't do this without help).
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public static class ValueTypeConverter<T>
-    {
-        /// <summary>
-        /// Converts the given value to the specified type.
-        /// </summary>
-        public static readonly Func<long, T> Convert;
-
-        /// <summary>
-        /// Initializes the <see cref="ValueTypeConverter{T}"/> class.
-        /// </summary>
-        /// <exception cref="NotSupportedException"></exception>
-        static ValueTypeConverter()
-        {
-            if (typeof(T) == typeof(sbyte))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, sbyte>)ValueTypeConverter.ToSByte;
-            }
-            else if (typeof(T) == typeof(byte))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, byte>)ValueTypeConverter.ToByte;
-            }
-            else if (typeof(T) == typeof(short))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, short>)ValueTypeConverter.ToShort;
-            }
-            else if (typeof(T) == typeof(ushort))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, ushort>)ValueTypeConverter.ToUShort;
-            }
-            else if (typeof(T) == typeof(int))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, int>)ValueTypeConverter.ToInt;
-            }
-            else if (typeof(T) == typeof(uint))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, uint>)ValueTypeConverter.ToUInt;
-            }
-            else if (typeof(T) == typeof(long))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, long>)ValueTypeConverter.ToLong;
-            }
-            else if (typeof(T) == typeof(ulong))
-            {
-                Convert = (Func<long, T>)(object)(Func<long, ulong>)ValueTypeConverter.ToULong;
-            }
-            else
-            {
-                throw new NotSupportedException($"Type {typeof(T)} is not supported.");
-            }
         }
     }
 }
