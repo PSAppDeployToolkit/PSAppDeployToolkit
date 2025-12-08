d007f95208db4c4f5178575af9b0abc754df780f
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-08 12:16:54 +1100

    Fix bad disposal setup within `DialogManager.ShowBalloonTip()`.

--- src/PSADT/PSADT.UserInterface/DialogManager.cs
+++ src/PSADT/PSADT.UserInterface/DialogManager.cs
@@ -263,7 +263,7 @@ namespace PSADT.UserInterface
             // Don't let this dispose until the balloon tip closes. If it disposes too early, Windows won't show the BalloonTipIcon properly.
             // It's worth noting that while a timeout can be specified, Windows doesn't necessarily honour it and will likely show for ~7 seconds only.
             using var notifyIcon = new System.Windows.Forms.NotifyIcon { Icon = Dialogs.Classic.ClassicAssets.GetIcon(options.TrayIcon), Visible = true };
-            ManualResetEventSlim balloonTipClosed = new();
+            using ManualResetEventSlim balloonTipClosed = new();
             notifyIcon.BalloonTipClosed += (_, _) => balloonTipClosed.Set();
             notifyIcon.BalloonTipClicked += (_, _) => balloonTipClosed.Set();
             notifyIcon.ShowBalloonTip(ValueTypeConverter.ToInt(options.BalloonTipTime), options.BalloonTipTitle, options.BalloonTipText, options.BalloonTipIcon);
