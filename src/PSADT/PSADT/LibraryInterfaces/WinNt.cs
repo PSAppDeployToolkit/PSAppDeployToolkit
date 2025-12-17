79b9f5ab2e58050c76831241f74bcc07dd12445a
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-17 16:39:50 +1100

    Fix up the `SUITE_MASK` so it's correct to the source.
    We were missing a value here, and its values are wider than a ushort.

--- src/PSADT/PSADT/LibraryInterfaces/WinNt.cs
+++ src/PSADT/PSADT/LibraryInterfaces/WinNt.cs
@@ -333,75 +333,80 @@ namespace PSADT.LibraryInterfaces
     /// Flags for determining a product's paricular SKU features.
     /// </summary>
     [Flags]
-    internal enum SUITE_MASK : ushort
+    internal enum SUITE_MASK : uint
     {
         /// <summary>
-        /// Microsoft BackOffice components are installed. 
+        /// Microsoft Small Business Server was once installed on the system, but may have been upgraded to another version of Windows.
+        /// Refer to the Remarks section for more information about this bit flag. 
         /// </summary>
-        VER_SUITE_BACKOFFICE = (ushort)Windows.Win32.PInvoke.VER_SUITE_BACKOFFICE,
-
-        /// <summary>
-        /// Windows Server 2003, Web Edition is installed
-        /// </summary>
-        VER_SUITE_BLADE = (ushort)Windows.Win32.PInvoke.VER_SUITE_BLADE,
-
-        /// <summary>
-        /// Windows Server 2003, Compute Cluster Edition is installed.
-        /// </summary>
-        VER_SUITE_COMPUTE_SERVER = (ushort)Windows.Win32.PInvoke.VER_SUITE_COMPUTE_SERVER,
-
-        /// <summary>
-        /// Windows Server 2008 Datacenter, Windows Server 2003, Datacenter Edition, or Windows 2000 Datacenter Server is installed. 
-        /// </summary>
-        VER_SUITE_DATACENTER = (ushort)Windows.Win32.PInvoke.VER_SUITE_DATACENTER,
+        VER_SUITE_SMALLBUSINESS = Windows.Win32.PInvoke.VER_SUITE_SMALLBUSINESS,
 
         /// <summary>
         /// Windows Server 2008 Enterprise, Windows Server 2003, Enterprise Edition, or Windows 2000 Advanced Server is installed.
         /// Refer to the Remarks section for more information about this bit flag. 
         /// </summary>
-        VER_SUITE_ENTERPRISE = (ushort)Windows.Win32.PInvoke.VER_SUITE_ENTERPRISE,
+        VER_SUITE_ENTERPRISE = Windows.Win32.PInvoke.VER_SUITE_ENTERPRISE,
 
         /// <summary>
-        /// Windows XP Embedded is installed. 
+        /// Microsoft BackOffice components are installed. 
         /// </summary>
-        VER_SUITE_EMBEDDEDNT = (ushort)Windows.Win32.PInvoke.VER_SUITE_EMBEDDEDNT,
-
-        /// <summary>
-        /// Windows Vista Home Premium, Windows Vista Home Basic, or Windows XP Home Edition is installed. 
-        /// </summary>
-        VER_SUITE_PERSONAL = (ushort)Windows.Win32.PInvoke.VER_SUITE_PERSONAL,
-
-        /// <summary>
-        /// Remote Desktop is supported, but only one interactive session is supported. This value is set unless the system is running in application server mode. 
-        /// </summary>
-        VER_SUITE_SINGLEUSERTS = (ushort)Windows.Win32.PInvoke.VER_SUITE_SINGLEUSERTS,
-
-        /// <summary>
-        /// Microsoft Small Business Server was once installed on the system, but may have been upgraded to another version of Windows.
-        /// Refer to the Remarks section for more information about this bit flag. 
-        /// </summary>
-        VER_SUITE_SMALLBUSINESS = (ushort)Windows.Win32.PInvoke.VER_SUITE_SMALLBUSINESS,
-
-        /// <summary>
-        /// Microsoft Small Business Server is installed with the restrictive client license in force. Refer to the Remarks section for more information about this bit flag. 
-        /// </summary>
-        VER_SUITE_SMALLBUSINESS_RESTRICTED = (ushort)Windows.Win32.PInvoke.VER_SUITE_SMALLBUSINESS_RESTRICTED,
-
-        /// <summary>
-        /// Windows Storage Server 2003 R2 or Windows Storage Server 2003 is installed. 
-        /// </summary>
-        VER_SUITE_STORAGE_SERVER = (ushort)Windows.Win32.PInvoke.VER_SUITE_STORAGE_SERVER,
+        VER_SUITE_BACKOFFICE = Windows.Win32.PInvoke.VER_SUITE_BACKOFFICE,
 
         /// <summary>
         /// Terminal Services is installed. This value is always set.
         /// If VER_SUITE_TERMINAL is set but VER_SUITE_SINGLEUSERTS is not set, the system is running in application server mode.
         /// </summary>
-        VER_SUITE_TERMINAL = (ushort)Windows.Win32.PInvoke.VER_SUITE_TERMINAL,
+        VER_SUITE_TERMINAL = Windows.Win32.PInvoke.VER_SUITE_TERMINAL,
+
+        /// <summary>
+        /// Microsoft Small Business Server is installed with the restrictive client license in force. Refer to the Remarks section for more information about this bit flag. 
+        /// </summary>
+        VER_SUITE_SMALLBUSINESS_RESTRICTED = Windows.Win32.PInvoke.VER_SUITE_SMALLBUSINESS_RESTRICTED,
+
+        /// <summary>
+        /// Windows XP Embedded is installed. 
+        /// </summary>
+        VER_SUITE_EMBEDDEDNT = Windows.Win32.PInvoke.VER_SUITE_EMBEDDEDNT,
+
+        /// <summary>
+        /// Windows Server 2008 Datacenter, Windows Server 2003, Datacenter Edition, or Windows 2000 Datacenter Server is installed. 
+        /// </summary>
+        VER_SUITE_DATACENTER = Windows.Win32.PInvoke.VER_SUITE_DATACENTER,
+
+        /// <summary>
+        /// Remote Desktop is supported, but only one interactive session is supported. This value is set unless the system is running in application server mode. 
+        /// </summary>
+        VER_SUITE_SINGLEUSERTS = Windows.Win32.PInvoke.VER_SUITE_SINGLEUSERTS,
+
+        /// <summary>
+        /// Windows Vista Home Premium, Windows Vista Home Basic, or Windows XP Home Edition is installed. 
+        /// </summary>
+        VER_SUITE_PERSONAL = Windows.Win32.PInvoke.VER_SUITE_PERSONAL,
+
+        /// <summary>
+        /// Windows Server 2003, Web Edition is installed
+        /// </summary>
+        VER_SUITE_BLADE = Windows.Win32.PInvoke.VER_SUITE_BLADE,
+
+        /// <summary>
+        /// Windows Storage Server 2003 R2 or Windows Storage Server 2003 is installed. 
+        /// </summary>
+        VER_SUITE_STORAGE_SERVER = Windows.Win32.PInvoke.VER_SUITE_STORAGE_SERVER,
+
+        /// <summary>
+        /// Windows Server 2003, Compute Cluster Edition is installed.
+        /// </summary>
+        VER_SUITE_COMPUTE_SERVER = Windows.Win32.PInvoke.VER_SUITE_COMPUTE_SERVER,
 
         /// <summary>
         /// Windows Home Server is installed. 
         /// </summary>
-        VER_SUITE_WH_SERVER = (ushort)Windows.Win32.PInvoke.VER_SUITE_WH_SERVER,
+        VER_SUITE_WH_SERVER = Windows.Win32.PInvoke.VER_SUITE_WH_SERVER,
+
+        /// <summary>
+        /// AppServer mode is enabled.
+        /// </summary>
+        VER_SUITE_MULTIUSERTS = Windows.Win32.PInvoke.VER_SUITE_MULTIUSERTS,
     }
 
     /// <summary>
