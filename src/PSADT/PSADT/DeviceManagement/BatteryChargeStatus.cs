f02de22c08618793b85f1eda30f79fbdaaed8fe2
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-18 13:45:58 +1100

    Prefer shifted values rather than constants in bitfields.

--- src/PSADT/PSADT/DeviceManagement/BatteryChargeStatus.cs
+++ src/PSADT/PSADT/DeviceManagement/BatteryChargeStatus.cs
@@ -16,31 +16,31 @@ namespace PSADT.DeviceManagement
         /// <summary>
         /// The battery capacity is at more than 66 percent.
         /// </summary>
-        High = 1,
+        High = 1 << 0,
 
         /// <summary>
         /// The battery capacity is at less than 33 percent
         /// </summary>
-        Low = 2,
+        Low = 1 << 1,
 
         /// <summary>
         /// The battery capacity is at less than five percent.
         /// </summary>
-        Critical = 4,
+        Critical = 1 << 2,
 
         /// <summary>
         /// The battery is charging.
         /// </summary>
-        Charging = 8,
+        Charging = 1 << 3,
 
         /// <summary>
         /// The system has no battery installed.
         /// </summary>
-        NoSystemBattery = 128,
+        NoSystemBattery = 1 << 7,
 
         /// <summary>
         /// Unable to read the battery flag information.
         /// </summary>
-        Unknown = 255
+        Unknown = byte.MaxValue
     }
 }
