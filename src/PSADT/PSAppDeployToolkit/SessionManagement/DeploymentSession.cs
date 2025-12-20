e506876c93cb32ff94bb4d65161043877213f6bd
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-20 11:42:51 +1100

    Use better naming for `WriteInitialDivider()`'s ref bool.

--- src/PSADT/PSAppDeployToolkit/SessionManagement/DeploymentSession.cs
+++ src/PSADT/PSAppDeployToolkit/SessionManagement/DeploymentSession.cs
@@ -1183,14 +1183,14 @@ namespace PSAppDeployToolkit.SessionManagement
         /// <summary>
         /// Writes a divider if one hasn't been written already.
         /// </summary>
-        private void WriteInitialDivider(ref bool write)
+        private void WriteInitialDivider(ref bool written)
         {
-            if (write)
+            if (written)
             {
                 return;
             }
             WriteLogDivider();
-            write = true;
+            written = true;
         }
 
         /// <summary>
