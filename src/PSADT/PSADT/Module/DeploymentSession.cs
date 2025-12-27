4189105751bb62f5acf5cffcaeafc7bba09873a7
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-12-28 00:17:15 +1100

    Remove unnecessary range checks in zero-config MSI files tests.

--- src/PSADT/PSADT/Module/DeploymentSession.cs
+++ src/PSADT/PSADT/Module/DeploymentSession.cs
@@ -356,15 +356,12 @@ namespace PSADT.Module
                         // Generate list of MSI executables for use with Show-ADTInstallationWelcome.
                         if (!Settings.HasFlag(DeploymentSettings.DisableDefaultMsiProcessList))
                         {
-                            ReadOnlyCollection<PSObject> gmtpOutput = ModuleDatabase.InvokeScript(ScriptBlock.Create("$gmtpParams = @{ Path = $args[0] }; if ($args[1]) { $gmtpParams.Add('TransformPath', $args[1]) }; & $Script:CommandTable.'Get-ADTMsiTableProperty' @gmtpParams -Table File -TablePropertyValueColumnNum 3"), DefaultMsiFile!, DefaultMstFile!);
-                            if (gmtpOutput.Count > 0)
+                            IReadOnlyDictionary<string, object> gmtpOutput = (IReadOnlyDictionary<string, object>)ModuleDatabase.InvokeScript(ScriptBlock.Create("$gmtpParams = @{ Path = $args[0] }; if ($args[1]) { $gmtpParams.Add('TransformPath', $args[1]) }; & $Script:CommandTable.'Get-ADTMsiTableProperty' @gmtpParams -Table File -TablePropertyValueColumnNum 3"), DefaultMsiFile!, DefaultMstFile!)[0].BaseObject;
+                            ProcessDefinition[] msiExecList = [.. gmtpOutput.Values.Where(static p => ((string)p).EndsWith(".exe", StringComparison.OrdinalIgnoreCase)).Select(static p => new ProcessDefinition(Regex.Replace(Path.GetFileNameWithoutExtension(((string)p).Split(['|'], StringSplitOptions.RemoveEmptyEntries).Last()), "^_", string.Empty)))];
+                            if (msiExecList.Length > 0)
                             {
-                                ProcessDefinition[] msiExecList = [.. ((IReadOnlyDictionary<string, object>)gmtpOutput[0].BaseObject).Values.Where(static p => ((string)p).EndsWith(".exe", StringComparison.OrdinalIgnoreCase)).Select(static p => new ProcessDefinition(Regex.Replace(Path.GetFileNameWithoutExtension(((string)p).Split(['|'], StringSplitOptions.RemoveEmptyEntries).Last()), "^_", string.Empty)))];
-                                if (msiExecList.Length > 0)
-                                {
-                                    _appProcessesToClose = _appProcessesToClose.Concat(msiExecList).GroupBy(static p => p.Name, StringComparer.OrdinalIgnoreCase).Select(static g => g.First()).ToList().AsReadOnly();
-                                    WriteLogEntry($"MSI Executable List [{string.Join(", ", msiExecList.Select(static p => p.Name))}].");
-                                }
+                                _appProcessesToClose = new([.. _appProcessesToClose.Concat(msiExecList).GroupBy(static p => p.Name, StringComparer.OrdinalIgnoreCase).Select(static g => g.First())]);
+                                WriteLogEntry($"MSI Executable List [{string.Join(", ", msiExecList.Select(static p => p.Name))}].");
                             }
                         }
 
