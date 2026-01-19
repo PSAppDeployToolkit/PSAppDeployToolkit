15af82f7e06e748ce142a82810ea71a5fc39d918
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-01-20 00:27:30 +1100

    Improve `Invoke-ADTClientServerOperation` return processing.

--- src/PSAppDeployToolkit/Private/Invoke-ADTClientServerOperation.ps1
+++ src/PSAppDeployToolkit/Private/Invoke-ADTClientServerOperation.ps1
@@ -431,7 +431,7 @@ function Private:Invoke-ADTClientServerOperation
     }
 
     # Only write a result out for modes where we're expecting a result.
-    if (($result -ne [PSADT.ClientServer.ServerInstance]::SuccessSentinel) -and ($PSCmdlet.ParameterSetName -match '^(InitCloseAppsDialog|ProgressDialogOpen|ShowModalDialog|GetProcessWindowInfo|GetUserNotificationState|GetForegroundWindowProcessId|GetEnvironmentVariable)$'))
+    if ((![System.String]::IsNullOrWhiteSpace(($result | Out-String))) -and ($result -ne [PSADT.ClientServer.ServerInstance]::SuccessSentinel) -and ($PSCmdlet.ParameterSetName -match '^(InitCloseAppsDialog|ProgressDialogOpen|ShowModalDialog|GetProcessWindowInfo|GetUserNotificationState|GetForegroundWindowProcessId|GetEnvironmentVariable)$'))
     {
         return $result
     }
