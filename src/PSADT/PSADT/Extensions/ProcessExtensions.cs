71225b8e75ed568b4a2f84ae6d05307826ed0db2
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-01-24 15:36:56 +1100

    Tighten up a number of things within the `ProcessVersionInfo` class.
    We don't want to hold onto the supplied `Process` object as we don't own it, so its liable to be disposed on us. We could also reduce some of the access rights needed for opening the process. Lastly, we were able to consolidate some shared code into a `Process` extension method.

---
+++ src/PSADT/PSADT/Extensions/ProcessExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using PSADT.FileSystem;
+using PSADT.ProcessManagement;
+
+namespace PSADT.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for working with <see cref="Process"/> instances.
+    /// </summary>
+    internal static class ProcessExtensions
+    {
+        /// <summary>
+        /// Retrieves the full file system path of the executable associated with the specified process.
+        /// </summary>
+        /// <remarks>This method attempts to retrieve the file path using the process's main module. If
+        /// that fails, it falls back to an alternative mechanism that may use the provided NT path lookup table. The
+        /// returned path may be empty if the process is inaccessible or the path cannot be resolved.</remarks>
+        /// <param name="process">The process for which to obtain the executable file path. Must not be null.</param>
+        /// <param name="ntPathLookupTable">An optional lookup table used to resolve NT device paths to file system paths. If null, a default lookup
+        /// table is used.</param>
+        /// <returns>A string containing the full file system path of the process's executable. Returns an empty string if the
+        /// path cannot be determined.</returns>
+        internal static string GetFilePath(this Process process, ReadOnlyDictionary<string, string>? ntPathLookupTable = null)
+        {
+            try
+            {
+                if (process.MainModule is not null)
+                {
+                    return process.MainModule.FileName;
+                }
+            }
+            catch (Exception ex) when (ex.Message is not null)
+            {
+                return ProcessUtilities.GetProcessImageName(process, ntPathLookupTable ?? FileSystemUtilities.GetNtPathLookupTable());
+            }
+            return ProcessUtilities.GetProcessImageName(process, ntPathLookupTable ?? FileSystemUtilities.GetNtPathLookupTable());
+        }
+    }
+}
