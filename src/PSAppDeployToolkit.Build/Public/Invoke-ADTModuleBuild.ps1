0128aaad82aae480c8de03b338a3cbcd2f65556b
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-04 23:34:50 +1100

    Allow module build steps to be specified by the caller.

--- src/PSAppDeployToolkit.Build/Public/Invoke-ADTModuleBuild.ps1
+++ src/PSAppDeployToolkit.Build/Public/Invoke-ADTModuleBuild.ps1
@@ -9,26 +9,63 @@ function Invoke-ADTModuleBuild
     [CmdletBinding()]
     param
     (
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Prerequisites', 'Clean', 'Dependencies', 'DotNet', 'Analyze', 'UnitTests', 'Build', 'IntegrationTests')]
+        [System.String[]]$Steps = ('Prerequisites', 'Clean', 'Dependencies', 'DotNet', 'Analyze', 'UnitTests', 'Build', 'IntegrationTests')
     )
 
     # Go through the motions.
     Initialize-ADTModuleBuild
+    $imported = $false
     try
     {
-        Test-ADTBuildEnvironment
-        Reset-ADTModuleBuildOutputPath
-        Confirm-ADTBuildModulesPresent
-        Invoke-ADTDotNetCompilation
-        Import-ADTDevelopmentModule
-        Confirm-ADTScriptEncoding
-        Confirm-ADTScriptFormatting
-        Confirm-ADTScriptIntegrity
-        Confirm-ADTAdmxTemplateValid
-        Confirm-ADTStringTablesValid
-        Invoke-ADTPesterUnitTesting
-        Invoke-ADTModuleCompilation
-        Invoke-ADTPesterIntegrationTesting
-        Export-ADTScriptTemplate
+        if ($Steps -contains 'Prerequisites')
+        {
+            Test-ADTBuildEnvironment
+        }
+        if ($Steps -contains 'Clean')
+        {
+            Reset-ADTModuleBuildOutputPath
+        }
+        if ($Steps -contains 'Dependencies')
+        {
+            Confirm-ADTBuildModulesPresent
+        }
+        if ($Steps -contains 'DotNet')
+        {
+            Invoke-ADTDotNetCompilation
+        }
+        if ($Steps -contains 'Analyze')
+        {
+            Confirm-ADTScriptEncoding
+            Confirm-ADTScriptFormatting
+            Confirm-ADTScriptIntegrity
+            Confirm-ADTAdmxTemplateValid
+            Confirm-ADTStringTablesValid
+        }
+        if ($Steps -contains 'UnitTests')
+        {
+            if (!$imported)
+            {
+                Import-ADTDevelopmentModule
+                $imported = $true
+            }
+            Invoke-ADTPesterUnitTesting
+        }
+        if ($Steps -contains 'Build')
+        {
+            if (!$imported)
+            {
+                Import-ADTDevelopmentModule
+                $imported = $true
+            }
+            Invoke-ADTModuleCompilation
+            Export-ADTScriptTemplate
+            if ($Steps -contains 'IntegrationTests')
+            {
+                Invoke-ADTPesterIntegrationTesting
+            }
+        }
         Complete-ADTModuleBuild
     }
     catch
