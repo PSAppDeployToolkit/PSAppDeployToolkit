d9af112e782557d2b9f6e1b3aed0a171164ba526
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-04 23:39:14 +1100

    Fortify `Import-ADTDevelopmentModule` against existing module imports.

--- src/PSAppDeployToolkit.Build/Private/Import-ADTDevelopmentModule.ps1
+++ src/PSAppDeployToolkit.Build/Private/Import-ADTDevelopmentModule.ps1
@@ -12,6 +12,10 @@ function Import-ADTDevelopmentModule
     {
         # We need the module imported for Pester tests, among other things.
         Write-ADTBuildLogEntry -Message "Importing PSApppDeployToolkit development module."
+        if (Get-Module -Name $Script:ModuleConstants.ModuleName)
+        {
+            throw "A conflicting $($Script:ModuleConstants.ModuleName) module is already imported. Please restart PowerShell and try again."
+        }
         $Script:ModuleBuildState.CommandTable = & (Import-Module -FullyQualifiedName $Script:ModuleConstants.ModuleSpecification -Global -Force -PassThru) { $CommandTable }
         Complete-ADTModuleBuildFunction
     }
