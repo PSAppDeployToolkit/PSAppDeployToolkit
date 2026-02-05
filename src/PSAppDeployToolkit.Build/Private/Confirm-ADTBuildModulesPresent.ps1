eb22fdf868b05b011068352ea3c858b873f09d4b
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-05 17:04:58 +1100

    Add `Get-ADTInstalledModuleFromModulePath` to the build system.
    This is cheaper and more reliable when it comes to side-loaded modules within the PSModulePath, such as cached modules via GitHub Actions, etc.

--- src/PSAppDeployToolkit.Build/Private/Confirm-ADTBuildModulesPresent.ps1
+++ src/PSAppDeployToolkit.Build/Private/Confirm-ADTBuildModulesPresent.ps1
@@ -11,57 +11,54 @@ function Confirm-ADTBuildModulesPresent
     try
     {
         # Get all installed modules and see if anything's missing.
-        if ($env:GITHUB_ACTIONS -ne 'true')
+        Write-ADTBuildLogEntry -Message "Confirming required PowerShell modules are installed."
+        $installedModules = Get-ADTInstalledModuleFromModulePath; $missingModules = foreach ($requiredModule in $Script:ModuleConstants.RequiredModules)
         {
-            Write-ADTBuildLogEntry -Message "Confirming required PowerShell modules are installed."
-            $installedModules = Get-InstalledModule; $missingModules = foreach ($requiredModule in $Script:ModuleConstants.RequiredModules)
+            # Return this ModuleSpecification if the module is missing.
+            if (!($installedModule = $installedModules | & { process { if ($_.Name -eq $requiredModule.Name) { return $_ } } } | Select-Object -First 1))
             {
-                # Return this ModuleSpecification if the module is missing.
-                if (!($installedModule = $installedModules | & { process { if ($_.Name -eq $requiredModule.Name) { return $_ } } } | Select-Object -First 1))
-                {
-                    $requiredModule
-                    continue
-                }
-
-                # Return this ModuleSpecification if the version is less than we need.
-                if ($installedModule.Version -lt $requiredModule.Version)
-                {
-                    $requiredModule
-                    continue
-                }
+                $requiredModule
+                continue
             }
 
-            # Return early if the required modules are available.
-            if ($missingModules)
+            # Return this ModuleSpecification if the version is less than we need.
+            if ($installedModule.Version -lt $requiredModule.Version)
             {
-                # We've got missing modules... Start by confirming NuGet is available so we can get them installed.
-                Write-ADTBuildLogEntry -Message "The following modules are missing or out of date: [$([System.String]::Join(', ', $missingModules.Name))]"
-                Write-ADTBuildLogEntry -Message "Confirming NuGet package provider state, please wait..."
-                if (!($nugetProvider = Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction Ignore) -or ($nugetProvider.Version -lt [System.Version]::new(2, 8, 5, 201)))
-                {
-                    Write-ADTBuildLogEntry -Message "Installing/updating NuGet package provider, please wait..."
-                    $null = Install-PackageProvider -Name NuGet -Scope $Scope -Force
-                }
+                $requiredModule
+                continue
+            }
+        }
 
-                # Commence installing modules one by one. We need to do this individually as we can't specify names and differing minimum versions.
-                foreach ($missingModule in $missingModules)
+        # Return early if the required modules are available.
+        if ($missingModules)
+        {
+            # We've got missing modules... Start by confirming NuGet is available so we can get them installed.
+            Write-ADTBuildLogEntry -Message "The following modules are missing or out of date: [$([System.String]::Join(', ', $missingModules.Name))]"
+            Write-ADTBuildLogEntry -Message "Confirming NuGet package provider state, please wait..."
+            if (!($nugetProvider = Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction Ignore) -or ($nugetProvider.Version -lt [System.Version]::new(2, 8, 5, 201)))
+            {
+                Write-ADTBuildLogEntry -Message "Installing/updating NuGet package provider, please wait..."
+                $null = Install-PackageProvider -Name NuGet -Scope $Scope -Force
+            }
+
+            # Commence installing modules one by one. We need to do this individually as we can't specify names and differing minimum versions.
+            foreach ($missingModule in $missingModules)
+            {
+                # Special case for Pester certificate mismatch with older Pester versions (https://github.com/pester/Pester/issues/2389).
+                Write-ADTBuildLogEntry -Message "Installing module [$($missingModule.Name)], please wait..."
+                if (($missingModule.Name -eq 'Pester') -and ($PSVersionTable.PSVersion -le [System.Version]'5.1'))
                 {
-                    # Special case for Pester certificate mismatch with older Pester versions (https://github.com/pester/Pester/issues/2389).
-                    Write-ADTBuildLogEntry -Message "Installing module [$($missingModule.Name)], please wait..."
-                    if (($missingModule.Name -eq 'Pester') -and ($PSVersionTable.PSVersion -le [System.Version]'5.1'))
-                    {
-                        Install-Module -Name $missingModule.Name -MinimumVersion $missingModule.Version -Scope CurrentUser -Force -SkipPublisherCheck
-                    }
-                    else
-                    {
-                        Install-Module -Name $missingModule.Name -MinimumVersion $missingModule.Version -Scope CurrentUser -Force
-                    }
+                    Install-Module -Name $missingModule.Name -MinimumVersion $missingModule.Version -Scope CurrentUser -Force -SkipPublisherCheck
+                }
+                else
+                {
+                    Install-Module -Name $missingModule.Name -MinimumVersion $missingModule.Version -Scope CurrentUser -Force
                 }
             }
-            else
-            {
-                Write-ADTBuildLogEntry -Message "Confirmed all required modules are already present."
-            }
+        }
+        else
+        {
+            Write-ADTBuildLogEntry -Message "Confirmed all required modules are already present."
         }
 
         # Import all required modules ahead of time.
---
+++ src/PSAppDeployToolkit.Build/Private/Get-ADTInstalledModuleFromModulePath.ps1
@@ -0,0 +1,179 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Get-ADTInstalledModuleFromModulePath
+#
+#-----------------------------------------------------------------------------
+
+function Get-ADTInstalledModuleFromModulePath
+{
+    [CmdletBinding(DefaultParameterSetName = 'SpecificVersions')]
+    param
+    (
+        [Parameter(Mandatory = $false, ParameterSetName = 'SpecificVersions')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'AllVersions')]
+        [ValidateNotNullOrEmpty()]
+        [System.String[]]$Name,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'SpecificVersions')]
+        [ValidateNotNullOrEmpty()]
+        [System.Version]$MinimumVersion,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'SpecificVersions')]
+        [ValidateNotNullOrEmpty()]
+        [System.Version]$RequiredVersion,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'SpecificVersions')]
+        [ValidateNotNullOrEmpty()]
+        [System.Version]$MaximumVersion,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'AllVersions')]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.SwitchParameter]$AllVersions
+    )
+
+    # Internal worker function to generate the output object.
+    function New-ADTInstalledModuleInfo
+    {
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [System.IO.DirectoryInfo]$Directory
+        )
+
+        # The output here is to mimic Get-InstalledModule as much as possible, but it's not 1:1.
+        # Start off by getting the module's manifest information, if the module has a .psd1 file.
+        $moduleInfo = if ([System.IO.File]::Exists("$($Directory.FullName)\$($Directory.Parent.BaseName).psd1"))
+        {
+            Import-LocalizedData -BaseDirectory $Directory.FullName -FileName "$($Directory.Parent.BaseName).psd1"
+        }
+        return [pscustomobject]@{
+            Name = $Directory.Parent.BaseName
+            Version = $Directory.BaseName
+            Type = 'Module'
+            Description = $(try { $moduleInfo.Description } catch { $null = $null })
+            Author = $(try { $moduleInfo.Author } catch { $null = $null })
+            CompanyName = $(try { $moduleInfo.CompanyName } catch { $null = $null })
+            Copyright = $(try { $moduleInfo.Copyright } catch { $null = $null })
+            PublishedDate = $null
+            InstalledDate = $Directory.CreationTime
+            UpdatedDate = $Directory.LastWriteTime
+            LicenseUri = $(try { $moduleInfo.PrivateData.PSData.LicenseUri } catch { $null = $null })
+            ProjectUri = $(try { $moduleInfo.PrivateData.PSData.ProjectUri } catch { $null = $null })
+            IconUri = $(try { $moduleInfo.PrivateData.PSData.IconUri } catch { $null = $null })
+            Tags = $(try { $moduleInfo.PrivateData.PSData.Tags } catch { $null = $null })
+            Includes = $null
+            PowerShellGetFormatVersion = $null
+            ReleaseNotes = $null
+            Dependencies = $(try { $moduleInfo.RequiredModules } catch { $null = $null })
+            RepositorySourceLocation = $null
+            Repository = $null
+            PackageManagementProvider = $null
+            AdditionalMetadata = $null
+            InstalledLocation = $Directory.FullName
+        }
+    }
+
+    # Get each individual ModulePath values. We reverse the array so we can return objects
+    # in order of SystemDirectory/ProgramFiles/UserDirectory like PowerShellGet does.
+    [System.Array]::Reverse(($modulePaths = [System.Environment]::GetEnvironmentVariable('PSModulePath').Split(';', [System.StringSplitOptions]::RemoveEmptyEntries).Trim()))
+
+    # Cycle through each name provided by the caller.
+    foreach ($module in $(if ($Name) { $Name } else { (Get-ChildItem -LiteralPath $modulePaths -Directory).BaseName | Sort-Object -Unique }))
+    {
+        # Open a collector to hold all retrieved objects to return.
+        $moduleDirectories = [System.Collections.Generic.List[System.IO.DirectoryInfo]]::new()
+
+        # Cycle through each module path
+        foreach ($path in $modulePaths)
+        {
+            # Get all installed modules that match our criteria.
+            $installed = Get-ChildItem -Path "$path\$module" -Directory -ErrorAction Ignore | & {
+                process
+                {
+                    # Skip any directory that isn't a version.
+                    [System.Version]$version = $null; if (![System.Version]::TryParse($_.BaseName, [ref]$version))
+                    {
+                        return
+                    }
+
+                    # Handle the various versioning options.
+                    if ($AllVersions -or (!$MinimumVersion -and !$RequiredVersion -and !$MaximumVersion))
+                    {
+                        return $_
+                    }
+                    if ($MinimumVersion -and ($version -ge $MinimumVersion))
+                    {
+                        return $_
+                    }
+                    if ($RequiredVersion -and ($version -eq $RequiredVersion))
+                    {
+                        return $_
+                    }
+                    if ($MaximumVersion -and ($version -le $MaximumVersion))
+                    {
+                        return $_
+                    }
+                }
+            }
+
+            # Add these to our collector if we have anything.
+            if ($installed)
+            {
+                $moduleDirectories.AddRange([System.IO.DirectoryInfo[]]($installed | Sort-Object -Property @{ Expression = { [System.Version]$_.BaseName } } -Unique))
+            }
+        }
+
+        # Process each found directory.
+        if ($moduleDirectories.Count)
+        {
+            # For AllVersions, we return unique entries, but in the order we've collected them.
+            if ($AllVersions)
+            {
+                # These get returned uniquely, but in the order we've collected them.
+                foreach ($directory in ($moduleDirectories | Select-Object -Unique))
+                {
+                    $PSCmdlet.WriteObject((New-ADTInstalledModuleInfo -Directory $directory))
+                }
+                continue
+            }
+
+            # Handle the version-specific stuff next.
+            $moduleDirectories = [System.IO.DirectoryInfo[]]($moduleDirectories | Sort-Object -Property @{ Expression = { [System.Version]$_.BaseName } } -Unique)
+            if ($MinimumVersion)
+            {
+                # Return the highest version greater or equal to the minimum.
+                if ($directory = $moduleDirectories | & { process { if ([System.Version]$_.BaseName -ge $MinimumVersion) { return $_ } } } | Select-Object -Last 1)
+                {
+                    $PSCmdlet.WriteObject((New-ADTInstalledModuleInfo -Directory $directory))
+                }
+                continue
+            }
+            if ($RequiredVersion)
+            {
+                # Return the exact version if we've got it.
+                if ($directory = $moduleDirectories | & { process { if ([System.Version]$_.BaseName -eq $RequiredVersion) { return $_ } } } | Select-Object -First 1)
+                {
+                    $PSCmdlet.WriteObject((New-ADTInstalledModuleInfo -Directory $directory))
+                }
+                continue
+            }
+            if ($MaximumVersion)
+            {
+                # Return the highest version lesser or equal to the maximum
+                if ($directory = $moduleDirectories | & { process { if ([System.Version]$_.BaseName -le $MaximumVersion) { return $_ } } } | Select-Object -Last 1)
+                {
+                    $PSCmdlet.WriteObject((New-ADTInstalledModuleInfo -Directory $directory))
+                }
+                continue
+            }
+            if (!$MinimumVersion -and !$RequiredVersion -and !$MaximumVersion)
+            {
+                # Return the highest version we've got.
+                $PSCmdlet.WriteObject((New-ADTInstalledModuleInfo -Directory $moduleDirectories[-1]))
+                continue
+            }
+        }
+    }
+}
