6fa76e4c39f0db5a9c60e3813d368745ed1000a6
Dan Cunningham <dancunningham@me.com>
2026-02-06 17:05:45 -0500

    feat: create Classic ListSelectionDialog stubs with NotImplementedException

---
+++ src/PSADT/PSADT.UserInterface/Interfaces/Classic/ListSelectionDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using PSADT.UserInterface.DialogOptions;
+
+namespace PSADT.UserInterface.Interfaces.Classic
+{
+    /// <summary>
+    /// Abortable classic dialog form.
+    /// </summary>
+    internal partial class ListSelectionDialog : ClassicDialog, IModalDialog
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListSelectionDialog"/> class.
+        /// </summary>
+        internal ListSelectionDialog() : this(default!)
+        {
+            if (LicenseManager.UsageMode == LicenseUsageMode.Runtime)
+            {
+                throw new InvalidOperationException("This constructor cannot be used in runtime mode.");
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListSelectionDialog"/> class with the specified options.
+        /// </summary>
+        /// <param name="options"></param>
+        internal ListSelectionDialog(ListSelectionDialogOptions options) : base(options)
+        {
+            if (LicenseManager.UsageMode == LicenseUsageMode.Runtime)
+            {
+                throw new NotImplementedException("The ListSelectionDialog is only implemented for the Fluent dialog type.");
+            }
+            InitializeComponent();
+        }
+    }
+}
