b5916fa28ebb78b4e1e97ed994043a90203a31b2
Dan Cunningham <dancunningham@me.com>
2026-02-07 05:07:17 -0500

    Add ListSelectionDialog support in client/server invocation

--- src/PSADT/PSADT.ClientServer.Client/ClientExecutable.cs
+++ src/PSADT/PSADT.ClientServer.Client/ClientExecutable.cs
@@ -703,8 +703,9 @@ namespace PSADT.ClientServer
                 DialogType.HelpConsole => DialogManager.ShowHelpConsole((HelpConsoleOptions)options),
                 DialogType.InputDialog => DialogManager.ShowInputDialog(dialogStyle, (InputDialogOptions)options),
                 DialogType.CustomDialog => DialogManager.ShowCustomDialog(dialogStyle, (CustomDialogOptions)options),
+                DialogType.ListSelectionDialog => DialogManager.ShowListSelectionDialog(dialogStyle, (ListSelectionDialogOptions)options),
                 DialogType.RestartDialog => DialogManager.ShowRestartDialog(dialogStyle, (RestartDialogOptions)options),
-                DialogType.ListSelectionDialog or DialogType.ProgressDialog or _ => throw new ClientException($"The specified DialogType of [{dialogType}] is not supported.", ClientExitCode.UnsupportedDialog)
+                DialogType.ProgressDialog or _ => throw new ClientException($"The specified DialogType of [{dialogType}] is not supported.", ClientExitCode.UnsupportedDialog)
             };
         }
 
