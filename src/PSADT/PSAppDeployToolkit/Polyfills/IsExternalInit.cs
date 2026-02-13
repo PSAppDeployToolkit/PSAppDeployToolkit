251e2043df80a2a0232e6189b5285e76d320b575
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-13 11:31:15 +1100

    Fully leverage C#14's `field` keyword in `DeploymentSession`.
    This gets rid of all the reflection hacks for the compatibility mode stuff, which makes me feel a lot better about it (at least for now).

---
+++ src/PSADT/PSAppDeployToolkit/Polyfills/IsExternalInit.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130
+{
+    /// <summary>
+    /// Polyfill for init-only setters on .NET Framework 4.7.2.
+    /// This class is used by the compiler to mark init-only property setters.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
