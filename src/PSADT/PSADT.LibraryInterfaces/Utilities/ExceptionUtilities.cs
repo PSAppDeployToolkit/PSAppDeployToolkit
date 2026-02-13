35e6fd0aa83595efe4093280739144ef1dc2d470
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-13 13:37:35 +1100

    Replace `ExceptionUtilities.GetHRForWin32ErrorCode()` with `HRESULT_FROM_WIN32()`.

--- src/PSADT/PSADT.LibraryInterfaces/Utilities/ExceptionUtilities.cs
+++ src/PSADT/PSADT.LibraryInterfaces/Utilities/ExceptionUtilities.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using Windows.Win32;
 using Windows.Win32.Foundation;
+using Windows.Win32.System.Diagnostics.Debug;
 
 namespace PSADT.LibraryInterfaces.Utilities
 {
@@ -17,21 +19,23 @@ namespace PSADT.LibraryInterfaces.Utilities
         /// <returns></returns>
         internal static Exception GetExceptionForLastWin32Error(WIN32_ERROR? lastWin32Error = null)
         {
-            int win32ErrorCode = lastWin32Error.HasValue ? unchecked((int)lastWin32Error.Value) : Marshal.GetLastWin32Error();
-            Exception? marshalException = Marshal.GetExceptionForHR(GetHRForWin32ErrorCode(win32ErrorCode));
-            return marshalException is null || (marshalException is COMException && win32ErrorCode != (int)WIN32_ERROR.ERROR_MR_MID_NOT_FOUND)
-                ? new Win32Exception(win32ErrorCode)
+            WIN32_ERROR win32ErrorCode = lastWin32Error ?? unchecked((WIN32_ERROR)Marshal.GetLastWin32Error());
+            Exception? marshalException = Marshal.GetExceptionForHR(HRESULT_FROM_WIN32(win32ErrorCode).Value);
+            return marshalException is null || (marshalException is COMException && win32ErrorCode != WIN32_ERROR.ERROR_MR_MID_NOT_FOUND)
+                ? new Win32Exception(unchecked((int)win32ErrorCode))
                 : marshalException;
         }
 
         /// <summary>
-        /// Gets the HRESULT for the last Win32 error.
+        /// Converts a Win32 error code to its corresponding HRESULT value.
         /// </summary>
-        /// <param name="win32ErrorCode"></param>
-        /// <returns></returns>
-        private static int GetHRForWin32ErrorCode(int win32ErrorCode)
+        /// <remarks>Use this method to translate Win32 error codes into HRESULT values for
+        /// interoperability with APIs that require HRESULT-based error handling.</remarks>
+        /// <param name="win32Error">The Win32 error code to convert. This value should be a valid member of the WIN32_ERROR enumeration.</param>
+        /// <returns>An HRESULT value that represents the specified Win32 error code.</returns>
+        internal static HRESULT HRESULT_FROM_WIN32(WIN32_ERROR win32Error)
         {
-            return (win32ErrorCode & 0x80000000u) != 2147483648u ? (win32ErrorCode & 0xFFFF) | -2147024896 : win32ErrorCode;
+            return PInvoke.HRESULT_FROM_WIN32(win32Error);
         }
     }
 }
