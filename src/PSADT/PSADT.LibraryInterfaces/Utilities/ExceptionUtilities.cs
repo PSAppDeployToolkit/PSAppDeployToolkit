f78f121860c13a6d0b920a71dc563da3c1130bfb
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-14 11:11:31 +1100

    Create new `ExceptionUtilities.GetException()` methods.
    These properly waterfall an NtStatusException > Win32Exception > ManagedException to provide more information to the caller.

--- src/PSADT/PSADT.LibraryInterfaces/Utilities/ExceptionUtilities.cs
+++ src/PSADT/PSADT.LibraryInterfaces/Utilities/ExceptionUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using PSADT.LibraryInterfaces.Exceptions;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.System.Diagnostics.Debug;
@@ -19,11 +20,67 @@ namespace PSADT.LibraryInterfaces.Utilities
         /// <returns></returns>
         internal static Exception GetExceptionForLastWin32Error(WIN32_ERROR? lastWin32Error = null)
         {
-            WIN32_ERROR win32ErrorCode = lastWin32Error ?? unchecked((WIN32_ERROR)Marshal.GetLastWin32Error());
-            Exception? marshalException = Marshal.GetExceptionForHR(HRESULT_FROM_WIN32(win32ErrorCode).Value);
-            return marshalException is null || (marshalException is COMException && win32ErrorCode != WIN32_ERROR.ERROR_MR_MID_NOT_FOUND)
-                ? new Win32Exception(unchecked((int)win32ErrorCode))
-                : marshalException;
+            return GetException(lastWin32Error ?? unchecked((WIN32_ERROR)Marshal.GetLastWin32Error()));
+        }
+
+        /// <summary>
+        /// Creates a new exception that represents the specified Win32 error code.
+        /// </summary>
+        /// <remarks>The exception message is formatted to ensure it ends with a single period, providing
+        /// consistent error messages.</remarks>
+        /// <param name="win32Error">The Win32 error code that identifies the error condition.</param>
+        /// <returns>An instance of the <see cref="Exception"/> class that describes the specified Win32 error.</returns>
+        internal static Exception GetException(WIN32_ERROR win32Error)
+        {
+            // Create the exception given Win32 error code. Trim the trailing period from the message and add it back to ensure consistent formatting.
+            Win32Exception win32Exception = new(unchecked((int)win32Error), new Win32Exception((int)win32Error).Message.TrimEnd('.') + '.');
+
+            // Try for an ManagedException > Win32Exception based on the WIN32_ERROR code, falling back as appripriate.
+            if (GetException(HRESULT_FROM_WIN32(win32Error), win32Exception) is Exception hrException)
+            {
+                // There was a managed exception for the HRESULT corresponding to the WIN32_ERROR code, return that instead of the Win32Exception.
+                return hrException;
+            }
+            else
+            {
+                // Just return the Win32Exception with the message from FormatMessage for the WIN32_ERROR code.
+                return win32Exception;
+            }
+        }
+
+        /// <summary>
+        /// Creates an exception that best represents the error condition indicated by the specified NTSTATUS code.
+        /// </summary>
+        /// <remarks>This method attempts to map the NTSTATUS code to a more user-friendly exception type,
+        /// prioritizing managed exceptions and Win32Exception when possible. If no suitable mapping is found, it
+        /// returns an NtStatusException. The exception message is formatted consistently for clarity.</remarks>
+        /// <param name="ntStatus">The NTSTATUS code that identifies the error to be converted into an exception.</param>
+        /// <returns>An exception corresponding to the provided NTSTATUS code. The returned exception may be a Win32Exception,
+        /// NtStatusException, or another managed exception, depending on the mapping.</returns>
+        internal static Exception GetException(NTSTATUS ntStatus)
+        {
+            // Generate the base NtStatusException for use throughout the method.
+            NtStatusException ntStatusException = new(ntStatus);
+
+            // Try for an ManagedException > Win32Exception > NtStatusException based on the NTSTATUS code, falling back as appripriate.
+            if ((WIN32_ERROR)PInvoke.RtlNtStatusToDosError(ntStatus) is WIN32_ERROR win32Error and not WIN32_ERROR.ERROR_MR_MID_NOT_FOUND)
+            {
+                // Trim the trailing period from the message and add it back to ensure consistent formatting. It's
+                // crucial we call SetLastError() after as the first Win32Exception call can clobber the last error.
+                string message = new Win32Exception((int)win32Error).Message.TrimEnd('.') + '.'; PInvoke.SetLastError(win32Error);
+                Win32Exception win32Exception = new(message, ntStatusException);
+                return GetException(HRESULT_FROM_WIN32(win32Error), win32Exception) is Exception hrException ? hrException : win32Exception;
+            }
+            else if (GetException(HRESULT_FROM_NT(ntStatus), ntStatusException) is Exception hrException)
+            {
+                // There was no suitable Win32Exception, however there was a managed exception for the HRESULT corresponding to the NTSTATUS code.
+                return hrException;
+            }
+            else
+            {
+                // Just return an NtStatusException with the message from FormatMessage for the NTSTATUS code.
+                return ntStatusException;
+            }
         }
 
         /// <summary>
@@ -46,6 +103,42 @@ namespace PSADT.LibraryInterfaces.Utilities
         internal static HRESULT HRESULT_FROM_WIN32(WIN32_ERROR win32Error)
         {
             return PInvoke.HRESULT_FROM_WIN32(win32Error);
+        }
+
+        /// <summary>
+        /// Creates an exception instance that corresponds to the specified HRESULT value, optionally associating it
+        /// with an inner exception for additional context.
+        /// </summary>
+        /// <remarks>This method attempts to map the HRESULT to a known exception type and inject the
+        /// provided inner exception. If the HRESULT does not correspond to a recognized exception or if instantiation
+        /// fails, null is returned.</remarks>
+        /// <param name="hResult">The HRESULT value that identifies the error condition for which an exception should be created.</param>
+        /// <param name="innerException">An optional exception that provides additional context and will be set as the InnerException of the created
+        /// exception.</param>
+        /// <returns>An exception instance that represents the specified HRESULT value, or null if no suitable exception can be
+        /// created.</returns>
+        private static Exception? GetException(HRESULT hResult, Exception innerException)
+        {
+            // Return early if there's no suitable exception to get for the HRESULT value.
+            if (Marshal.GetExceptionForHR(hResult) is not Exception hrException || hrException is COMException)
+            {
+                return null;
+            }
+
+            // Create a new instance of the exception using reflection so we can inject an InnerException.
+            try
+            {
+                if (Activator.CreateInstance(hrException.GetType(), hrException.Message, innerException) is Exception createdException)
+                {
+                    return createdException;
+                }
+            }
+            catch
+            {
+                return null;
+                throw;
+            }
+            return null;
         }
     }
 }
