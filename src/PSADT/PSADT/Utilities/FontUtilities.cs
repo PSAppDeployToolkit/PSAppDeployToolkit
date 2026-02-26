1c074e44a03b32e75072cc9acc026fd988ff699b
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-02-26 13:04:15 +1100

    Tighten up `PSADT.Utilities.AddFonts()` method.
    Public methods shouldn't convey implementation details. Therefore, rather than accepting a `ReadOnlyCollection<string>` object for input, prefer an interface type like `IReadOnlyList<string>`. This also widens the scope of what the caller can send through, while still guaranteeing to them that their object won't be mutated since the interface is still read only.
    
    For the return type, rather than returning the overall added font count, it's best to return this to the caller as a Dictionary that references the source file as the key, and the added count as the value. Again adhering to the comments above, we return this as a `ReadOnlyDictionary<string, int>`, but advertise it as `IReadOnlyDictionary<string, int>` to hide the internal implementation details.

--- src/PSADT/PSADT/Utilities/FontUtilities.cs
+++ src/PSADT/PSADT/Utilities/FontUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using PSADT.Interop;
@@ -29,36 +30,32 @@ namespace PSADT.Utilities
         }
 
         /// <summary>
-        /// Removes a font resource from the specified file.
+        /// Adds font resources from the specified file paths and updates the system to recognize the new fonts.
         /// </summary>
-        /// <param name="fontFilePaths">The full path to the font file.</param>
-        /// <returns>True if the font was removed successfully; otherwise, false.</returns>
-        /// ///
-        public static int AddFonts(ReadOnlyCollection<string> fontFilePaths)
+        /// <remarks>After adding the fonts, the method broadcasts a system-wide notification to prompt
+        /// all top-level windows to refresh their font settings.</remarks>
+        /// <param name="fontFilePaths">A read-only list of strings that specify the file paths of the font files to add. Each path must refer to an
+        /// existing font file.</param>
+        /// <returns>A read-only dictionary that maps each font file path to its corresponding resource identifier.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="fontFilePaths"/> is <see langword="null"/>.</exception>
+        /// <exception cref="FileNotFoundException">Thrown if any file specified in <paramref name="fontFilePaths"/> does not exist.</exception>
+        public static IReadOnlyDictionary<string, int> AddFonts(IReadOnlyList<string> fontFilePaths)
         {
             if (fontFilePaths is null)
             {
                 throw new ArgumentNullException(nameof(fontFilePaths));
             }
-
-            int result = 0;
+            Dictionary<string, int> fontResults = [];
             foreach (string fontFilePath in fontFilePaths)
             {
                 if (!File.Exists(fontFilePath))
                 {
                     throw new FileNotFoundException("Font file not found.", fontFilePath);
                 }
-
-                // Add the font resource
-                result += NativeMethods.AddFontResource(fontFilePath);
+                fontResults.Add(fontFilePath, NativeMethods.AddFontResource(fontFilePath));
             }
-
-            if (result > 0)
-            {
-                // Notify all top-level windows that the font table has changed
-                _ = NativeMethods.SendNotifyMessage(HWND.HWND_BROADCAST, WINDOW_MESSAGE.WM_FONTCHANGE, (WPARAM)0, (LPARAM)0);
-            }
-            return result;
+            _ = NativeMethods.SendNotifyMessage(HWND.HWND_BROADCAST, WINDOW_MESSAGE.WM_FONTCHANGE);
+            return new ReadOnlyDictionary<string, int>(fontResults);
         }
 
         /// <summary>
