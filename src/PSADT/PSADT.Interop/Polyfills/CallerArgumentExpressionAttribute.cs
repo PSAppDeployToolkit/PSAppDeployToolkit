78d5a17f6b0ca54f6bc40daab2c95eb0e670951c
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-03-02 21:23:48 +1100

    Add `CallerArgumentExpressionAttribute` polyfill.

---
+++ src/PSADT/PSADT.Interop/Polyfills/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,26 @@
+﻿#if !NET5_0_OR_GREATER
+using System.ComponentModel;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+{
+    /// <summary>
+    /// Polyfill for CallerArgumentExpressionAttribute on .NET Framework 4.7.2.
+    /// Allows capturing the expression passed to a method parameter as a string at compile time.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="CallerArgumentExpressionAttribute"/> class.
+    /// </remarks>
+    /// <param name="parameterName">The name of the parameter whose expression should be captured.</param>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
+    {
+        /// <summary>
+        /// Gets the name of the parameter whose expression should be captured.
+        /// </summary>
+        public string ParameterName { get; } = parameterName;
+    }
+}
+#endif
