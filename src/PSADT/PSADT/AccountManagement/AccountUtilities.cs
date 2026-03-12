acf4e5e4bf10c3caff9bd52d789b3c57d3a145ab
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-03-12 11:28:15 +1100

    Add some exiting checks on `AccountUtilities.CallerUsingServiceUI` setup.
    Have never seen this before, but received the following on a recent pipeline run
    
    ```
    [2026-03-11T18:03:03.3115623Z] InvalidOperationException: Process has exited, so the requested information is not available.
    [2026-03-11T18:03:03.3160841Z] TypeInitializationException: The type initializer for 'PSADT.AccountManagement.AccountUtilities' threw an exception.
    [2026-03-11T18:03:03.3221251Z] MethodInvocationException: Exception calling "WriteLogEntry" with "9" argument(s): "The type initializer for 'PSADT.AccountManagement.AccountUtilities' threw an exception."
    [2026-03-11T18:03:03.3264340Z] at Write-ADTLogEntry<End>, D:\a\PSAppDeployToolkit\PSAppDeployToolkit\src\PSAppDeployToolkit\Public\Write-ADTLogEntry.ps1:180
    [2026-03-11T18:03:03.3324882Z] at Invoke-ADTFunctionErrorHandler, D:\a\PSAppDeployToolkit\PSAppDeployToolkit\src\PSAppDeployToolkit\Public\Invoke-ADTFunctionErrorHandler.ps1:160
    [2026-03-11T18:03:03.3377963Z] at Add-ADTFont<Process>, D:\a\PSAppDeployToolkit\PSAppDeployToolkit\src\PSAppDeployToolkit\Public\Add-ADTFont.ps1:159
    [2026-03-11T18:03:03.3458740Z] at <ScriptBlock>, D:\a\PSAppDeployToolkit\PSAppDeployToolkit\src\Tests\Unit\Add-ADTFont.Tests.ps1:359
    ```

--- src/PSADT/PSADT/AccountManagement/AccountUtilities.cs
+++ src/PSADT/PSADT/AccountManagement/AccountUtilities.cs
@@ -71,7 +71,7 @@ namespace PSADT.AccountManagement
             CallerIsLocalService = CallerSid.IsWellKnown(WellKnownSidType.LocalServiceSid);
             CallerIsNetworkService = CallerSid.IsWellKnown(WellKnownSidType.NetworkServiceSid);
             CallerIsSystemInteractive = CallerIsLocalSystem && Environment.UserInteractive;
-            CallerUsingServiceUI = ProcessUtilities.GetParentProcesses().Any(static p => p.ProcessName.Equals("ServiceUI", StringComparison.OrdinalIgnoreCase));
+            CallerUsingServiceUI = ProcessUtilities.GetParentProcesses().Any(static p => !ProcessUtilities.HasProcessExited(p) && p.ProcessName.Equals("ServiceUI", StringComparison.OrdinalIgnoreCase));
 
             // Generate a RunAsActiveUser object for the current user.
             CallerRunAsActiveUser = new(CallerUsername, CallerSid, CallerSessionId, CallerIsAdmin);
