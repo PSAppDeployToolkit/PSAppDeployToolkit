a4bee272f1226a87170b1a09b55e6d30dec0f956
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-03-15 11:18:20 +1100

    Tidy up some `$deploymentMode` stuff within the dialog functions.

--- src/PSAppDeployToolkit/Public/Show-ADTInstallationProgress.ps1
+++ src/PSAppDeployToolkit/Public/Show-ADTInstallationProgress.ps1
@@ -156,13 +156,13 @@ function Show-ADTInstallationProgress
         $adtStrings = Get-ADTStringTable -SessionState $SessionState
 
         # Set up DeploymentType.
-        [System.String]$deploymentType = if ($adtSession)
+        [System.String]$deploymentType = if (!$adtSession)
         {
-            $adtSession.DeploymentType
+            [PSAppDeployToolkit.Foundation.DeploymentType]::Install
         }
         else
         {
-            [PSAppDeployToolkit.Foundation.DeploymentType]::Install
+            $adtSession.DeploymentType
         }
 
         # Set up defaults if not specified.
