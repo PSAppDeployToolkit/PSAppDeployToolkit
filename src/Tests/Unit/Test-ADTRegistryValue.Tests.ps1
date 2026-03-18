d3031b2367e6b59655c10b4ddcfbeb8e935d2e0a
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-03-17 23:19:40 -0400

    Add Pester tests for `Test-ADTRegistryValue`

---
+++ src/Tests/Unit/Test-ADTRegistryValue.Tests.ps1
@@ -0,0 +1,41 @@
+﻿BeforeAll {
+    Remove-Module PSAppDeployToolkit -Force -ErrorAction SilentlyContinue
+    Import-Module "$PSScriptRoot\..\..\PSAppDeployToolkit\PSAppDeployToolkit.psd1" -Force
+}
+Describe 'Test-ADTRegistryValue' {
+    BeforeAll {
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'TestRegistry', Justification = 'This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.')]
+        $TestRegistry = (New-Item -Path 'TestRegistry:\TestLocation' -ItemType Directory).PSPath
+        New-ItemProperty -LiteralPath $TestRegistry -Name 'Test' -Value 0 -PropertyType DWord | Out-Null
+
+        # Mock Write-ADTLogEntry due to its expense when running via Pester.
+        Mock -ModuleName PSAppDeployToolkit Write-ADTLogEntry { }
+    }
+
+    Context 'Functionality' {
+        It 'Should return $true' {
+            Test-ADTRegistryValue -Key $TestRegistry -Name 'Test' | Should -Be $true
+        }
+        It 'Should return $false' {
+            Test-ADTRegistryValue -Key $TestRegistry -Name 'DoesNotExist' | Should -Be $false
+        }
+    }
+
+    Context 'Input Validation' {
+        It 'Should verify that Key is not null, empty or whitespace' {
+            { Test-ADTRegistryValue -Key $null -Name 'Anything' } | Should -Throw
+            { Test-ADTRegistryValue -Key '' -Name 'Anything' } | Should -Throw
+            { Test-ADTRegistryValue -Key ' ' -Name 'Anything' } | Should -Throw
+        }
+        It 'Should verify that Name is not null, empty or whitespace' {
+            { Test-ADTRegistryValue -Key 'Anything' -Name $null } | Should -Throw
+            { Test-ADTRegistryValue -Key 'Anything' -Name '' } | Should -Throw
+            { Test-ADTRegistryValue -Key 'Anything' -Name ' ' } | Should -Throw
+        }
+        It 'Should verify that SID is not null or empty' {
+            { Test-ADTRegistryValue -Key 'Anything' -Name 'Test' -SID $null } | Should -Throw
+            { Test-ADTRegistryValue -Key 'Anything' -Name 'Test' -SID '' } | Should -Throw
+            { Test-ADTRegistryValue -Key 'Anything' -Name 'Test' -SID ' ' } | Should -Throw
+        }
+    }
+}
