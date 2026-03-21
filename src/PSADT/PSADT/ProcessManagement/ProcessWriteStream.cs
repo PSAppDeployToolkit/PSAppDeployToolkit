5730ceb638a43a8ad71d8b0e094221ec5cbb82ed
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-03-21 19:59:59 +1100

    Simplify the `ProcessLaunchData` `STARTUPINFOW` object instantiation.

---
+++ src/PSADT/PSADT/ProcessManagement/ProcessWriteStream.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace PSADT.ProcessManagement
+{
+    /// <summary>
+    /// Represents a write handle for standard input or output associated with a process, enabling asynchronous write
+    /// operations.
+    /// </summary>
+    /// <param name="Task">The task that manages the asynchronous operation for the standard I/O write handle. Must not be null.</param>
+    internal sealed record ProcessWriteStream(Task Task) : ProcessStream(Task);
+}
