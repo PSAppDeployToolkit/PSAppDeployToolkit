2a90dc0bf8099d9ffa3f0dac5db1fcba33ae820a
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-03-23 00:12:58 -0400

    Add `Test-ADTCallerIsAdmin` pester tests

---
+++ src/Tests/Unit/Test-ADTCallerIsAdmin.Tests.ps1
@@ -0,0 +1,20 @@
+﻿BeforeAll {
+    Remove-Module PSAppDeployToolkit -Force -ErrorAction SilentlyContinue
+    Import-Module "$PSScriptRoot\..\..\PSAppDeployToolkit\PSAppDeployToolkit.psd1" -Force
+}
+Describe 'Test-ADTCallerIsAdmin' {
+    Context 'Functionality' {
+        It 'Should return $true when the caller is in the Administrator role' {
+            $callerIsAdmin = [System.Security.Principal.WindowsPrincipal]::new([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+            if ($callerIsAdmin)
+            {
+                Test-ADTCallerIsAdmin | Should -BeTrue
+            }
+            else
+            {
+                Test-ADTCallerIsAdmin | Should -BeFalse
+            }
+        }
+    }
+}
