b1315fd4881f4e32e9496389f2c00baa623bfc99
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-04-06 15:21:17 +1000

    Ensure a few single line methods are appropriately inlined.

--- src/PSADT/PSADT/Foundation/ClientServerUtilities.cs
+++ src/PSADT/PSADT/Foundation/ClientServerUtilities.cs
@@ -19,6 +19,7 @@ namespace PSADT.Foundation
         /// <remarks>This method updates a specific registry key to signal that a no-wait operation has
         /// completed successfully. It is intended for internal use and should not be called directly by external
         /// code.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void SetOperationSuccessFlag()
         {
             Registry.SetValue(UserRegistryPath, OperationSuccessRegistryProperty, 1, RegistryValueKind.DWord);
