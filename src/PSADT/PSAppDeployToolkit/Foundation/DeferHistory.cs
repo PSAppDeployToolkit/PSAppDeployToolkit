2532dc29dd7795a5c67e0e6ad45114e54fe92b1d
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-04-06 16:42:53 +1000

    Regrettably, get rid of the `IsExternalInit` polyfill stuff from `PSADT.Interop`.
    PowerShell treats these as settable fields as it has no understanding of the `init` concept. This is unacceptable for our immutability requirements/standards, so it has to go. There is an open ticket for this (https://github.com/PowerShell/PowerShell/issues/13819), but with half a decade of inactivity, we can't wait for the PowerShell team to pull their finger out with this, and even when they do, it still won't help the Windows PowerShell situation.

--- src/PSADT/PSAppDeployToolkit/Foundation/DeferHistory.cs
+++ src/PSADT/PSAppDeployToolkit/Foundation/DeferHistory.cs
@@ -6,8 +6,36 @@ namespace PSAppDeployToolkit.Foundation
     /// Represents the deferral history for an operation, including the number of remaining deferrals, the deadline for
     /// completion, and the most recent run interval time.
     /// </summary>
-    /// <param name="DeferTimesRemaining">The number of times the operation can still be deferred. Specify null if there is no limit.</param>
-    /// <param name="DeferDeadline">The deadline by which the operation must be completed. Specify null if there is no deadline.</param>
-    /// <param name="DeferRunIntervalLastTime">The most recent time the defer interval was run. Specify null if the operation has not yet run.</param>
-    public sealed record DeferHistory(uint? DeferTimesRemaining, DateTime? DeferDeadline, DateTime? DeferRunIntervalLastTime);
+    public sealed record DeferHistory
+    {
+        /// <summary>
+        /// Initializes a new instance of the DeferHistory class with the specified defer times remaining, deadline, and
+        /// last run interval time.
+        /// </summary>
+        /// <param name="deferTimesRemaining">The number of times the operation can still be deferred. Specify null if there is no limit.</param>
+        /// <param name="deferDeadline">The deadline by which the operation must be completed. Specify null if there is no deadline.</param>
+        /// <param name="deferRunIntervalLastTime">The date and time when the defer run interval was last executed. Specify null if the operation has not been
+        /// run yet.</param>
+        public DeferHistory(uint? deferTimesRemaining, DateTime? deferDeadline, DateTime? deferRunIntervalLastTime)
+        {
+            DeferTimesRemaining = deferTimesRemaining;
+            DeferDeadline = deferDeadline;
+            DeferRunIntervalLastTime = deferRunIntervalLastTime;
+        }
+
+        /// <summary>
+        /// Gets the number of times the operation can be deferred before it must be completed.
+        /// </summary>
+        public uint? DeferTimesRemaining { get; }
+
+        /// <summary>
+        /// Gets the deadline by which the deferred operation must be completed, if one is set.
+        /// </summary>
+        public DateTime? DeferDeadline { get; }
+
+        /// <summary>
+        /// Gets the date and time when the defer run interval was last recorded, or null if it has not been set.
+        /// </summary>
+        public DateTime? DeferRunIntervalLastTime { get; }
+    }
 }
