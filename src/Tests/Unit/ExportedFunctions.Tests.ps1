94d962bbf2b316c4d5cc84c11d20ce422b75ca66
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-04-09 23:29:46 -0400

    Overhaul ExportedFunctions Pester tests to validate output types.

--- src/Tests/Unit/ExportedFunctions.Tests.ps1
+++ src/Tests/Unit/ExportedFunctions.Tests.ps1
@@ -10,6 +10,8 @@
     $moduleExported = Get-Command -Module $ModuleName | Select-Object -ExpandProperty Name
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'manifestExported', Justification = "This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.")]
     $manifestExported = ($manifestContent.ExportedFunctions).Keys
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'typeAcceleratorNames', Justification = "This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.")]
+    $typeAcceleratorNames = [System.Management.Automation.PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')::Get.Keys
 }
 BeforeDiscovery {
     Set-Location -Path $PSScriptRoot
@@ -23,46 +25,94 @@ BeforeDiscovery {
     $manifestExported = ($manifestContent.ExportedFunctions).Keys
 }
 Describe $ModuleName {
-
     Context 'Exported Commands' -Fixture {
-
         Context 'Number of commands' -Fixture {
-
             It 'Exports the same number of public functions as what is listed in the Module Manifest' {
                 ($manifestExported | Measure-Object).Count | Should -BeExactly ($moduleExported | Measure-Object).Count
             }
-
         }
 
         Context 'Explicitly exported commands' {
-
             It 'Includes <_> in the Module Manifest ExportedFunctions' -ForEach $moduleExported {
                 $manifestExported -contains $_ | Should -BeTrue
             }
-
         }
-    } #context_ExportedCommands
+    }
 
     Context 'Command Help' -Fixture {
         Context '<_>' -ForEach $moduleExported {
-
             BeforeEach {
                 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'help', Justification = "This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.")]
                 $help = Get-Help -Name $_ -Full
+
+                [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'command', Justification = 'This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.')]
+                $command = Get-Command -Name $_
+
+                [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'outputTypes', Justification = 'This variable is used within scriptblocks that PSScriptAnalyzer has no visibility of.')]
+                $outputTypes = $command.OutputType | & { process { return $_.Type } }
             }
 
-            It -Name 'Includes a Synopsis' -Test {
+            It 'Includes a Synopsis' {
                 $help.Synopsis | Should -Not -BeNullOrEmpty
             }
-
-            It -Name 'Includes a Description' -Test {
+            It 'Includes a Description' {
                 $help.description.Text | Should -Not -BeNullOrEmpty
             }
-
-            It -Name 'Includes an Example' -Test {
+            It 'Includes an Example' {
                 $help.examples.example | Should -Not -BeNullOrEmpty
             }
+            It 'Includes an Input' {
+                $help.inputTypes | Should -Not -BeNullOrEmpty
+            }
+            It 'Includes an Output' {
+                $help.returnValues | Should -Not -BeNullOrEmpty
+            }
+            It 'All outputs defined in the comment-based help are defined in OutputType attributes' {
+                $returnValueTypes = foreach ($returnValue in $help.returnValues.returnValue.Type.Name)
+                {
+                    $returnValueName = $returnValue.Split([System.Environment]::NewLine)[0]
+                    if ($returnValueName -eq 'None')
+                    {
+                        continue
+                    }
+
+                    # All types should be referenced by their full name
+                    $returnValueName | Should -Not -BeIn $typeAcceleratorNames -Because 'types should be referenced by their full name in the comment-based help'
+
+                    # Validate that the type specified in the comment-based help is a valid type before we do an early return
+                    $returnValueType = [System.Type]$returnValueName
+
+                    # Functions that aren't CmdletBinding cannot have the [OutputType()] attribute
+                    if (!$command.CmdletBinding)
+                    {
+                        continue
+                    }
+
+                    $returnValueType | Should -BeIn $outputTypes -Because 'all outputs defined the comment-based help should also be defined in OutputType attributes'
+
+                    # Validate that the generic type arguments are referenced by their full name
+                    if ($returnValueType.IsConstructedGenericType)
+                    {
+                        foreach ($genericArgument in $returnValueType.GenericTypeArguments)
+                        {
+                            $returnValueName | Should -Match ([System.Text.RegularExpressions.Regex]::Escape($genericArgument.FullName)) -Because 'generic type arguments should be referenced by their full name'
+                        }
+                    }
+
+                    $returnValueType
+                }
+
+                if ($null -eq $returnValueTypes)
+                {
+                    $outputTypes | Should -BeNullOrEmpty -Because 'all outputs defined in OutputType attributes should be also defined in the comment-based help'
+                }
+
+                foreach ($outputType in $outputTypes)
+                {
+                    $outputType | Should -BeIn $returnValueTypes -Because 'all outputs defined in OutputType attributes should also be defined in the comment-based help'
+                }
+            }
         }
-    } #context_CommandHelp
+    }
 }
 
