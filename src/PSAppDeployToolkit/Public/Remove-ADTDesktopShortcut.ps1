268c0ffe3f53fd1d2886e58c213312b881250ebe
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-05-03 00:29:59 -0400

    Document the valid scopes in `Remove-ADTDesktopShortcut`

--- src/PSAppDeployToolkit/Public/Remove-ADTDesktopShortcut.ps1
+++ src/PSAppDeployToolkit/Public/Remove-ADTDesktopShortcut.ps1
@@ -14,7 +14,11 @@ function Remove-ADTDesktopShortcut
         The `Remove-ADTDesktopShortcut` function removes desktop shortcuts from the common desktop folder or logged on user's desktop, either all since the commencement of the session, all shortcuts in general, or based on a custom FilterScript.
 
     .PARAMETER Scope
-        The scope of which to target (common desktop and/or logged on user's desktop).
+        The scope of which desktop directory to target.
+
+        Valid values for this parameter are:
+        - `AllUsersDesktop`: Removes shortcuts from the common desktop (`$EnvCommonDesktop`).
+        - `RunAsActiveUser`: Removes shortcuts from the active user's desktop.
 
     .PARAMETER SinceSessionStart
         Removes all shortcuts created after the active deployment session commenced.
