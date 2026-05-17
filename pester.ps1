8bc22dcb5c1fd2ccc443c11315c7b8e65af04d49
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-05-17 15:19:25 -0400

    Add build script to run pester tests independently

---
+++ pester.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+param
+(
+)
+
+# Stop on a dime, then import our build module and get started.
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+Set-StrictMode -Version 3
+try
+{
+	Import-Module -Name ([System.Management.Automation.WildcardPattern]::Escape("$PSScriptRoot\src\PSAppDeployToolkit.Build\PSAppDeployToolkit.Build.psd1"))
+	Invoke-ADTModuleBuild -Steps Prerequisites, Dependencies, Analyze, UnitTests
+}
+catch
+{
+	$PSCmdlet.ThrowTerminatingError($_)
+}
