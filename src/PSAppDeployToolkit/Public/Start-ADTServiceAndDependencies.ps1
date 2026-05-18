3b541aa6d682f955fa3b7049c6bcdbf062763d70
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-05-18 12:30:12 +1000

    Fix up some typos as pointed out by our dear friend, CodeQL.

--- src/PSAppDeployToolkit/Public/Start-ADTServiceAndDependencies.ps1
+++ src/PSAppDeployToolkit/Public/Start-ADTServiceAndDependencies.ps1
@@ -14,10 +14,10 @@ function Start-ADTServiceAndDependencies
         The `Start-ADTServiceAndDependencies` function starts a specified Windows service and any service(s) that depend on it. It provides options to skip starting dependent services, wait for a service to get out of a pending state, and return the service object.
 
     .PARAMETER Name
-        Specifies the service name(s) of services to be stopped. Wildcards are permitted.
+        Specifies the service name(s) of services to be started. Wildcards are permitted.
 
     .PARAMETER DisplayName
-        Specifies the display name(s) of services to be stopped. Wildcards are permitted.
+        Specifies the display name(s) of services to be started. Wildcards are permitted.
 
     .PARAMETER InputObject
         Specifies `ServiceController` object(s) representing the services to be started.
@@ -59,12 +59,12 @@ function Start-ADTServiceAndDependencies
     .EXAMPLE
         Start-ADTServiceAndDependencies -Name 'wuauserv' -PendingStatusWait 00:01:00
 
-        Starts the Windows Update service and any service(s) that depend on it, waiting 1 minute for the serivce to start.
+        Starts the Windows Update service and any service(s) that depend on it, waiting 1 minute for the service to start.
 
     .EXAMPLE
         Start-ADTServiceAndDependencies -Name 'wuauserv' -PendingStatusWait (New-TimeSpan -Minutes 1)
 
-        Starts the Windows Update service and any service(s) that depend on it, waiting 1 minute for the serivce to start.
+        Starts the Windows Update service and any service(s) that depend on it, waiting 1 minute for the service to start.
 
     .NOTES
         An active ADT session is NOT required to use this function.
