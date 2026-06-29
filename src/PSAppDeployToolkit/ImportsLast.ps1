6704e8494ded5d19d46c9604d1d0d1fa0edaca11
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-06-29 19:18:47 +1000

    Fix bad config.psd1 comment for `FluentAccentColorDark` property.

--- src/PSAppDeployToolkit/ImportsLast.ps1
+++ src/PSAppDeployToolkit/ImportsLast.ps1
@@ -130,7 +130,7 @@ try
                                                 FluentAccentColor = $null
 
                                                 # Specify the Accent Color in hex for dark mode (with the first two characters for transparency, 00 = 0%, FF = 100%), e.g. 0xFF0078D7.
-                                                # The value specified here should be literally typed (i.e. `FluentAccentColor = 0xFF0078D7`) and not wrapped in quotes.
+                                                # The value specified here should be literally typed (i.e. `FluentAccentColorDark = 0xFF0078D7`) and not wrapped in quotes.
                                                 FluentAccentColorDark = $null
 
                                                 # Exit code used when a UI prompt times out.
