99377e04344191a2b21b2d07cbf83deef44a0911
neo42JBR <julian.behr@neo42.de>
2026-08-08 09:22:40 +0200

    Apply coding guidelines, split into separate functions and support LocalPrincipal

--- src/PSADT/PSAppDeployToolkit/Attributes/IdentityTransformationAttribute.cs
+++ src/PSADT/PSAppDeployToolkit/Attributes/IdentityTransformationAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Management.Automation;
 using System.Reflection;
 using System.Security.Principal;
@@ -16,81 +17,113 @@ namespace PSAppDeployToolkit.Attributes
         ///  * <see cref="IdentityReference"/>
         ///  * <see cref="WindowsIdentity"/>
         ///  * <see cref="WellKnownSidType"/>
-        ///  * LocalUser
+        ///  * LocalPrincipal
         ///  * <see cref="string"/> representing the above
         /// </summary>
         /// <param name="engineIntrinsics">The PowerShell engine intrinsics.</param>
         /// <param name="inputData">The input value to transform.</param>
-        /// <returns></returns>
-        /// <exception cref="InvalidOperationException">Thrown if the input object did not contain the required properties.</exception>
+        /// <returns>The identity reference the input object represents.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the input object was valid but did not contain the required data.</exception>
         /// <exception cref="ArgumentException">Thrown if the input object is not supported for transformation.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if the input data is null.</exception>
+        [SuppressMessage("Style", "IDE0046:Use conditional expression for return", Justification = "Conditional expression would be unreadable.")]
         public override object Transform(EngineIntrinsics engineIntrinsics, object inputData)
         {
-
+            if (inputData is null)
+            {
+                throw new ArgumentNullException(nameof(inputData), "Cannot transform null to an IdentityReference.");
+            }
             if (inputData is WellKnownSidType wellKnownSidType)
             {
-                return new SecurityIdentifier(wellKnownSidType, null);
+                return new SecurityIdentifier(wellKnownSidType, domainSid: null);
             }
-            else if (inputData is IdentityReference identity)
+            if (inputData is IdentityReference identity)
             {
                 return identity;
             }
-            else if (inputData is WindowsIdentity windowsIdentity)
+            if (inputData is WindowsIdentity windowsIdentity)
             {
-                return windowsIdentity.User
-                    ?? throw new InvalidOperationException("The given WindowsIdentity did not provide the required user identity.");
+                return windowsIdentity.User ?? throw new InvalidOperationException("The given WindowsIdentity did not provide the required user identity.");
             }
-            else if (inputData.GetType().FullName == "Microsoft.PowerShell.Commands.LocalUser"
-                && inputData.GetType().GetProperty("SID", typeof(SecurityIdentifier)) is PropertyInfo localUserSidProperty)
+            if (TryConvertPowerShellCommandObject(inputData, out SecurityIdentifier? pwshCommandSid))
             {
-                return localUserSidProperty.GetValue(inputData)
-                    ?? throw new InvalidOperationException("The given LocalUser did not provide the required SID property.");
+                return pwshCommandSid;
             }
-            else if (inputData is string str && TryParseIdentityReference(str, out IdentityReference strIdentity))
+            if (inputData is string str && TryParseIdentityReference(str, out IdentityReference? strIdentity))
             {
                 return strIdentity;
             }
-            else
-            {
-                throw new ArgumentException("Input data must be of type string, IdentityReference, WindowsIdentity or a string representing any of those types, or a WellKnownSidType value.", nameof(inputData));
-            }
+            throw new ArgumentException("Input data must be of type IdentityReference, WindowsIdentity, LocalPrincipal, WellKnownSidType or a string representing any of those types.", nameof(inputData));
         }
 
         /// <summary>
-        /// Tries to parse a string as <see cref="IdentityReference"/>, returning the corresponding object.
+        /// Try to parse string into an <see cref="IdentityReference"/>.
         /// </summary>
-        /// <param name="identityString">The string to parse.</param>
-        /// <param name="identity">The <see cref="IdentityReference"/> if parsing succeeds; otherwise NullSid</param>
-        /// <returns>True if the input string was successfully parsed  name; otherwise, false.</returns>
-        private static bool TryParseIdentityReference(string identityString, out IdentityReference identity)
+        /// <param name="identityString">The string representing the IdentityReference.</param>
+        /// <param name="identity">The parsed IdentityReference.</param>
+        /// <returns>True if parsing was successful, otherwise false.</returns>
+        private static bool TryParseIdentityReference(string identityString, [NotNullWhen(true)] out IdentityReference? identity)
         {
-            identity = new SecurityIdentifier(WellKnownSidType.NullSid, null);
+            identity = null;
             if (string.IsNullOrWhiteSpace(identityString))
             {
                 return false;
             }
-
-            // Try Enum
-            if (Enum.TryParse(identityString, true, out WellKnownSidType wellKnownSidType))
+            if (Enum.TryParse(identityString, ignoreCase: true, out WellKnownSidType wellKnownSidType))
             {
-                identity = new SecurityIdentifier(wellKnownSidType, null);
+                identity = new SecurityIdentifier(wellKnownSidType, domainSid: null);
                 return true;
             }
-
-            // Try SID
-            if (identityString.StartsWith("S-", StringComparison.OrdinalIgnoreCase))
+            if (TryParseSid(identityString, out SecurityIdentifier? sid))
             {
-                try
-                {
-                    identity = new SecurityIdentifier(identityString);
-                    return true;
-                }
-                catch (ArgumentException)
-                {
-                }
+                identity = sid;
+                return true;
             }
+            if (TryParseNTAccount(identityString, out NTAccount? ntAccount))
+            {
+                identity = ntAccount;
+                return true;
+            }
+            return false;
+        }
 
-            // Try NTAccount
+        /// <summary>
+        /// Try to parse string into a <see cref="SecurityIdentifier"/>.
+        /// </summary>
+        /// <param name="identityString">The string representing the SecrutiyIdentifier.</param>
+        /// <param name="identity">The parsed SecrutiyIdentifier.</param>
+        /// <returns>True if parsing was successful, otherwise false.</returns>
+        private static bool TryParseSid(string identityString, [NotNullWhen(true)] out SecurityIdentifier? identity)
+        {
+            identity = null;
+            if (string.IsNullOrWhiteSpace(identityString))
+            {
+                return false;
+            }
+            try
+            {
+                identity = new SecurityIdentifier(identityString);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Try to parse string into a <see cref="NTAccount"/>.
+        /// </summary>
+        /// <param name="identityString">The string representing the NTAccount.</param>
+        /// <param name="identity">The parsed NTAccount.</param>
+        /// <returns>True if parsing was successful, otherwise false.</returns>
+        private static bool TryParseNTAccount(string identityString, [NotNullWhen(true)] out NTAccount? identity)
+        {
+            identity = null;
+            if (string.IsNullOrWhiteSpace(identityString))
+            {
+                return false;
+            }
             try
             {
                 identity = new NTAccount(identityString);
@@ -98,8 +131,49 @@ namespace PSAppDeployToolkit.Attributes
             }
             catch (IdentityNotMappedException)
             {
+                return false;
             }
+        }
 
+
+        /// <summary>
+        /// Try to extract the SID from a LocalPrincipal type using reflection. The LocalPrincipal type is used within the Microsoft.PowerShell.LocalAccounts module.
+        /// </summary>
+        /// <param name="identityObject">The object to analyse.</param>
+        /// <param name="identity">The parsed SecrutiyIdentifier.</param>
+        /// <returns>True if extraction was successful, otherwise false.</returns>
+        private static bool TryConvertPowerShellCommandObject(object identityObject, [NotNullWhen(true)] out SecurityIdentifier? identity)
+        {
+            identity = null;
+            // Unwrap the actual object, if it is wrapped
+            if (identityObject is PSObject psObj)
+            {
+                identityObject = psObj.BaseObject;
+            }
+            if (identityObject is null)
+            {
+                return false;
+            }
+            Type objectType = identityObject.GetType();
+            if (!string.Equals(objectType.Namespace, "Microsoft.PowerShell.Commands", StringComparison.Ordinal))
+            {
+                return false;
+            }
+            if (string.Equals(objectType.Name, "LocalPrincipal", StringComparison.Ordinal)
+                && objectType.GetProperty("SID", typeof(SecurityIdentifier)) is PropertyInfo directProperty
+                && directProperty.GetValue(identityObject) is SecurityIdentifier directSid)
+            {
+                identity = directSid;
+                return true;
+            }
+            if (objectType.BaseType is Type parentType
+                && string.Equals(parentType.Name, "LocalPrincipal", StringComparison.Ordinal)
+                && objectType.GetProperty("SID", typeof(SecurityIdentifier)) is PropertyInfo parentProperty
+                && parentProperty.GetValue(identityObject) is SecurityIdentifier parentSid)
+            {
+                identity = parentSid;
+                return true;
+            }
             return false;
         }
     }
