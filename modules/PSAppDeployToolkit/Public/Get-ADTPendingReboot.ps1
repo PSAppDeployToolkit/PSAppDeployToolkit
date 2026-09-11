1c6a21acec60fe2e93f2de7e6f844345d5ae664e
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-09-11 14:21:16 +1000

    Centralise `Out-String -Width` setup into a default parameter.
    Fixes #2273.

--- modules/PSAppDeployToolkit/Public/Get-ADTPendingReboot.ps1
+++ modules/PSAppDeployToolkit/Public/Get-ADTPendingReboot.ps1
@@ -141,7 +141,7 @@ function Get-ADTPendingReboot
                     $PendingFileRenameOperations,
                     $PendRebootErrorMsg.AsReadOnly()
                 )
-                Write-ADTLogEntry -Message "Pending reboot status on the local computer [$HostName]:`n$($PendingRebootInfo | Format-List | Out-String -Width ([System.Int16]::MaxValue))"
+                Write-ADTLogEntry -Message "Pending reboot status on the local computer [$HostName]:`n$($PendingRebootInfo | Format-List | Out-String)"
                 return $PendingRebootInfo
             }
             catch
