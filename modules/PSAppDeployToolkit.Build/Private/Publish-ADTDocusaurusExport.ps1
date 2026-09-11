dbcf35408c61f0fe90f2b5166e512b7adb6cbe75
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-09-11 13:04:49 +1000

    Tidy up variable names within `Publish-ADTDocusaurusExport`.

--- modules/PSAppDeployToolkit.Build/Private/Publish-ADTDocusaurusExport.ps1
+++ modules/PSAppDeployToolkit.Build/Private/Publish-ADTDocusaurusExport.ps1
@@ -12,12 +12,12 @@ function Publish-ADTDocusaurusExport
     {
         # Clone the destination repo.
         Write-ADTBuildLogEntry -Message "Cloning destination repository, this may take a while."
-        $dstBnch = 'main'; $dstRepo = "https://github.com/$env:GITHUB_REPOSITORY_OWNER/website.git"
+        $destBranch = 'main'; $dstRepo = "https://github.com/$env:GITHUB_REPOSITORY_OWNER/website.git"
         $gitAuthToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("x-access-token:$env:API_TOKEN_GITHUB"))
         $gitAuthHeader = "AUTHORIZATION: basic $gitAuthToken"
-        $dstBase = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [System.IO.Path]::GetRandomFileName())
-        $dstPath = "$dstBase\docs\reference\functions"
-        $null = git -c "http.https://github.com/.extraheader=$gitAuthHeader" clone -q -b $dstBnch $dstRepo $dstBase
+        $destBase = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [System.IO.Path]::GetRandomFileName())
+        $destPath = "$destBase\docs\reference\functions"
+        $null = git -c "http.https://github.com/.extraheader=$gitAuthHeader" clone -q -b $destBranch $dstRepo $destBase
         if ($Global:LASTEXITCODE)
         {
             throw "The cloning of the destination repository failed."
@@ -25,11 +25,11 @@ function Publish-ADTDocusaurusExport
 
         # Update the docs from the source repo to the destination.
         Write-ADTBuildLogEntry -Message "Updating the markdown files in destination repository."
-        Remove-Item -Path "$dstPath\*" -Force -Confirm:$false
-        Get-ChildItem -Path "$($Script:ModuleConstants.Paths.DocusaurusOutput)\commands\*" -File | Copy-Item -Destination $dstPath
+        Remove-Item -Path "$destPath\*" -Force -Confirm:$false
+        Get-ChildItem -Path "$($Script:ModuleConstants.Paths.DocusaurusOutput)\commands\*" -File | Copy-Item -Destination $destPath
 
         # Change into the repository's directory.
-        Push-Location -LiteralPath $dstBase
+        Push-Location -LiteralPath $destBase
         try
         {
             # Add any changes that may exist.
