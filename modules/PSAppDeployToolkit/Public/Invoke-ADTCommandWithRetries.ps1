958a89d46985cf03a25ec656e0ff828a038f3d4b
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-09-20 22:35:49 +1000

    Improve command lookup within `Invoke-ADTCommandWithRetries`.

--- modules/PSAppDeployToolkit/Public/Invoke-ADTCommandWithRetries.ps1
+++ modules/PSAppDeployToolkit/Public/Invoke-ADTCommandWithRetries.ps1
@@ -122,13 +122,14 @@ function Invoke-ADTCommandWithRetries
             try
             {
                 # Attempt to get command from our lookup table.
+                [System.Management.Automation.CommandInfo]$commandObj = $null
                 $commandObj = if ($Command -is [System.Management.Automation.CommandInfo])
                 {
                     $Command
                 }
-                elseif ($Script:CommandTable.ContainsKey($Command))
+                elseif ($Script:CommandTable.TryGetValue($Command, [ref]$commandObj))
                 {
-                    $Script:CommandTable.$Command
+                    $commandObj
                 }
                 else
                 {
