c1cece007b0c651ebe47bcb1f02a73bff04c3d3d
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-09-22 14:44:59 +1000

    Fix parameter exclusion within `Uninstall-ADTApplication`.

--- modules/PSAppDeployToolkit/Public/Uninstall-ADTApplication.ps1
+++ modules/PSAppDeployToolkit/Public/Uninstall-ADTApplication.ps1
@@ -261,7 +261,7 @@ function Uninstall-ADTApplication
         }
 
         # Build the hashtable with the options that will be passed to Start-ADTMsiProcess using splatting
-        $sampParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation -Exclude InstalledApplication, Name, NameMatch, ProductCode, FilterScript, ApplicationType, ForceUninstallString, WaitForChildProcesses, KillChildProcessesWithParent
+        $sampParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation -Exclude InstalledApplication, Name, NameMatch, ProductCode, FilterScript, ApplicationType, ForceUninstallString, WaitForChildProcesses, KillChildProcessesWithParent, Force
         $sampParams.Action = 'Uninstall'
 
         # Build the hashtable with the options that will be passed to Start-ADTProcess using splatting.
