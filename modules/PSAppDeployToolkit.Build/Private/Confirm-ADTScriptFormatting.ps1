271ba4d95ccd334a684dee2fd8989930479c0840
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2026-09-25 16:41:39 +1000

    Reposition some PSScriptAnalyzer build log messages.

--- modules/PSAppDeployToolkit.Build/Private/Confirm-ADTScriptFormatting.ps1
+++ modules/PSAppDeployToolkit.Build/Private/Confirm-ADTScriptFormatting.ps1
@@ -11,7 +11,6 @@ function Confirm-ADTScriptFormatting
     function Confirm-ADTScriptFormattingImpl
     {
         # Verify the formatting of all PowerShell script files within the repository.
-        Write-ADTBuildLogEntry -Message "Confirming all PowerShell files are formatted correctly."
         if ([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]$result = Invoke-ScriptAnalyzer -Path $Script:ModuleConstants.Paths.SourceRoot -Setting CodeFormattingAllman -ExcludeRule PSAlignAssignmentStatement -Recurse -Fix:(!(Test-ADTBuildingWithinPipeline)) -Verbose:$false | & { process { if (!$_.RuleName.Equals('PSUseToExportFieldsInManifest') -or !$_.ScriptName.Equals('PSAppDeployToolkit.Extensions.psd1')) { return $_ } } })
         {
             Write-ADTBuildLogEntry -Message "PSScriptAnalyzer returned $($result.Count) script formatting violations." -ForegroundColor DarkRed
@@ -24,6 +23,7 @@ function Confirm-ADTScriptFormatting
     Initialize-ADTModuleBuildFunction
     try
     {
+        Write-ADTBuildLogEntry -Message "Confirming all PowerShell files are formatted correctly."
         Confirm-ADTScriptFormattingImpl
         Complete-ADTModuleBuildFunction
     }
--- modules/PSAppDeployToolkit.Build/Private/Confirm-ADTScriptIntegrity.ps1
+++ modules/PSAppDeployToolkit.Build/Private/Confirm-ADTScriptIntegrity.ps1
@@ -11,7 +11,6 @@ function Confirm-ADTScriptIntegrity
     function Confirm-ADTScriptIntegrityImpl
     {
         # Verify the formatting of all PowerShell script files within the repository.
-        Write-ADTBuildLogEntry -Message "Confirming all PowerShell files have no code violations."
         if ([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]$result = Invoke-ScriptAnalyzer -Path $Script:ModuleConstants.Paths.SourceRoot -ExcludeRule PSUseShouldProcessForStateChangingFunctions, PSUseSingularNouns -Recurse -Fix:(!(Test-ADTBuildingWithinPipeline)) -Verbose:$false | & { process { if ((!$_.RuleName.Equals('PSUseToExportFieldsInManifest') -or !$_.ScriptName.Equals('PSAppDeployToolkit.Extensions.psd1')) -and (!$_.RuleName.Equals('PSAvoidUsingWriteHost') -or ($_.ScriptPath -notmatch 'PSAppDeployToolkit\.Build'))) { return $_ } } })
         {
             Write-ADTBuildLogEntry -Message "PSScriptAnalyzer returned $($result.Count) script formatting violations." -ForegroundColor DarkRed
@@ -24,6 +23,7 @@ function Confirm-ADTScriptIntegrity
     Initialize-ADTModuleBuildFunction
     try
     {
+        Write-ADTBuildLogEntry -Message "Confirming all PowerShell files have no code violations."
         Confirm-ADTScriptIntegrityImpl
         Complete-ADTModuleBuildFunction
     }
